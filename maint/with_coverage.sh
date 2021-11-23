9c040e85b49521df527ccd56e67e1868ca9dda6d
Nick Mathewson <nickm@torproject.org>
2021-11-23 16:37:59 -0500

    Let with_coverage take command-line flags.
    Now the shell is controlled by the presence of an -i flag, and can
    be combined with a command.
    
    This commit also adds usage messages and better reports for a couple
    of kinds of setup errors.

--- maint/with_coverage.sh
+++ maint/with_coverage.sh
@@ -1,9 +1,62 @@
 #!/bin/bash
 
-# usage: with_coverage <command> [args...] : run <command> with [args...]
-#        with_coverage                     : runs bash interactively
-
 set -e
+
+SCRIPT_NAME=$(basename "$0")
+
+function usage()
+{
+    cat <<EOF
+${SCRIPT_NAME}: Generate coverage using grcov.
+
+Usage:
+  with_coverage [opts] <command> [args...] : Run <command> with [args].
+  with_coverage -i [opts]                  : Run bash interactively.
+
+Options:
+  -h: Print this message.
+  -i: Run an interactive shell after the command (if any)
+
+Notes:
+  You need to have grcov, rust-nightly, and llvm-tools-preview installed.
+EOF
+}
+
+interactive=no
+
+while getopts "hi" opt ; do
+    case "$opt" in
+	h) usage
+	   exit 0
+	   ;;
+	i) interactive=yes
+	   ;;
+	*) echo "Unknown option."
+	   exit 1
+	   ;;
+    esac
+done
+
+# Remove the flags we parsed.
+shift $((OPTIND-1))
+
+# Make sure that we'll be doing _something_.
+if [ $# -eq 0 ] && [ $interactive = "no" ]; then
+    echo "No command specified: Use the -i flag if you want a shell."
+    echo
+    echo "Run ${SCRIPT_NAME} -h for help."
+    exit 1
+fi
+
+# Validate that +nightly is installed.  This will log a message to stderr
+# if it isn't.
+cargo +nightly -h >/dev/null
+
+# Validate that grcov is installed.
+if [ "$(which grcov 2>/dev/null)" = "" ]; then
+    echo "grcov appears not to be installed.  Try 'cargo install grcov'." >&2
+    exit 1
+fi
 
 COVERAGE_BASEDIR=$(git rev-parse --show-toplevel)
 export RUSTFLAGS="-Z instrument-coverage"
@@ -15,11 +68,15 @@ rm -r "$COVERAGE_BASEDIR/coverage_meta" || true
 
 mkdir -p "$COVERAGE_BASEDIR/coverage"
 
-if [ $# -eq 0 ]; then
-	# when run interactivelly, don't die on error
-	bash || true
-else
-	"$@"
+if [ $# -ne 0 ]; then
+    "$@"
+fi
+
+if [ $interactive = "yes" ] ; then
+    echo "Launching a bash shell."
+    echo "Exit this shell when you are ready to genate a coverage report."
+    # when run interactivelly, don't die on error
+    bash || true
 fi
 
 echo "Generating report..."
