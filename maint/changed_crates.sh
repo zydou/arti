d3039d805d9628a6a32e6034872679f943fc8fc7
Nick Mathewson <nickm@torproject.org>
2022-01-31 10:30:50 -0500

    Update changed_crates to be more useful.

--- maint/changed_crates.sh
+++ maint/changed_crates.sh
@@ -11,8 +11,9 @@ fi
 
 for crate in $(cd "${TOP}/crates/" && ls); do
     if git diff --quiet "$TAG..HEAD" "${TOP}/crates/${crate}"; then
-	echo "$crate: ...:"
+	# echo "$crate: ...:"
+	:
     else
-	echo "$crate: CHANGED"
+	echo "$crate"
     fi
 done
