feab848509a25c4865ccb41e990a010ba33eaa44
Nick Mathewson <nickm@torproject.org>
2022-02-01 14:21:58 -0500

    arti_config: Refactor configuration sources into a struct
    This is by no means our final API, but should represent an
    improvement.  Here instead of having to specify a list of files and
    their is-this-optional status, along with a list of command-line
    options, we have a single structure that encapsulates all of that
    information.
    
    Two advantages here:
    
     - Callers no longer have to remember what the boolean means.
     - We can "reload" more easily, by keeping the source object around.
    
    This change also implements the correct behavior for our default
    configuration file in `arti::main`: if the file is absent and the
    user doesn't list a config file, that's no problem.  But if the user
    lists _that very same config file, we should insist that it be
    present.

--- crates/arti-config/src/lib.rs
+++ crates/arti-config/src/lib.rs
@@ -52,42 +52,89 @@ use tor_config::CfgPath;
 
 use std::path::{Path, PathBuf};
 
-/// Load configuration into a a [`config::Config`] object from configuration files on disk (`files`),
-/// and/or command-line arguments (`opts`).
-///
-/// `files` should be a list of TOML config file paths to read, with the boolean specifying whether
-/// a failure to read this file should be an error or not.
-///
-/// `opts` are passed into a [`CmdLine`], and use the extended syntax of that mechanism.
-pub fn load<P: AsRef<Path>>(
-    files: &[(P, bool)],
-    opts: Vec<String>,
-) -> Result<config::Config, config::ConfigError> {
-    let mut config = config::Config::new();
-    config.merge(config::File::from_str(
-        options::ARTI_DEFAULTS,
-        config::FileFormat::Toml,
-    ))?;
-    load_mut(&mut config, files, opts)?;
-    Ok(config)
+/// A description of where to find our configuration options.
+#[derive(Clone, Debug, Default)]
+pub struct ConfigurationSources {
+    /// List of files to read (in order).
+    files: Vec<(PathBuf, MustRead)>,
+    /// A list of command-line options to apply after parsing the files.
+    options: Vec<String>,
 }
 
+/// Rules for whether we should proceed if a configuration file is unreadable.
+///
+/// Some files (like the default configuration file) are okay to skip if they
+/// aren't present. Others (like those specified on the command line) really
+/// need to be there.
+#[derive(Clone, Debug, Copy, Eq, PartialEq)]
+enum MustRead {
+    /// This file is okay to skip if it isn't present,
+    TolerateAbsence,
+
+    /// This file must be present and readable.
+    MustRead,
+}
+
+impl ConfigurationSources {
+    /// Create a new empty [`ConfigurationSources`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+    /// Add `p` to the list of files that we want to read configuration from.
+    ///
+    /// Configuration files are loaded and applied in the order that they are
+    /// added to this object.
+    ///
+    /// If the listed file is absent, loading the configuration won't succeed.
+    pub fn push_file<P: AsRef<Path>>(&mut self, p: P) {
+        self.files.push((p.as_ref().to_owned(), MustRead::MustRead));
+    }
+    /// As `push_file`, but if the listed file can't be loaded, loading the
+    /// configuration can still succeed.
+    pub fn push_optional_file<P: AsRef<Path>>(&mut self, p: P) {
+        self.files
+            .push((p.as_ref().to_owned(), MustRead::TolerateAbsence));
+    }
+    /// Add `s` to the list of overridden options to apply to our configuration.
+    ///
+    /// Options are applied after all configuration files are loaded, in the
+    /// order that they are added to this object.
+    ///
+    /// The format for `s` is as in [`CmdLine`].
+    pub fn push_option<S: ToOwned<Owned = String> + ?Sized>(&mut self, option: &S) {
+        self.options.push(option.to_owned());
+    }
+
+    /// Load the configuration into a new [`config::Config`].
+    pub fn load(&self) -> Result<config::Config, config::ConfigError> {
+        let mut config = config::Config::new();
+        config.merge(config::File::from_str(
+            options::ARTI_DEFAULTS,
+            config::FileFormat::Toml,
+        ))?;
+        load_mut(&mut config, &self.files, &self.options)?;
+        Ok(config)
+    }
+}
 /// As [`load()`], but load into a mutable `Config` object.
 fn load_mut<P: AsRef<Path>>(
     cfg: &mut config::Config,
-    files: &[(P, bool)],
-    opts: Vec<String>,
+    files: &[(P, MustRead)],
+    opts: &[String],
 ) -> Result<(), config::ConfigError> {
-    for (path, required) in files {
+    for (path, must_read) in files {
         // Not going to use File::with_name here, since it doesn't
         // quite do what we want.
         let f: config::File<_> = path.as_ref().into();
-        cfg.merge(f.format(config::FileFormat::Toml).required(*required))?;
+        let required = must_read == &MustRead::MustRead;
+        cfg.merge(f.format(config::FileFormat::Toml).required(required))?;
     }
 
     let mut cmdline = CmdLine::new();
     for opt in opts {
-        cmdline.push_toml_line(opt);
+        // TODO: This cloning is infelicitous, but probably won't matter in
+        // practice.
+        cmdline.push_toml_line(opt.clone());
     }
     cfg.merge(cmdline)?;
 
@@ -115,7 +162,7 @@ friends = 4242
     fn non_required_file() {
         let td = tempdir().unwrap();
         let dflt = td.path().join("a_file");
-        let files = vec![(dflt, false)];
+        let files = vec![(dflt, MustRead::TolerateAbsence)];
         let mut c = config::Config::new();
         load_mut(&mut c, &files, Default::default()).unwrap();
     }
@@ -132,7 +179,7 @@ world = \"nonsense\"
         let cf = td.path().join("other_file");
         let mut c = config::Config::new();
         std::fs::write(&cf, EX2_TOML).unwrap();
-        let files = vec![(dflt, false), (cf, true)];
+        let files = vec![(dflt, MustRead::TolerateAbsence), (cf, MustRead::MustRead)];
         load_mut(&mut c, &files, Default::default()).unwrap();
 
         assert!(c.get_str("hello.friends").is_err());
@@ -147,9 +194,9 @@ world = \"nonsense\"
         let mut c = config::Config::new();
         std::fs::write(&cf1, EX_TOML).unwrap();
         std::fs::write(&cf2, EX2_TOML).unwrap();
-        let v = vec![(cf1, true), (cf2, true)];
+        let v = vec![(cf1, MustRead::TolerateAbsence), (cf2, MustRead::MustRead)];
         let v2 = vec!["other.var=present".to_string()];
-        load_mut(&mut c, &v, v2).unwrap();
+        load_mut(&mut c, &v, &v2).unwrap();
 
         assert_eq!(c.get_str("hello.friends").unwrap(), "4242".to_string());
         assert_eq!(c.get_str("hello.world").unwrap(), "nonsense".to_string());
