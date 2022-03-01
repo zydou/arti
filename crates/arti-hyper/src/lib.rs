4fa78e3f440fc5a01eef11b78318755d6cb6a7bb
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-03-01 11:08:52 +0000

    arti-hyper: Upgrade from an example to an "adaptation layer"

--- crates/arti-hyper/src/lib.rs
+++ crates/arti-hyper/src/lib.rs
@@ -1,4 +1,12 @@
 //! High-level layer for making http(s) requests the Tor network as a client.
+//!
+//! This can be used by applications which embed Arti,
+//! and could also be used as an example of how to build on top of [`arti_client`].
+//!
+//! There is an example program [`hyper.rs`] which uses `arti-hyper`
+//! to connect to Tor and make a single HTTP\[S] request.
+//!
+//! [`hyper.rs`]: <https://gitlab.torproject.org/tpo/core/arti/-/blob/main/crates/arti-hyper/examples/hyper.rs>
 
 #![deny(missing_docs)]
 #![warn(noop_method_call)]
