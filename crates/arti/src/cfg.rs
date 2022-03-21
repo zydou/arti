85cf744e9e9dfbd657507d7986ef70651e89a056
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-03-21 12:38:10 +0000

    Move ApplicationConfig to arti crate
    Code motion and import fixups.

--- crates/arti/src/cfg.rs
+++ crates/arti/src/cfg.rs
@@ -9,10 +9,34 @@ use serde::Deserialize;
 
 use arti_client::config::{SystemConfig, SystemConfigBuilder, TorClientConfigBuilder};
 use arti_client::TorClientConfig;
-use arti_config::{ApplicationConfig, ApplicationConfigBuilder};
 use tor_config::ConfigBuildError;
 
 use crate::{LoggingConfig, LoggingConfigBuilder};
+
+/// Structure to hold our application configuration options
+#[derive(Deserialize, Debug, Default, Clone, Builder, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+#[builder(build_fn(error = "ConfigBuildError"))]
+#[builder(derive(Deserialize))]
+pub struct ApplicationConfig {
+    /// If true, we should watch our configuration files for changes, and reload
+    /// our configuration when they change.
+    ///
+    /// Note that this feature may behave in unexpected ways if the path to the
+    /// directory holding our configuration files changes its identity (because
+    /// an intermediate symlink is changed, because the directory is removed and
+    /// recreated, or for some other reason).
+    #[serde(default)]
+    #[builder(default)]
+    watch_configuration: bool,
+}
+
+impl ApplicationConfig {
+    /// Return true if we're configured to watch for configuration changes.
+    pub fn watch_configuration(&self) -> bool {
+        self.watch_configuration
+    }
+}
 
 /// Configuration for one or more proxy listeners.
 #[derive(Deserialize, Debug, Clone, Builder, Eq, PartialEq)]
