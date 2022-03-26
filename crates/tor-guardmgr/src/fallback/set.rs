b88789d1dd3e550eed7362119dae4440960bab4e
Nick Mathewson <nickm@torproject.org>
2022-03-26 11:08:46 -0400

    guardmgr::fallback::set: basic unit tests.

--- crates/tor-guardmgr/src/fallback/set.rs
+++ crates/tor-guardmgr/src/fallback/set.rs
@@ -1,7 +1,7 @@
 //! Declare the [`FallbackSet`] type, which is used to store a set of FallbackDir.
 
 use rand::seq::IteratorRandom;
-use std::{iter::FromIterator, time::Instant};
+use std::time::Instant;
 
 use super::{FallbackDir, Status};
 use crate::{GuardId, PickGuardError};
@@ -17,14 +17,6 @@ use serde::Deserialize;
 pub struct FallbackList {
     /// The underlying fallbacks in this set.
     fallbacks: Vec<FallbackDir>,
-}
-
-impl FromIterator<FallbackDir> for FallbackList {
-    fn from_iter<T: IntoIterator<Item = FallbackDir>>(iter: T) -> Self {
-        FallbackList {
-            fallbacks: iter.into_iter().collect(),
-        }
-    }
 }
 
 impl<T: IntoIterator<Item = FallbackDir>> From<T> for FallbackList {
@@ -245,9 +237,10 @@ where
 #[cfg(test)]
 mod test {
     #![allow(clippy::unwrap_used)]
+    use super::*;
 
     #[test]
-    fn matching_items() {
+    fn test_matching_items() {
         let odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
         let primes = ["2", "3", "5", "7", "11", "13", "17", "19"];
 
@@ -269,5 +262,192 @@ mod test {
                 (19, "19")
             ]
         );
+    }
+
+    /// Construct a `FallbackDir` with random identity keys and addresses.
+    ///
+    /// Since there are 416 bits of random id here, the risk of collision is
+    /// negligible.
+    fn rand_fb() -> FallbackDir {
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+        let ed: [u8; 32] = rng.gen();
+        let rsa: [u8; 20] = rng.gen();
+        let ip: u32 = rng.gen();
+        FallbackDir::builder()
+            .ed_identity(ed.into())
+            .rsa_identity(rsa.into())
+            .orport(std::net::SocketAddrV4::new(ip.into(), 9090).into())
+            .build()
+            .unwrap()
+    }
+
+    #[test]
+    fn construct_fallback_set() {
+        use rand::seq::SliceRandom;
+
+        // fabricate some fallbacks.
+        let fbs = vec![rand_fb(), rand_fb(), rand_fb(), rand_fb()];
+        let fb_other = rand_fb();
+        let id_other = GuardId::from_chan_target(&fb_other);
+
+        // basic case: construct a set
+        let list: FallbackList = fbs.clone().into();
+        assert!(!list.is_empty());
+        assert_eq!(list.len(), 4);
+        let mut set: FallbackSet = list.clone().into();
+
+        // inspect the generated set
+        assert_eq!(set.fallbacks.len(), 4);
+        assert!(set.fallbacks[0].id() < set.fallbacks[1].id());
+        assert!(set.fallbacks[1].id() < set.fallbacks[2].id());
+        assert!(set.fallbacks[2].id() < set.fallbacks[3].id());
+
+        // use the constructed set a little.
+        for fb in fbs.iter() {
+            let id = GuardId::from_chan_target(fb);
+            assert_eq!(set.lookup_mut(&id).unwrap().id(), &id);
+        }
+        assert!(set.lookup_mut(&id_other).is_none());
+
+        // Now try an input set with duplicates.
+        let mut redundant_fbs = fbs.clone();
+        redundant_fbs.extend(fbs.clone());
+        redundant_fbs.extend(fbs[0..2].iter().map(Clone::clone));
+        redundant_fbs[..].shuffle(&mut rand::thread_rng());
+        let list2 = redundant_fbs.into();
+        assert_ne!(&list, &list2);
+        let set2: FallbackSet = list2.into();
+
+        // It should have the same elements, in the same order.
+        assert_eq!(set.fallbacks.len(), set2.fallbacks.len());
+        assert!(set
+            .fallbacks
+            .iter()
+            .zip(set2.fallbacks.iter())
+            .all(|(ent1, ent2)| ent1.id() == ent2.id()));
+    }
+
+    #[test]
+    fn set_choose() {
+        let fbs = vec![rand_fb(), rand_fb(), rand_fb(), rand_fb()];
+        let list: FallbackList = fbs.into();
+        let mut set: FallbackSet = list.into();
+
+        let mut counts = [0_usize; 4];
+        let mut rng = rand::thread_rng();
+        let now = Instant::now();
+
+        fn lookup_idx(set: &FallbackSet, id: &GuardId) -> Option<usize> {
+            set.fallbacks.binary_search_by(|ent| ent.id().cmp(id)).ok()
+        }
+        // Basic case: everybody is up.
+        for _ in 0..100 {
+            let fb = set.choose(&mut rng, now).unwrap();
+            let idx = lookup_idx(&set, fb.id()).unwrap();
+            counts[idx] += 1;
+        }
+        assert!(counts.iter().all(|v| *v > 0));
+
+        // Mark somebody down and make sure they don't get chosen.
+        let ids: Vec<_> = set.fallbacks.iter().map(|fb| fb.id().clone()).collect();
+        set.note_failure(&ids[2], now);
+        counts = [0; 4];
+        for _ in 0..100 {
+            let fb = set.choose(&mut rng, now).unwrap();
+            let idx = lookup_idx(&set, fb.id()).unwrap();
+            counts[idx] += 1;
+        }
+        assert_eq!(counts.iter().filter(|v| **v > 0).count(), 3);
+        assert_eq!(counts[2], 0);
+
+        // Mark everybody down; make sure we get the right error.
+        for id in ids.iter() {
+            set.note_failure(id, now);
+        }
+        assert!(matches!(
+            set.choose(&mut rng, now),
+            Err(PickGuardError::AllFallbacksDown { .. })
+        ));
+
+        // Construct an empty set; make sure we get the right error.
+        let empty_set = FallbackSet::from(FallbackList::from(vec![]));
+        assert!(matches!(
+            empty_set.choose(&mut rng, now),
+            Err(PickGuardError::NoCandidatesAvailable)
+        ));
+
+        // TODO: test restrictions and filters once they're implemented.
+    }
+
+    #[test]
+    fn test_status() {
+        let fbs = vec![rand_fb(), rand_fb(), rand_fb(), rand_fb()];
+        let list: FallbackList = fbs.clone().into();
+        let mut set: FallbackSet = list.into();
+        let ids: Vec<_> = set.fallbacks.iter().map(|fb| fb.id().clone()).collect();
+
+        let now = Instant::now();
+
+        // There's no "next retry time" when everybody's up.
+        assert!(set.next_retry().is_none());
+
+        // Mark somebody down; try accessors.
+        set.note_failure(&ids[3], now);
+        assert!(set.fallbacks[3].status.next_retriable().unwrap() > now);
+        assert!(!set.fallbacks[3].status.usable_at(now));
+        assert_eq!(set.next_retry(), set.fallbacks[3].status.next_retriable());
+
+        // Mark somebody else down; try accessors.
+        set.note_failure(&ids[0], now);
+        assert!(set.fallbacks[0].status.next_retriable().unwrap() > now);
+        assert!(!set.fallbacks[0].status.usable_at(now));
+        assert_eq!(
+            set.next_retry().unwrap(),
+            std::cmp::min(
+                set.fallbacks[0].status.next_retriable().unwrap(),
+                set.fallbacks[3].status.next_retriable().unwrap()
+            )
+        );
+
+        // Mark somebody as running; try accessors.
+        set.note_success(&ids[0]);
+        assert!(set.fallbacks[0].status.next_retriable().is_none());
+        assert!(set.fallbacks[0].status.usable_at(now));
+
+        assert!(set.lookup_mut(&ids[0]).unwrap().status.usable_at(now));
+
+        for id in ids.iter() {
+            dbg!(id, set.lookup_mut(id).map(|e| e.id()));
+        }
+
+        // Make a new set with slightly different members; make sure that we can copy stuff successfully.
+        let mut fbs2: Vec<_> = fbs
+            .into_iter()
+            // (Remove the fallback with id==ids[2])
+            .filter(|fb| GuardId::from_chan_target(fb) != ids[2])
+            .collect();
+        // add 2 new ones.
+        let fbs_new = [rand_fb(), rand_fb(), rand_fb()];
+        fbs2.extend(fbs_new.clone());
+
+        let mut set2 = FallbackSet::from(FallbackList::from(fbs2.clone()));
+        set2.take_status_from(set); // consumes set.
+        assert_eq!(set2.fallbacks.len(), 6); // Started with 4, added 3, removed 1.
+
+        // Make sure that the status entries  are correctly copied.
+        assert!(set2.lookup_mut(&ids[0]).unwrap().status.usable_at(now));
+        assert!(set2.lookup_mut(&ids[1]).unwrap().status.usable_at(now));
+        assert!(set2.lookup_mut(&ids[2]).is_none());
+        assert!(!set2.lookup_mut(&ids[3]).unwrap().status.usable_at(now));
+
+        // Make sure that the new fbs are there.
+        for new_fb in fbs_new {
+            assert!(set2
+                .lookup_mut(&GuardId::from_chan_target(&new_fb))
+                .unwrap()
+                .status
+                .usable_at(now));
+        }
     }
 }
