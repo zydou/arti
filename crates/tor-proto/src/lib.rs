0cc94d449ed5853838221a882f66c7838a36ee3f
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-05-11 15:30:28 +0100

    clippy: Change a binding to (), and allow clippy::let_unit_value
    The type of ret.map_err(codec_err_to_chan)? is ().  ISTM that
    writing `let () = ` makes it clear that there is nothing there,
    but the lint forbids this.
    
    This lint is warn by default and trips here for me on current nightly.
    
    It seems wrong to me.  We should be able to make it clear to the
    reader that there is nothing here - note how this differs from the
    lines below where Ready contains msg.  A let () binding is a good way
    to do that.
    
    I think the lint allow ought to be added everywhere, but that doesn't
    seem easy right now - see this issue about maint/add_warning:
       https://gitlab.torproject.org/tpo/core/arti/-/issues/469

--- crates/tor-proto/src/lib.rs
+++ crates/tor-proto/src/lib.rs
@@ -111,6 +111,7 @@
 #![deny(clippy::unnecessary_wraps)]
 #![warn(clippy::unseparated_literal_suffix)]
 #![deny(clippy::unwrap_used)]
+#![allow(clippy::let_unit_value)] // This can reasonably be done for explicitness
 
 pub mod channel;
 pub mod circuit;
