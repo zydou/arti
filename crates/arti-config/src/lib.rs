4e2bd6c7983dfa1adab59180e034c68bbe167daf
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-05-13 11:42:51 +0100

    Abolish arti-config, replacing with tombstone crate

--- crates/arti-config/src/lib.rs
+++ crates/arti-config/src/lib.rs
@@ -1,50 +1,8 @@
-//! `arti-config`: Tools for configuration management in Arti
+//! `arti-config`: Removed crate.  (Tools for configuration management in Arti)
 //!
-//! # Overview
-//!
-//! This crate is part of
+//! This crate was part of
 //! [Arti](https://gitlab.torproject.org/tpo/core/arti/), a project to
 //! implement [Tor](https://www.torproject.org/) in Rust.
 //!
-//! It provides a client configuration tool using `serde` and `config`,
-//! plus extra features defined here for convenience.
-//!
-//! # ⚠ Stability Warning ⚠
-//!
-//! The design of this crate, and of the configuration system for
-//! Arti, is likely to change significantly before the release of Arti
-//! 1.0.0.  For more information see ticket [#285].
-//!
-//! [#285]: https://gitlab.torproject.org/tpo/core/arti/-/issues/285
-
-#![deny(missing_docs)]
-#![warn(noop_method_call)]
-#![deny(unreachable_pub)]
-#![warn(clippy::all)]
-#![deny(clippy::await_holding_lock)]
-#![deny(clippy::cargo_common_metadata)]
-#![deny(clippy::cast_lossless)]
-#![deny(clippy::checked_conversions)]
-#![warn(clippy::cognitive_complexity)]
-#![deny(clippy::debug_assert_with_mut_call)]
-#![deny(clippy::exhaustive_enums)]
-#![deny(clippy::exhaustive_structs)]
-#![deny(clippy::expl_impl_clone_on_copy)]
-#![deny(clippy::fallible_impl_from)]
-#![deny(clippy::implicit_clone)]
-#![deny(clippy::large_stack_arrays)]
-#![warn(clippy::manual_ok_or)]
-#![deny(clippy::missing_docs_in_private_items)]
-#![deny(clippy::missing_panics_doc)]
-#![warn(clippy::needless_borrow)]
-#![warn(clippy::needless_pass_by_value)]
-#![warn(clippy::option_option)]
-#![warn(clippy::rc_buffer)]
-#![deny(clippy::ref_option_ref)]
-#![warn(clippy::semicolon_if_nothing_returned)]
-#![warn(clippy::trait_duplication_in_bounds)]
-#![deny(clippy::unnecessary_wraps)]
-#![warn(clippy::unseparated_literal_suffix)]
-#![deny(clippy::unwrap_used)]
-
-pub use tor_config::ConfigurationSources;
+//! The project continues, but this particular crate is now superseded.
+//! This empty crate is published as a tombstone.
