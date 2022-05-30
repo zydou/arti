de92a295fbd10d1f5fd003b4838c50859e1c4210
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-05-30 19:19:26 +0100

    lints: tor-protover: Move two allows outside the auto block
    These need to survive.

--- crates/tor-protover/src/lib.rs
+++ crates/tor-protover/src/lib.rs
@@ -35,7 +35,6 @@
 
 // @@ begin lint list maintained by maint/add_warning @@
 #![deny(missing_docs)]
-#![allow(non_upper_case_globals)]
 #![warn(noop_method_call)]
 #![deny(unreachable_pub)]
 #![warn(clippy::all)]
@@ -64,9 +63,11 @@
 #![deny(clippy::unnecessary_wraps)]
 #![warn(clippy::unseparated_literal_suffix)]
 #![deny(clippy::unwrap_used)]
-#![allow(clippy::upper_case_acronyms)]
 #![allow(clippy::let_unit_value)] // This can reasonably be done for explicitness
 //! <!-- @@ end lint list maintained by maint/add_warning @@ -->
+
+#![allow(non_upper_case_globals)]
+#![allow(clippy::upper_case_acronyms)]
 
 use caret::caret_int;
 
