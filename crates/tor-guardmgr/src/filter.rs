9c4f31edc13b51c12e5bf42372132bba347e257b
Nick Mathewson <nickm@torproject.org>
2022-06-01 10:00:49 -0400

    GuardMgr: Add a new filter type based on reachable addresses.

--- crates/tor-guardmgr/src/filter.rs
+++ crates/tor-guardmgr/src/filter.rs
@@ -1,6 +1,9 @@
 //! Implement GuardFilter and related types.
 
 use tor_linkspec::ChanTarget;
+// TODO(nickm): Conceivably, this type should be exposed from a lower-level crate than
+// tor-netdoc.
+use tor_netdoc::types::policy::AddrPortPattern;
 
 /// An object specifying which relays are eligible to be guards.
 ///
@@ -20,12 +23,21 @@ pub struct GuardFilter {
     /// A list of filters to apply to guard or fallback selection.  Each filter
     /// restricts which guards may be used, and possibly how those guards may be
     /// contacted.
+    ///
+    /// This list of filters has "and" semantics: a relay is permitted by this
+    /// filter if ALL patterns in this list permit that first hop.
     filters: Vec<SingleFilter>,
 }
 
 /// A single restriction places upon usable guards.
 #[derive(Debug, Clone)]
 enum SingleFilter {
+    /// A set of allowable addresses that we are willing to try to connect to.
+    ///
+    /// This list of patterns has "or" semantics: a guard is permitted by this filter
+    /// if ANY pattern in this list permits one of the guard's addresses.
+    ReachableAddrs(Vec<AddrPortPattern>),
+
     /// Testing only: checks whether the first byte of the rsa key is 0 modulo 4.
     ///
     /// TODO: remove this once real filters are implemented.
@@ -39,6 +51,13 @@ impl GuardFilter {
     /// permissible guards at all.
     pub fn unfiltered() -> Self {
         GuardFilter::default()
+    }
+
+    /// Restrict this filter to only permit connections to an address permitted
+    /// by one of the patterns in `addrs`.
+    pub fn push_reachable_addresses(&mut self, addrs: impl IntoIterator<Item = AddrPortPattern>) {
+        self.filters
+            .push(SingleFilter::ReachableAddrs(addrs.into_iter().collect()));
     }
 
     /// Return true if this filter permits the provided `target`.
@@ -64,16 +83,12 @@ impl GuardFilter {
 impl SingleFilter {
     /// Return true if this filter permits the provided target.
     fn permits<C: ChanTarget>(&self, target: &C) -> bool {
-        // TODO: This is ugly, but the whole function will get rewritten once we
-        // have a real filter.
-        #[cfg(test)]
         match self {
+            SingleFilter::ReachableAddrs(patterns) => patterns
+                .iter()
+                .any(|pat| target.addrs().iter().any(|addr| pat.matches_sockaddr(addr))),
+            #[cfg(test)]
             SingleFilter::TestingLimitKeys => target.rsa_identity().as_bytes()[0] & 3 == 0,
-        }
-        #[cfg(not(test))]
-        {
-            let _ = target;
-            true
         }
     }
 }
