76403563e58a9462521a9a45abba456b29d440a7
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-07-25 14:54:03 +0100

    tor-cell, testing: Provide PaddingNegotiate::from_raw
    This allows test cases to describe precisely the contents of the
    negotiation cell ought to be generated.

--- crates/tor-cell/src/chancell/msg.rs
+++ crates/tor-cell/src/chancell/msg.rs
@@ -861,6 +861,14 @@ impl PaddingNegotiate {
             ito_high_ms: 0,
         }
     }
+
+    /// Construct from the three fields: command, low_ms, high_ms, as a tupe
+    ///
+    /// For testing only
+    #[cfg(feature = "testing")]
+    pub fn from_raw(command: PaddingNegotiateCmd, ito_low_ms: u16, ito_high_ms: u16) -> Self {
+        PaddingNegotiate { command, ito_low_ms, ito_high_ms, }
+    }
 }
 impl Default for PaddingNegotiate {
     fn default() -> Self {
