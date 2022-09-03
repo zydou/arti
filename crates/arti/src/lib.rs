5e41e7dad408bbc35e2f4dddab950e54de9f67f7
trinity-1686a <trinity@deuxfleurs.fr>
2022-09-03 11:29:26 +0200

    document TOR_SKIP_CONTROLPORTTEST=1 for no warning on Tor Browser

--- crates/arti/src/lib.rs
+++ crates/arti/src/lib.rs
@@ -72,13 +72,13 @@
 //! ### Linux
 //!
 //! ```text
-//! $ TOR_SKIP_LAUNCH=1 TOR_SOCKS_PORT=9150 ./start-tor-browser.desktop
+//! $ TOR_SKIP_LAUNCH=1 TOR_SOCKS_PORT=9150 TOR_SKIP_CONTROLPORTTEST=1 ./start-tor-browser.desktop
 //! ```
 //!
 //! ### OS X
 //!
 //! ```text
-//! $ TOR_SKIP_LAUNCH=1 TOR_SOCKS_PORT=9150 /path/to/Tor\ Browser/Contents/MacOS/firefox
+//! $ TOR_SKIP_LAUNCH=1 TOR_SOCKS_PORT=9150 TOR_SKIP_CONTROLPORTTEST=1 /path/to/Tor\ Browser/Contents/MacOS/firefox
 //! ```
 //!
 //! ### Windows
@@ -86,7 +86,7 @@
 //! Create a shortcut with the `Target` set to:
 //!
 //! ```text
-//! C:\Windows\System32\cmd.exe /c "SET TOR_SKIP_LAUNCH=1&& SET TOR_SOCKS_PORT=9150&& START /D ^"C:\path\to\Tor Browser\Browser^" firefox.exe"
+//! C:\Windows\System32\cmd.exe /c "SET TOR_SKIP_LAUNCH=1&& SET TOR_SOCKS_PORT=9150&& TOR_SKIP_CONTROLPORTTEST=1&& START /D ^"C:\path\to\Tor Browser\Browser^" firefox.exe"
 //! ```
 //!     
 //! and `Start in` set to:
@@ -98,13 +98,11 @@
 //! (You may need to adjust the actual path to wherever you have put your Tor
 //! Browser.)
 //!
-//! When you start Tor browser, it will give you a big red error page because
-//! Arti isn't offering it a control port interface.  But it will still work!
-//! Try [check.torproject.org](https://check.torproject.org/) to be sure.
-//!
 //! The resulting Tor Browser should be using arti.  Note that onion services
-//! won't work (Arti doesn't have them yet), and neither will any feature
-//! depending on Tor's control-port protocol.
+//! and bridges won't work (Arti doesn't support them yet), and neither will
+//! any feature depending on Tor's control-port protocol. Features not depending
+//! on the control-port such as the "New circuit for this site" button should
+//! work.
 //!
 //! # Compile-time features
 //!
