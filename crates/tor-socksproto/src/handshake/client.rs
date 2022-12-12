ec7720ae917ba2ca0e6af088a487d5914d4398bc
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-12-12 17:43:29 +0000

    test lint blocks: Standardise one in tor-socksproto
    The previous syntax was recognised by the real script, but not by the
    ad-hoc script I'm about to use to add all the missing ones.

--- crates/tor-socksproto/src/handshake/client.rs
+++ crates/tor-socksproto/src/handshake/client.rs
@@ -304,7 +304,7 @@ impl SocksClientHandshake {
 
 #[cfg(test)]
 mod test {
-    // @@ begin test lint list
+    // @@ begin test lint list maintained by maint/add_warning @@
     #![allow(clippy::bool_assert_comparison)]
     #![allow(clippy::clone_on_copy)]
     #![allow(clippy::dbg_macro)]
@@ -312,7 +312,7 @@ mod test {
     #![allow(clippy::print_stdout)]
     #![allow(clippy::single_char_pattern)]
     #![allow(clippy::unwrap_used)]
-    //! <!-- @@ end test lint list
+    //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
 
     use super::*;
     use crate::msg::SocksCmd;
