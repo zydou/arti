a5be14ed5e33ece6e26080cc6118c2e1ac9d062d
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-02-09 16:36:44 +0000

    tor-netdoc: util: Add standard lint block to tests
    Not xure how my MR to try to do this everyone missed this one.

--- crates/tor-netdoc/src/util.rs
+++ crates/tor-netdoc/src/util.rs
@@ -101,6 +101,16 @@ pub(crate) mod private {
 
 #[cfg(test)]
 mod tests {
+    // @@ begin test lint list maintained by maint/add_warning @@
+    #![allow(clippy::bool_assert_comparison)]
+    #![allow(clippy::clone_on_copy)]
+    #![allow(clippy::dbg_macro)]
+    #![allow(clippy::print_stderr)]
+    #![allow(clippy::print_stdout)]
+    #![allow(clippy::single_char_pattern)]
+    #![allow(clippy::unwrap_used)]
+    #![allow(clippy::unchecked_duration_subtraction)]
+    //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
 
     #[test]
     fn test_pause_at() {
