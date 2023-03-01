53e44b58f5fa0cfa57073618d18bd71a1632afff
Nick Mathewson <nickm@torproject.org>
2023-03-01 07:29:07 -0500

    tor-cell: Fix compilation for fuzzers
    (I also ran them a couple billion iterations, and didn't hit
    any bugs.)

--- crates/tor-cell/fuzz/fuzz_targets/chanmsg.rs
+++ crates/tor-cell/fuzz/fuzz_targets/chanmsg.rs
@@ -1,8 +1,9 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 use tor_cell::chancell::codec::ChannelCodec;
+use tor_cell::chancell::msg::AnyChanMsg;
 
 fuzz_target!(|data: &[u8]| {
     let mut bytes = data.into();
-    let _ = ChannelCodec::new(4).decode_cell(&mut bytes);
+    let _ = ChannelCodec::new(4).decode_cell::<AnyChanMsg>(&mut bytes);
 });
