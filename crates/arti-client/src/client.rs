b6f5c066b4bca59aa24ac7827d5ca4255d375fcd
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-03-23 11:25:57 +0000

    Apply deferred rustfmt churn

--- crates/arti-client/src/client.rs
+++ crates/arti-client/src/client.rs
@@ -920,7 +920,10 @@ impl<R: Runtime> TorClient<R> {
                         self.isolation(prefs),
                     )
                     .await
-                    .map_err(|cause| ErrorDetail::ObtainHsCircuit { cause, hsid: hsid.into() })?;
+                    .map_err(|cause| ErrorDetail::ObtainHsCircuit {
+                        cause,
+                        hsid: hsid.into(),
+                    })?;
                 (circ, hostname, port)
             }
         };
