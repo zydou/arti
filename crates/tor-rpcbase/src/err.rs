2e017d35753c484e1310fb9c7298f58cb3e7d9f4
Nick Mathewson <nickm@torproject.org>
2023-04-12 12:27:16 -0400

    tor-rpcbase: Create error kinds properly.
    The field is called "kinds", it is a list, and it holds strings
    beginning with "arti:".

--- crates/tor-rpcbase/src/err.rs
+++ crates/tor-rpcbase/src/err.rs
@@ -7,9 +7,9 @@ pub struct RpcError {
     message: String,
     /// An error code inspired by json-rpc.
     code: i32,
-    /// The ErrorKind of this error.
+    /// The ErrorKind(s) of this error.
     #[serde(serialize_with = "ser_kind")]
-    kind: tor_error::ErrorKind,
+    kinds: tor_error::ErrorKind,
     /// An underlying serializable object, if any, to be sent along with the
     /// error.
     data: Option<Box<dyn erased_serde::Serialize + Send>>,
@@ -22,13 +22,13 @@ where
     fn from(value: T) -> Self {
         let message = value.to_string();
         let code = -12345; // TODO RPC: this is wrong.
-        let kind = value.kind();
+        let kinds = value.kind();
         let boxed: Box<dyn erased_serde::Serialize + Send> = Box::new(value);
         let data = Some(boxed);
         RpcError {
             message,
             code,
-            kind,
+            kinds,
             data,
         }
     }
@@ -38,8 +38,8 @@ impl From<crate::dispatch::InvokeError> for crate::RpcError {
     fn from(_value: crate::dispatch::InvokeError) -> Self {
         crate::RpcError {
             message: "Tried to invoke unsupported method on object".to_string(),
-            code: -23456,                               // TODO RPC wrong.
-            kind: tor_error::ErrorKind::NotImplemented, // TODO RPC wrong
+            code: -23456,                                // TODO RPC wrong.
+            kinds: tor_error::ErrorKind::NotImplemented, // TODO RPC wrong
             data: None,
         }
     }
@@ -49,20 +49,22 @@ impl From<crate::LookupError> for crate::RpcError {
     fn from(value: crate::LookupError) -> Self {
         crate::RpcError {
             message: value.to_string(),
-            code: -3001,                       // TODO RPC wrong.
-            kind: tor_error::ErrorKind::Other, // TODO RPC wrong.
+            code: -3001,                        // TODO RPC wrong.
+            kinds: tor_error::ErrorKind::Other, // TODO RPC wrong.
             data: None,
         }
     }
 }
 
 /// Helper: Serialize an ErrorKind in RpcError.
-///
-/// TODO RPC: This function is bogus and should probably get replaced when we
-/// have more of a handle on our error format.
 fn ser_kind<S: serde::Serializer>(kind: &tor_error::ErrorKind, s: S) -> Result<S::Ok, S::Error> {
-    // TODO RPC: this format is wrong and temporary.
-    s.serialize_str(&format!("{:?}", kind))
+    // Our spec says that `kinds` is a list, and that each kind we
+    // define will be prefixed by arti:.
+
+    use serde::ser::SerializeSeq;
+    let mut seq = s.serialize_seq(Some(1))?;
+    seq.serialize_element(&format!("arti:{:?}", kind))?;
+    seq.end()
 }
 
 impl std::fmt::Debug for RpcError {
@@ -70,7 +72,7 @@ impl std::fmt::Debug for RpcError {
         f.debug_struct("RpcError")
             .field("message", &self.message)
             .field("code", &self.code)
-            .field("kind", &self.kind)
+            .field("kinds", &self.kinds)
             .field("data", &self.data.as_ref().map(|_| "..."))
             .finish()
     }
