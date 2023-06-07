4632d4ff443d53e8224eceb1e55084990697e0dd
trinity-1686a <trinity@deuxfleurs.fr>
2023-06-07 23:43:28 +0200

    move snowflake example to dedicated file and comment it more

--- crates/arti-client/src/config.rs
+++ crates/arti-client/src/config.rs
@@ -244,44 +244,22 @@ impl StorageConfig {
 ///     );
 /// bridge2_builder.set_addrs(vec!["198.51.100.25:443".parse()?]);
 /// bridge2_builder.set_ids(vec!["7DD62766BF2052432051D7B7E08A22F7E34A4543".parse()?]);
-/// // Now nsert the second bridge into our config builder.
+/// // Now insert the second bridge into our config builder.
 /// builder.bridges().bridges().push(bridge2_builder);
 ///
 /// // Now configure an obfs4 transport. (Requires the "pt-client" feature)
-/// let mut obfs4_transport = ManagedTransportConfigBuilder::default();
-/// obfs4_transport
+/// let mut transport = ManagedTransportConfigBuilder::default();
+/// transport
 ///     .protocols(vec!["obfs4".parse()?])
 ///     .path(CfgPath::new("/usr/bin/obfs4proxy".into()))
 ///     .run_on_startup(true);
-/// builder.bridges().transports().push(obfs4_transport);
-///
-/// // Also configure snowflake
-/// let mut snowflake_bridge_builder = BridgeConfigBuilder::default();
-/// snowflake_bridge_builder.transport("snowflake");
-/// // address is a bit meaningless for snowflake
-/// snowflake_bridge_builder.set_addrs(vec!["192.0.2.3:80".parse()?]);
-/// snowflake_bridge_builder.set_ids(vec!["2B280B23E1107BB62ABFC40DDCC8824814F80A72".parse()?]);
-/// builder.bridges().bridges().push(snowflake_bridge_builder);
-///
-/// let mut snowflake_transport = ManagedTransportConfigBuilder::default();
-/// snowflake_transport
-///     .protocols(vec!["snowflake".parse()?])
-///     .path(CfgPath::new("/usr/bin/snowflake-client".into()))
-///     .arguments(vec![
-///         "-url".to_string(),
-///         "https://snowflake-broker.torproject.net.global.prod.fastly.net/".to_string(),
-///         "-front".to_string(),
-///         "cdn.sstatic.net".to_string(),
-///         "-ice".to_string(),
-///         "stun:stun.voip.blackberry.com:3478,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.sonetel.net:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478".to_string(),
-///     ])
-///     .run_on_startup(true);
-/// builder.bridges().transports().push(snowflake_transport);
+/// builder.bridges().transports().push(transport);
 ///
 /// let config = builder.build()?;
 /// // Now you can pass `config` to TorClient::create!
 /// # Ok(())}
 /// ```
+/// You can also find an example based on snowflake in arti-client example folder.
 //
 // We leave this as an empty struct even when bridge support is disabled,
 // as otherwise the default config file would generate an unknown section warning.
