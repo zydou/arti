d8f7ce510c9ade9ffb5d9a7f9e4d7926a6193052
Nick Mathewson <nickm@torproject.org>
2023-06-14 13:48:23 -0400

    SharedRandVal: Note that it doesn't have to be Ct.

--- crates/tor-netdoc/src/doc/netstatus.rs
+++ crates/tor-netdoc/src/doc/netstatus.rs
@@ -299,8 +299,7 @@ pub struct SignatureGroup {
 #[derive(
     Debug, Clone, Copy, Eq, PartialEq, derive_more::From, derive_more::Into, derive_more::AsRef,
 )]
-// TODO hs: Use CtBytes for this.  I don't think it actually matters, but it
-// seems like a good idea.
+// (This doesn't need to use CtByteArray; we don't really need to compare these.)
 pub struct SharedRandVal([u8; 32]);
 
 /// A shared-random value produced by the directory authorities,
