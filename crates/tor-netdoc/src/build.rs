f32b8cfaa29f615dc8c3b358dc4f6f519f27e55b
Nick Mathewson <nickm@torproject.org>
2023-06-14 11:26:05 -0400

    netdoc::build: remove or narrow `allow`s.

--- crates/tor-netdoc/src/build.rs
+++ crates/tor-netdoc/src/build.rs
@@ -13,24 +13,14 @@
 //! It is the caller's responsibility to call `.item()` in the right order,
 //! with the right keywords and arguments.
 
-#![allow(unused_variables)] // TODO hs
-#![allow(unused_imports)] // TODO hs
-#![allow(dead_code)] // TODO hs
-#![allow(clippy::missing_docs_in_private_items)] // TODO hs
-#![allow(clippy::needless_pass_by_value)] // TODO hs
-
-use std::fmt::{self, Display, Write};
-use std::marker::PhantomData;
-use std::ops::Deref;
+use std::fmt::{Display, Write};
 use std::time::SystemTime;
 
 use base64ct::{Base64, Encoding};
 use humantime::format_rfc3339;
 use rand::{CryptoRng, RngCore};
 use tor_bytes::EncodeError;
-use tor_cert::Ed25519Cert;
-use tor_error::{internal, into_internal, Bug};
-use tor_llcrypto::pk::ed25519;
+use tor_error::{internal, Bug};
 
 use crate::parse::keyword::Keyword;
 use crate::parse::tokenize::tag_keywords_ok;
@@ -165,6 +155,7 @@ impl NetdocEncoder {
     /// In particular, `s` should end with a newline.
     /// No checks are performed.
     /// Incorrect use might lead to malformed documents, or later errors.
+    #[allow(dead_code)] // TODO: We should remove this if it never used.
     pub(crate) fn push_raw_string(&mut self, s: &dyn Display) {
         self.raw(s);
     }
@@ -283,6 +274,7 @@ impl<'n> ItemEncoder<'n> {
     /// separated by (single) spaces.
     /// This is not (properly) checked.
     /// Incorrect use might lead to malformed documents, or later errors.
+    #[allow(unused)] // TODO: We should eventually remove this if nothing starts to use it.
     pub(crate) fn args_raw_string(mut self, args: &dyn Display) -> Self {
         let args = args.to_string();
         if !args.is_empty() {
