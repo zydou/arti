9a05fbb10df113fb9a6e0cb66d108e3594d587e7
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-06-21 12:54:57 +0100

    tor-hsclient: Apply deferred clippy churn needed by fmt churn
    rustfmt *introduced* this clippy warning.

--- crates/tor-hsclient/src/state.rs
+++ crates/tor-hsclient/src/state.rs
@@ -346,7 +346,7 @@ fn obtain_circuit_or_continuation_info<D: MockableConnectorData>(
             match (got_error, stored) {
                 (Ok::<(), ConnError>(()), Ok::<(), Bug>(())) => {}
                 (Err(got_error), Ok(())) => {
-                    debug!("HS connection failure: {}: {}", hsid, got_error.report(),)
+                    debug!("HS connection failure: {}: {}", hsid, got_error.report(),);
                 }
                 (Ok(()), Err(bug)) => error!(
                     "internal error storing built HS circuit for {}: {}",
