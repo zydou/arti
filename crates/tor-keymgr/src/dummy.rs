297248cd9f3db22e213c22dd7cac19d5e9395aba
Gabriela Moldovan <gabi@torproject.org>
2023-06-21 10:57:26 +0100

    keymgr: Update outdated docs.

--- crates/tor-keymgr/src/dummy.rs
+++ crates/tor-keymgr/src/dummy.rs
@@ -1,20 +1,23 @@
+#![allow(clippy::unnecessary_wraps, clippy::extra_unused_type_parameters)]
+
 //! A dummy key manager implementation.
 //!
-//! If arti-client is compiled with the `keymgr` feature, [`TorClient`](crate::TorClient) will use
-//! the key manager implementation from the `tor-keymgr` crate.
+//! This key manager implementation is only used when the `keymgr` feature is disabled.
 //!
-//! If arti-client is compiled without the `keymgr` feature, then [`TorClient`](crate::TorClient)
-//! will use this no-op key manager implementation instead.
+//! The implementations from this module ignore their arguments. The unused arguments can't be
+//! removed, because the dummy implementations must have the same API as their fully-featured
+//! counterparts.
+
+use crate::{Error, Result};
 
 use fs_mistrust::Mistrust;
 use std::any::Any;
 use std::path::Path;
-use crate::{Error, Result};
 
 /// A dummy key manager implementation.
 ///
-/// This implementation has the same API as the key manager from the `tor-keymgr` crate, except all
-/// its operations are no-ops.
+/// This implementation has the same API as the key manager exposed when the `keymgr` feature is
+/// enabled, except all its read operations return `None` and all its write operations will fail.
 ///
 /// For operations that normally involve updating the state of the key manager and/or its
 /// underlying storage, such as `insert` or `remove`, this `KeyMgr` always returns an [`Error`].
@@ -32,10 +35,6 @@ pub struct KeyType;
 
 impl ArtiNativeKeyStore {
     /// Create a new [`ArtiNativeKeyStore`].
-    ///
-    /// The arguments of this constructor are ignored. They only exist because we need this
-    /// constructor to have the same API as the constructor of `ArtiNativeKeyStore` from the
-    /// `tor-keymgr` crate.
     #[allow(clippy::unnecessary_wraps)]
     pub fn from_path_and_mistrust(_: impl AsRef<Path>, _: &Mistrust) -> Result<Self> {
         Ok(Self)
@@ -46,9 +45,6 @@ impl KeyStore for ArtiNativeKeyStore {}
 
 impl KeyMgr {
     /// Create a new [`KeyMgr`].
-    ///
-    /// The argument of this constructor is ignored. It only exists because we need [`KeyMgr::new`]
-    /// to have the same API as the constructor of the key manager from the `tor-keymgr` crate.
     pub fn new(_: Vec<Box<dyn KeyStore>>) -> Self {
         Self
     }
@@ -56,9 +52,6 @@ impl KeyMgr {
     /// A dummy `get` implementation that always behaves like the requested key is not found.
     ///
     /// This function always returns `Ok(None)`.
-    //
-    // We need to allow these lints because this impl needs to mirror that of `tor_keymgr::KeyMgr`.
-    #[allow(clippy::unnecessary_wraps, clippy::extra_unused_type_parameters)]
     pub fn get<K>(&self, _: &dyn Any) -> Result<Option<K>> {
         Ok(None)
     }
@@ -66,9 +59,6 @@ impl KeyMgr {
     /// A dummy `insert` implementation that always fails.
     ///
     /// This function always returns [`Error`].
-    //
-    // We need to allow these lints because this impl needs to mirror that of `tor_keymgr::KeyMgr`.
-    #[allow(clippy::unnecessary_wraps)]
     pub fn insert<K>(&self, _: K, _: &dyn Any) -> Result<()> {
         Err(Error::KeyMgrNotSupported)
     }
@@ -76,9 +66,6 @@ impl KeyMgr {
     /// A dummy `remove` implementation that always fails.
     ///
     /// This function always returns [`Error`].
-    //
-    // We need to allow these lints because this impl needs to mirror that of `tor_keymgr::KeyMgr`.
-    #[allow(clippy::unnecessary_wraps, clippy::extra_unused_type_parameters)]
     pub fn remove<K>(&self, _: &dyn Any) -> Result<Option<()>> {
         Err(Error::KeyMgrNotSupported)
     }
