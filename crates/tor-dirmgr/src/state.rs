f2a6754c889cb58f1090d2c6ed114be8da8cc4b3
Nick Mathewson <nickm@torproject.org>
2023-06-26 13:32:36 -0400

    dirmgr: use voting_period.

--- crates/tor-dirmgr/src/state.rs
+++ crates/tor-dirmgr/src/state.rs
@@ -1163,11 +1163,8 @@ fn pick_download_time(lifetime: &Lifetime) -> SystemTime {
 /// clients should fetch the next one.
 fn client_download_range(lt: &Lifetime) -> (SystemTime, Duration) {
     let valid_after = lt.valid_after();
-    let fresh_until = lt.fresh_until();
     let valid_until = lt.valid_until();
-    let voting_interval = fresh_until
-        .duration_since(valid_after)
-        .expect("valid-after must precede fresh-until");
+    let voting_interval = lt.voting_period();
     let whole_lifetime = valid_until
         .duration_since(valid_after)
         .expect("valid-after must precede valid-until");
