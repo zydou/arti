b3b4abcf90ee4788a521186b3756d292bd807f44
Gabriela Moldovan <gabi@torproject.org>
2023-06-26 13:03:31 +0100

    keymgr: Document why we need the StdError impl for tor_keymgr::Error.

--- crates/tor-keymgr/src/err.rs
+++ crates/tor-keymgr/src/err.rs
@@ -29,6 +29,11 @@ impl<K: KeystoreError + Send + Sync> From<K> for Error {
     }
 }
 
+// This impl is needed because tor_keymgr::Error is the error source type of ErrorDetail::KeyStore,
+// which _must_ implement StdError (otherwise we get an error about thiserror::AsDynError not being
+// implemented for tor_keymgr::Error).
+//
+// See <https://github.com/dtolnay/thiserror/issues/212>
 impl StdError for Error {
     fn source(&self) -> Option<&(dyn StdError + 'static)> {
         let e: &dyn StdError = self;
