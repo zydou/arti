434a8b3f0d82a7ed6930f2e65d9dd1177d3ccb84
Gabriela Moldovan <gabi@torproject.org>
2023-06-29 14:48:49 +0100

    arti cfg tests: Add declare_exceptions for storage.keystore_dir.
    This moves `storage.keystore_dir` to a separate `declare_exceptions`
    block and explains why we have this exception.

--- crates/arti/src/cfg.rs
+++ crates/arti/src/cfg.rs
@@ -480,7 +480,28 @@ mod test {
             &[
                 // Settings only available with bridge support
                 "bridges.transports", // we recognise this so we can reject it
-                "storage.keystore_dir",
+            ],
+        );
+
+        declare_exceptions(
+            None,
+            None, // TODO HSS TESTS (#939): Make examples for keymgr settings!
+            FeatureDependent,
+            &[
+                // The keystore settings are feature-dependent.
+                //
+                // The keystore_dir option has a different default value depending on whether the
+                // keystore feature is enabled (if disabled, it defaults to false, if enabled, it
+                // defaults to true). Moreover, if the feature is disabled, keystore_dir **cannot**
+                // be set to true.
+                //
+                // As such, this test will fail no matter which default we write in the example
+                // config (if the example sets this value to `true` and the test will fail if the
+                // keymgr feature disabled. If it's set to `false`, the test will fail if the
+                // keymgr feature is enabled).
+                //
+                // See #939
+                "storage.keystore_dir", // we recognise this so we can reject it if
             ],
         );
 
