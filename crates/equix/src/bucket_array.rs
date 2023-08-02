21dfc6d58c15256f8115a756029aa8a5c87bc91b
Micah Elizabeth Scott <beth@torproject.org>
2023-08-02 14:46:46 -0700

    equix/fuzz: CI and doc fixes
    The build found a stale private doc comment as well as an exception
    that needed to be made in check_doc_features.
    
    The check_doc_features change solidifies a decision that things marked
    with cfg(fuzzing) aren't part of the documented API.

--- crates/equix/src/bucket_array.rs
+++ crates/equix/src/bucket_array.rs
@@ -1,14 +1,17 @@
 //! A data structure for the solver's bucket sort layers
 //!
-//! This module implements the [`KeyValueBucketArray`] and related types,
+//! This module implements the [`hash::KeyValueBucketArray`] and related types,
 //! forming the basis of our solver's temporary storage. The basic key/value
 //! bucket array is a hash table customized with a fixed capacity and minimal
-//! data types. The overall key/value array is organized in a struct-of-arrays
-//! fashion, keeping bucket counts in state memory alongside mutable references
-//! to external key and value memories.
+//! data types. It's organized in a struct-of-arrays fashion, keeping bucket
+//! counts in state memory alongside mutable references to external key and
+//! value memories.
 //!
-//! The implementation is split into a higher level which knows about the hash
-//! table semantics and a lower level that's responsible for memory safety.
+//! For performance and memory efficiency, the hash table layer is built on
+//! a manual memory management layer. Individual bucket arrays are backed
+//! by [`mem::BucketArrayMemory`]. Layouts defined at compile time can be
+//! marked using the [`mem::Uninit`] trait and constructed from a single large
+//! buffer of uninitialized reusable memory.
 
 pub(crate) mod hash;
 pub(crate) mod mem;
