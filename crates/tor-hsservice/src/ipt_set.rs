33d88b8ac8235a514942057826b3a41f6678ee4d
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-09-06 15:01:46 +0100

    tor-hsservice: ipt_set: Define the API

--- crates/tor-hsservice/src/ipt_set.rs
+++ crates/tor-hsservice/src/ipt_set.rs
@@ -1,6 +1,9 @@
 //! IPT set - the principal API between the IPT manager and publisher
 
+use std::ops::DerefMut;
 use std::time::{Duration, Instant};
+
+use void::Void;
 
 /// Set of introduction points to be advertised in a descriptor (if we are to publish)
 ///
@@ -31,6 +34,7 @@ pub(crate) struct IptSet {
 /// Introduction point as specified to publisher by manager
 ///
 /// Convenience type alias.
+#[derive(Debug)]
 pub(crate) struct IptInSet {
     /// Details of the introduction point
     ///
@@ -54,7 +58,9 @@ pub(crate) struct IptInSet {
     ///
     /// If the descriptor has never been published, is `None`.
     ///
-    /// This field is updated by the publisher and read by the manager.
+    /// This field is updated by the publisher, using
+    /// [`note_publication_attempt_start`](IptSet::note_publication_attempt_start)
+    /// and read by the manager.
     pub(crate) last_descriptor_expiry_including_slop: Option<Instant>,
 }
 
@@ -76,3 +82,86 @@ pub(crate) type Ipt = tor_netdoc::doc::hsdesc::IntroPointDesc;
 //
 // TODO HSS IPT_PUBLISH_EXPIRY_SLOP configure?
 pub(crate) const IPT_PUBLISH_EXPIRY_SLOP: Duration = Duration::from_secs(300); // 5 minutes
+
+/// Shared view of introduction points - IPT manager's view
+///
+/// This is the manager's end of a bidirectional "channel",
+/// containing a shared `PublishIptSet`, i.e. an `Option<IptSet>`.
+pub(crate) struct IptsManagerView {
+    /// TODO HSS
+    todo: Void,
+}
+
+/// Shared view of introduction points - IPT publisher's view
+///
+/// This is the publishers's end of a bidirectional "channel",
+/// containing a shared `PublishIptSet`, i.e. an `Option<IptSet>`.
+pub(crate) struct IptsPublisherView {
+    /// TODO HSS
+    todo: Void,
+}
+
+/// Create a new shared state channel for the publication instructions
+pub(crate) fn ipts_channel(initial_state: PublishIptSet) -> (IptsManagerView, IptsPublisherView) {
+    drop(initial_state); // clippy::needless_pass_by_value
+    todo!()
+}
+
+impl IptsManagerView {
+    /// Arrange to be able to update the list of introduction points
+    ///
+    /// The manager may add new ipts, or delete old ones.
+    ///
+    /// The returned value is a lock guard.
+    /// (It is not `Send` so cannot be held across await points.)
+    /// The publisher will be notified when it is dropped.
+    #[allow(unreachable_code)] // TODO HSS remove
+    pub(crate) fn borrow_for_update(&mut self) -> impl DerefMut<Target = PublishIptSet> + '_ {
+        // assist type inference with bogus type
+        std::convert::identity::<Box<PublishIptSet>>(void::unreachable(self.todo))
+    }
+}
+
+impl IptsPublisherView {
+    /// Wait until the IPT set has changed (or may have)
+    ///
+    /// After this returns, to find out what the new IPT set is,
+    /// the publisher calls `borrow_for_publish`.
+    ///
+    /// Will complete immediately if the IPT set has
+    /// changed since the last call to `await_update`.
+    ///
+    /// Returns:
+    ///  * `Some(Ok(())` if the IPT set was (or may have been) updated
+    ///  * `None` if the manager is shutting down and the publisher should shut down too
+    ///  * `Some(Err(..))` if a fatal error occurred
+    pub(crate) async fn await_update(&self) -> Option<Result<(), crate::FatalError>> {
+        void::unreachable(self.todo)
+    }
+
+    /// Look at the list of introduction points to publish
+    ///
+    /// Whenever a a publication attempt is started,
+    /// [`note_publication_attempt_start`](IptSet::note_publication_attempt_start)
+    /// must be called.
+    ///
+    /// The returned value is a lock guard.
+    /// (It is not `Send` so cannot be held across await points.)
+    #[allow(unreachable_code)] // TODO HSS remove
+    pub(crate) fn borrow_for_publish(&self) -> impl DerefMut<Target = PublishIptSet> + '_ {
+        // assist type inference with bogus type
+        std::convert::identity::<Box<PublishIptSet>>(void::unreachable(self.todo))
+    }
+}
+
+impl IptSet {
+    /// Update all the `last_descriptor_expiry_including_slop` for a publication attempt
+    ///
+    /// Called by the publisher when it starts a publication attempt.
+    ///
+    /// When calling this, the publisher promises that the publication attempt
+    /// will either complete, or be abandoned, before `worst_case_end`.
+    pub(crate) fn note_publication_attempt_start(&mut self, worst_case_end: Instant) {
+        todo!()
+    }
+}
