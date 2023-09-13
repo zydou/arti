16629341a89f180b00aa565478da8aeccf541233
Nick Mathewson <nickm@torproject.org>
2023-09-13 17:19:12 +0000

    tor-hsservice: Add more details to the backoff runner docs.

--- crates/tor-hsservice/src/svc/publish/backoff.rs
+++ crates/tor-hsservice/src/svc/publish/backoff.rs
@@ -41,6 +41,10 @@ impl<B: BackoffSchedule, R: Runtime> Runner<B, R> {
     }
 
     /// Run `fallible_fn`, retrying according to the [`BackoffSchedule`] of this `Runner`.
+    /// 
+    /// If `fallible_fn` eventually returns `Ok(_)`, return that output. Otherwise,
+    /// keep retrying until either `fallible_fn` has failed too many times, or until
+    /// a fatal error occurs.
     pub(super) async fn run<T, E, F>(
         mut self,
         mut fallible_fn: impl FnMut() -> F,
