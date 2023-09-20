5c3e51b1aef4744e2fc995d495c794270c1c86b8
Gabriela Moldovan <gabi@torproject.org>
2023-09-20 14:02:56 +0100

    tor-hsservice: Update Publisher::new docs.

--- crates/tor-hsservice/src/svc/publish.rs
+++ crates/tor-hsservice/src/svc/publish.rs
@@ -49,10 +49,12 @@ pub(crate) struct Publisher<R: Runtime> {
 }
 
 impl<R: Runtime> Publisher<R> {
-    /// Create and launch a new publisher.
+    /// Create a new publisher.
     ///
     /// When it launches, it will know no keys or introduction points,
     /// and will therefore not upload any descriptors.
+    ///
+    /// The publisher won't start publishing until you call [`Publisher::launch`].
     //
     // TODO HSS: perhaps we don't need both config and config_rx (we could read the initial config
     // value from config_rx).
