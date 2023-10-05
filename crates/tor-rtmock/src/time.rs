f0cbb57612f8df1013e8e0acec68fe5b146a153b
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-10-05 16:01:21 +0100

    tor-rtmock: MockSleepProvider docs: Move example (fmt)
    Adding "/// " to every line causes rustfmt to have conniptions about
    the trailing whitespace.

--- crates/tor-rtmock/src/time.rs
+++ crates/tor-rtmock/src/time.rs
@@ -74,15 +74,15 @@ use tor_rtcompat::SleepProvider;
 // or generous sprinklings of #[allow].
 ///
 /// ### Examples
-/// 
+///
 /// Suppose you've written a function that relies on making a
 /// connection to the network and possibly timing out:
-/// 
+///
 /// ```rust
 /// use tor_rtcompat::{Runtime,SleepProviderExt};
 /// use std::{net::SocketAddr, io::Result, time::Duration, io::Error};
 /// use futures::io::AsyncWriteExt;
-/// 
+///
 /// async fn say_hi(runtime: impl Runtime, addr: &SocketAddr) -> Result<()> {
 ///    let delay = Duration::new(5,0);
 ///    runtime.timeout(delay, async {
@@ -94,9 +94,9 @@ use tor_rtcompat::SleepProvider;
 ///    Ok(())
 /// }
 /// ```
-/// 
+///
 /// But how should you test this function?
-/// 
+///
 /// You might try connecting to a well-known website to test the
 /// connection case, and to a well-known black hole to test the
 /// timeout case... but that's a bit undesirable.  Your tests might be
@@ -105,31 +105,31 @@ use tor_rtcompat::SleepProvider;
 /// state of the internet.  Similarly, if you make your timeout too long,
 /// your tests might block for a long time; but if your timeout is too short,
 /// the tests might fail on a slow machine or on a slow network.
-/// 
+///
 /// Or, you could solve both of these problems by using `tor-rtmock`
 /// to replace the internet _and_ the passage of time.  (Here we're only
 /// replacing the internet.)
-/// 
+///
 /// ```rust,no_run
 /// # async fn say_hi<R,A>(runtime: R, addr: A) -> Result<(), ()> { Ok(()) }
 /// # // TODO this test hangs for some reason?  Fix it and remove no_run above
 /// use tor_rtmock::{MockSleepRuntime,MockNetRuntime,net::MockNetwork};
 /// use tor_rtcompat::{TcpProvider,TcpListener};
 /// use futures::io::AsyncReadExt;
-/// 
+///
 /// tor_rtcompat::test_with_all_runtimes!(|rt| async move {
-/// 
+///
 ///    let addr1 = "198.51.100.7".parse().unwrap();
 ///    let addr2 = "198.51.100.99".parse().unwrap();
 ///    let sockaddr = "198.51.100.99:101".parse().unwrap();
-/// 
+///
 ///    // Make a runtime that pretends that we are at the first address...
 ///    let fake_internet = MockNetwork::new();
 ///    let rt1 = fake_internet.builder().add_address(addr1).runtime(rt.clone());
 ///    // ...and one that pretends we're listening at the second address.
 ///    let rt2 = fake_internet.builder().add_address(addr2).runtime(rt);
 ///    let listener = rt2.listen(&sockaddr).await.unwrap();
-/// 
+///
 ///    // Now we can test our function!
 ///    let (result1,output) = futures::join!(
 ///           say_hi(rt1, &sockaddr),
@@ -140,7 +140,7 @@ use tor_rtcompat::SleepProvider;
 ///               conn.read_to_end(&mut output).await.unwrap();
 ///               output
 ///           });
-/// 
+///
 ///    assert!(result1.is_ok());
 ///    assert_eq!(&output[..], b"Hello world!\r\n");
 /// });
