8f019a199f61fa305503b47b6f403491549c7bff
Gabriela Moldovan <gabi@torproject.org>
2023-10-23 16:57:56 +0100

    tor-keymgr: Remove now-outdated TODO HSS.
    Key file extensions now have `_private`/`_public` suffixes again.

--- crates/tor-keymgr/src/key_type.rs
+++ crates/tor-keymgr/src/key_type.rs
@@ -84,15 +84,6 @@ impl KeyType {
     pub fn arti_extension(&self) -> String {
         use KeyType::*;
 
-        // TODO HSS:
-        //
-        // There used to be an explicit _private suffix in the extension, but I ended up dropping it
-        // from the name because the "privateness" of the key is encoded in the ArtiPath: !1586
-        // (comment 2942278).
-        //
-        // However, I suppose KS_hsc_desc_enc.x25519 is less obviously a private key than
-        // KS_hsc_desc_enc.x25519_private. Perhaps we do want some redundancy in the name after
-        // all..
         match self {
             Ed25519Keypair => "ed25519_private".into(),
             Ed25519PublicKey => "ed25519_public".into(),
