feaab7c812157810494917a3ca03ce25e20e6dd2
Gabriela Moldovan <gabi@torproject.org>
2023-10-27 11:52:58 +0100

    tor-hsservice: Add tests for define_svc_key_specifier.

--- crates/tor-hsservice/src/keys.rs
+++ crates/tor-hsservice/src/keys.rs
@@ -128,3 +128,65 @@ define_svc_key_specifier! {
         period: TimePeriod,
     }
 }
+
+#[cfg(test)]
+mod test {
+    // @@ begin test lint list maintained by maint/add_warning @@
+    #![allow(clippy::bool_assert_comparison)]
+    #![allow(clippy::clone_on_copy)]
+    #![allow(clippy::dbg_macro)]
+    #![allow(clippy::print_stderr)]
+    #![allow(clippy::print_stdout)]
+    #![allow(clippy::single_char_pattern)]
+    #![allow(clippy::unwrap_used)]
+    #![allow(clippy::unchecked_duration_subtraction)]
+    #![allow(clippy::useless_vec)]
+    #![allow(clippy::needless_pass_by_value)]
+    //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
+    use super::*;
+    use tor_keymgr::KeySpecifier;
+
+    #[test]
+    fn hsid_key_specifiers() {
+        let nickname = HsNickname::try_from("shallot".to_string()).unwrap();
+        let key_spec = HsIdPublicKeySpecifier::new(&nickname);
+        assert_eq!(
+            key_spec.arti_path().unwrap().as_str(),
+            "hs/shallot/KP_hs_id"
+        );
+
+        let key_spec = HsIdKeypairSpecifier::new(&nickname);
+        assert_eq!(
+            key_spec.arti_path().unwrap().as_str(),
+            "hs/shallot/KS_hs_id"
+        );
+    }
+
+    #[test]
+    fn blind_id_key_specifiers() {
+        let nickname = HsNickname::try_from("shallot".to_string()).unwrap();
+        let period = TimePeriod::from_parts(1, 2, 3);
+        let key_spec = BlindIdPublicKeySpecifier::new(&nickname, period);
+        assert_eq!(
+            key_spec.arti_path().unwrap().as_str(),
+            "hs/shallot/KP_hs_blind_id_2_1_3"
+        );
+
+        let key_spec = BlindIdKeypairSpecifier::new(&nickname, period);
+        assert_eq!(
+            key_spec.arti_path().unwrap().as_str(),
+            "hs/shallot/KS_hs_blind_id_2_1_3"
+        );
+    }
+
+    #[test]
+    fn desc_signing_key_specifiers() {
+        let nickname = HsNickname::try_from("shallot".to_string()).unwrap();
+        let period = TimePeriod::from_parts(1, 2, 3);
+        let key_spec = DescSigningKeypairSpecifier::new(&nickname, period);
+        assert_eq!(
+            key_spec.arti_path().unwrap().as_str(),
+            "hs/shallot/KS_hs_desc_sign_2_1_3"
+        );
+    }
+}
