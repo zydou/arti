1a45a2b9af6aa2a6bba8a255f2c2e0fa35ea9cc0
Nick Mathewson <nickm@torproject.org>
2023-11-29 11:03:05 -0500

    Clarify and downgrade TODO about ReusableSecret.

--- crates/tor-llcrypto/src/pk.rs
+++ crates/tor-llcrypto/src/pk.rs
@@ -13,7 +13,14 @@ pub mod rsa;
 /// key-agreement trait, but for now we are just re-using the APIs from
 /// [`x25519_dalek`].
 pub mod curve25519 {
-    // TODO DALEK: We may want to migrate from StaticSecret to ReusableSecret.
+    // TODO: We may want eventually want to expose ReusableSecret instead of
+    // StaticSecret, for use in places where we need to use a single secret
+    // twice in one handshake, but we do not need that secret to be persistent.
+    //
+    // The trouble here is that if we use ReusableSecret in these cases, we
+    // cannot easily construct it for testing purposes.  We could in theory
+    // kludge something together using a fake Rng, but that might be more
+    // trouble than we want to go looking for.
     pub use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret, StaticSecret};
 
     use educe::Educe;
