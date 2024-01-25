cdf9705a7ede881dc4a5ba1be46086a8f2db2c38
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-01-25 16:42:31 +0000

    test-temp-dir: Drop a TODO/allow

--- crates/test-temp-dir/src/lib.rs
+++ crates/test-temp-dir/src/lib.rs
@@ -40,9 +40,6 @@
 #![allow(clippy::needless_raw_string_hashes)] // complained-about code is fine, often best
 //! <!-- @@ end lint list maintained by maint/add_warning @@ -->
 
-// TODO make this into a pub module somewhere, perhaps tor-basic-utils
-// When we do that, add a proper tested example
-#![allow(unreachable_pub)]
 // We have a nonstandard test lint block
 #![allow(clippy::print_stdout)]
 
