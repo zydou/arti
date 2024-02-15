939b0c31dd96195aa8cc55a8d2fd247fca00c3d2
Gabriela Moldovan <gabi@torproject.org>
2024-02-15 16:50:52 +0000

    tor-keymgr: Make KeyMgr::remove_with_type take a KeystoreEntry (fmt).

--- crates/tor-keymgr/src/mgr.rs
+++ crates/tor-keymgr/src/mgr.rs
@@ -274,10 +274,7 @@ impl KeyMgr {
     /// Like [`KeyMgr::remove`], except this function takes an explicit
     /// [`&KeyType`](crate::KeyType) argument instead
     /// of obtaining it from the specified type's [`ToEncodableKey`] implementation.
-    pub fn remove_with_type(
-        &self,
-        entry: &KeystoreEntry,
-    ) -> Result<Option<()>> {
+    pub fn remove_with_type(&self, entry: &KeystoreEntry) -> Result<Option<()>> {
         let store = self.select_keystore(&(*entry.keystore_id()).into())?;
 
         store.remove(entry.key_path(), entry.key_type())
