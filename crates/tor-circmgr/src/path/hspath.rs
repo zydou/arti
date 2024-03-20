96bb083fc934f69e91c99faf80ac9a21abc8ebc1
Gabriela Moldovan <gabi@torproject.org>
2024-03-20 18:22:06 +0000

    tor-circmgr: Move HsPathBuilder trait impl closer to struct definition.
    This is just code movement to make the code a bit easier to follow.

--- crates/tor-circmgr/src/path/hspath.rs
+++ crates/tor-circmgr/src/path/hspath.rs
@@ -135,6 +135,41 @@ impl HsPathBuilder {
     }
 }
 
+impl<'a> AnonymousPathBuilder<'a> for HsPathBuilder {
+    fn chosen_exit(&self) -> Option<&Relay<'_>> {
+        None
+    }
+
+    fn compatible_with(&self) -> Option<&OwnedChanTarget> {
+        self.compatible_with.as_ref()
+    }
+
+    fn path_kind(&self) -> &'static str {
+        "onion-service circuit"
+    }
+
+    fn pick_exit<'s, R: Rng>(
+        &'s self,
+        rng: &mut R,
+        netdir: &'a NetDir,
+        guard_exclusion: RelayExclusion<'a>,
+        _rs_cfg: &RelaySelectionConfig<'_>,
+    ) -> Result<(Relay<'a>, RelayUsage)> {
+        // TODO: This usage is a bit convoluted, and some onion-service-
+        // related circuits don't need this much stability.
+        let usage = RelayUsage::middle_relay(Some(&RelayUsage::new_intro_point()));
+        let selector = RelaySelector::new(usage, guard_exclusion);
+
+        let (relay, info) = selector.select_relay(rng, netdir);
+        let relay = relay.ok_or_else(|| Error::NoRelay {
+            path_kind: self.path_kind(),
+            role: "final hop",
+            problem: info.to_string(),
+        })?;
+        Ok((relay, RelayUsage::middle_relay(Some(selector.usage()))))
+    }
+}
+
 /// A path builder for hidden service circuits that use vanguards.
 ///
 /// Used by [`HsPathBuilder`] when vanguards are enabled.
@@ -209,40 +244,5 @@ impl VanguardHsPathBuilder {
     /// for error reporting purposes.
     fn path_kind(&self) -> &'static str {
         "onion-service vanguard circuit"
-    }
-}
-
-impl<'a> AnonymousPathBuilder<'a> for HsPathBuilder {
-    fn chosen_exit(&self) -> Option<&Relay<'_>> {
-        None
-    }
-
-    fn compatible_with(&self) -> Option<&OwnedChanTarget> {
-        self.compatible_with.as_ref()
-    }
-
-    fn path_kind(&self) -> &'static str {
-        "onion-service circuit"
-    }
-
-    fn pick_exit<'s, R: Rng>(
-        &'s self,
-        rng: &mut R,
-        netdir: &'a NetDir,
-        guard_exclusion: RelayExclusion<'a>,
-        _rs_cfg: &RelaySelectionConfig<'_>,
-    ) -> Result<(Relay<'a>, RelayUsage)> {
-        // TODO: This usage is a bit convoluted, and some onion-service-
-        // related circuits don't need this much stability.
-        let usage = RelayUsage::middle_relay(Some(&RelayUsage::new_intro_point()));
-        let selector = RelaySelector::new(usage, guard_exclusion);
-
-        let (relay, info) = selector.select_relay(rng, netdir);
-        let relay = relay.ok_or_else(|| Error::NoRelay {
-            path_kind: self.path_kind(),
-            role: "final hop",
-            problem: info.to_string(),
-        })?;
-        Ok((relay, RelayUsage::middle_relay(Some(selector.usage()))))
     }
 }
