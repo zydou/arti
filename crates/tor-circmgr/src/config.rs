5522fb77af04beacf4ddebcf8f6325b5ebcf0df9
Gabriela Moldovan <gabi@torproject.org>
2024-05-09 10:59:03 +0100

    tor-circmgr: Downgrade some TODO HS-VANGUARDs.

--- crates/tor-circmgr/src/config.rs
+++ crates/tor-circmgr/src/config.rs
@@ -367,7 +367,7 @@ define_accessor_trait! {
         circuit_timing: CircuitTiming,
         preemptive_circuits: PreemptiveCircuitConfig,
         +
-        // TODO HS-VANGUARDS: ideally this would be defined in the same way as `path_rules`,
+        // Note: ideally this would be defined in the same way as `path_rules`,
         // `circuit_timing`, etc., but define_accessor_trait unconditionally adds
         // AsRef<VanguardsConfig> as a supertrait, which can't be cfg'd behind
         // the vanguards feature.
