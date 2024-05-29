237eb7a53f8953b8b6697ff110e1aec686b19d50
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-05-29 12:15:39 +0100

    ChannelSender::poll_ready_unpin_bool: move to util
    This is where it belongs.

--- crates/tor-proto/src/channel.rs
+++ crates/tor-proto/src/channel.rs
@@ -320,25 +320,6 @@ impl ChannelSender {
     }
 }
 
-pub(crate) trait SinkExt<T>: Sink<T> {
-    /// Calls `futures::Sink::poll_ready` but requires `Unpin` and returns `bool`
-    ///
-    /// Various gnarly places in the circuit reactor find this convenient.
-    ///
-    /// TODO #1397 (circuit reactor) probably remove this when the circuit reactor is rewritten.
-    fn poll_ready_unpin_bool(&mut self, cx: &mut Context<'_>) -> StdResult<bool, Self::Error>
-    where
-        Self: Unpin,
-    {
-        Ok(match Sink::poll_ready(Pin::new(self), cx) {
-            Poll::Ready(Ok(())) => true,
-            Poll::Ready(Err(e)) => return Err(e),
-            Poll::Pending => false,
-        })
-    }
-}
-impl<T, S: Sink<T>> SinkExt<T> for S {}
-
 impl Sink<AnyChanCell> for ChannelSender {
     type Error = Error;
 
