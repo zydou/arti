8fc4c063d7076a6e9ffe36f2ee5bf39394fa3113
Gabriela Moldovan <gabi@torproject.org>
2024-06-17 17:45:10 +0100

    arti: Add an arti hsc subcommand.
    This adds an experimental `arti hsc` subcommand for managing client
    state and keys. Currently, it only supports the
    `prepare-service-discovery-keys` operation described in #1281 and
    `doc/dev/notes/client-auth.md`.
    
    A note on terminology: I am referring to services that encrypt the
    second layer of their descriptor as running in "restricted discovery"
    mode (because they can only be discovered, i.e. have their IPT points
    found out, by a set of authorized clients). The corresponding client
    "auth" keys, being the keys that enable the client to find out the list
    of intro points, pow-params etc. of the service, are referred to as
    service "discovery keys".
    
    Alternative names I considered:
      * extra descriptor encryption: accurate, but overly technical. IMO,
        the CLI should be accessible to users who aren't familiar with the
        nitty-gritty of the protocol
      * shielded mode: good, but slightly misleading. Calling it "shielded
        mode" makes it sound like a universally desirable "extra protection"
        that should almost always be enabled (which is not the case). Seeing
        `shielded_mode = off` in the config might be worry operators that
        don't fully understand what "extra descriptor encryption" or
        "shielded mode" means
      * restricted mode: slightly inaccurate. It implies this mechanism is a
        good substitute for conventional service-side authentication, which
        it isn't (because client authorization isn't instantaneous)
    
    Closes #1281

---
+++ crates/arti/src/subcommands/hsc.rs
@@ -0,0 +1,147 @@
+//! The `hsc` subcommand.
+
+use crate::{Result, TorClient};
+
+use anyhow::{anyhow, Context};
+use arti_client::{HsClientDescEncKey, HsId, KeystoreSelector, TorClientConfig};
+use clap::{ArgMatches, Args, FromArgMatches, Parser, Subcommand, ValueEnum};
+use tor_rtcompat::Runtime;
+
+use std::fs::OpenOptions;
+use std::io;
+
+/// The hsc subcommands the arti CLI will be augumented with.
+#[derive(Parser, Debug)]
+pub(crate) enum HscSubcommands {
+    /// Run state management commands for an Arti hidden service client.
+    #[command(subcommand)]
+    Hsc(HscSubcommand),
+}
+
+#[derive(Debug, Subcommand)]
+pub(crate) enum HscSubcommand {
+    /// Prepare a service discovery key for connecting
+    /// to a service running in restricted discovery mode.
+    #[command(arg_required_else_help = true)]
+    PrepareServiceDiscoveryKey(PrepareServiceDiscoveryKeyArgs),
+}
+
+/// The arguments of the [`PrepareServiceDiscoveryKey`](HscSubcommand::PrepareServiceDiscoveryKey)
+/// subcommand.
+#[derive(Debug, Clone, Args)]
+pub(crate) struct PrepareServiceDiscoveryKeyArgs {
+    /// The .onion address of the hidden service
+    #[arg(long)]
+    onion_name: HsId,
+
+    /// Write the public key to <FILE>. Use - to write to stdout
+    #[arg(long, name = "FILE")]
+    output: String,
+
+    /// Whether to overwrite the output file if it already exists
+    #[arg(long)]
+    overwrite: bool,
+
+    /// Whether to generate the key if it is missing
+    #[arg(
+        long,
+        default_value_t = PrepareServiceDiscoveryKey::IfNeeded,
+        value_enum
+    )]
+    generate: PrepareServiceDiscoveryKey,
+    // TODO: add an option for selecting the keystore to generate the keypair in
+}
+
+/// Whether to generate the key if missing.
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, ValueEnum)]
+enum PrepareServiceDiscoveryKey {
+    /// Do not generate the key.
+    No,
+    /// Generate the key if it's missing.
+    #[default]
+    IfNeeded,
+}
+
+/// Run the `hsc` subcommand.
+pub(crate) fn run<R: Runtime>(
+    runtime: R,
+    hsc_matches: &ArgMatches,
+    config: &TorClientConfig,
+) -> Result<()> {
+    let subcommand =
+        HscSubcommand::from_arg_matches(hsc_matches).expect("Could not parse hsc subcommand");
+
+    match subcommand {
+        HscSubcommand::PrepareServiceDiscoveryKey(args) => {
+            prepare_service_discovery_key(runtime, &args, config)
+        }
+    }
+}
+
+/// Run the `hsc prepare-stealth-mode-key` subcommand.
+fn prepare_service_discovery_key<R: Runtime>(
+    runtime: R,
+    args: &PrepareServiceDiscoveryKeyArgs,
+    config: &TorClientConfig,
+) -> Result<()> {
+    let client = TorClient::with_runtime(runtime)
+        .config(config.clone())
+        .create_unbootstrapped()?;
+    let key = match args.generate {
+        PrepareServiceDiscoveryKey::IfNeeded => {
+            // TODO: consider using get_or_generate in generate_service_discovery_key
+            client
+                .get_service_discovery_key(args.onion_name)?
+                .map(Ok)
+                .unwrap_or_else(|| {
+                    client
+                        .generate_service_discovery_key(KeystoreSelector::Default, args.onion_name)
+                })?
+        }
+        PrepareServiceDiscoveryKey::No => {
+            match client.get_service_discovery_key(args.onion_name)? {
+                Some(key) => key,
+                None => {
+                    return Err(anyhow!(
+                        "Service discovery key not found. Rerun with --generate=if-needed to generate a new service discovery keypair"
+                    ));
+                }
+            }
+        }
+    };
+
+    // Output the public key to the specified file, or to stdout.
+    match args.output.as_str() {
+        "-" => write_public_key(io::stdout(), &key)?,
+        filename => {
+            let res = OpenOptions::new()
+                .create(true)
+                .create_new(!args.overwrite)
+                .write(true)
+                .truncate(true)
+                .open(filename)
+                .and_then(|f| write_public_key(f, &key));
+
+            if let Err(e) = res {
+                match e.kind() {
+                    io::ErrorKind::AlreadyExists => {
+                        return Err(anyhow!("{filename} already exists. Move it, or rerun with --overwrite to overwrite it"));
+                    }
+                    _ => {
+                        // TODO maybe handle some other ErrorKinds
+                        return Err(e)
+                            .with_context(|| format!("could not write public key to {filename}"));
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// Write the public part of `key` to `f`.
+fn write_public_key(mut f: impl io::Write, key: &HsClientDescEncKey) -> io::Result<()> {
+    write!(f, "{}", key.display_authorized_client())?;
+    Ok(())
+}
