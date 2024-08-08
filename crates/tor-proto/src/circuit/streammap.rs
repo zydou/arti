93f47a0cd72efc3bccbc19b9c13d8045452168b1
Jim Newsome <jnewsome@torproject.org>
2024-08-08 14:52:47 -0500

    StreamMap: Put open streams in their own map instead of CountedHashMap
    This temporarily results in *more* bookkeeping, but is a step towards
    putting the mpsc receivers back into `OpenStreamEnt`, and putting
    the whole `OpenStreamEnt`s into the `StreamPollSet`.

--- crates/tor-proto/src/circuit/streammap.rs
+++ crates/tor-proto/src/circuit/streammap.rs
@@ -1,5 +1,6 @@
 //! Types and code for mapping StreamIDs to streams on a circuit.
 
+// TODO: No longer used; remove?
 mod counted_map;
 
 use crate::circuit::halfstream::HalfStream;
@@ -14,6 +15,8 @@ use tor_cell::relaycell::UnparsedRelayMsg;
 use tor_cell::relaycell::{msg::AnyRelayMsg, StreamId};
 
 use futures::channel::mpsc;
+use std::collections::hash_map;
+use std::collections::HashMap;
 use std::num::NonZeroU16;
 use tor_error::{bad_api_usage, internal};
 
@@ -22,8 +25,6 @@ use rand::Rng;
 use crate::circuit::reactor::RECV_WINDOW_INIT;
 use crate::circuit::sendme::StreamRecvWindow;
 use tracing::debug;
-
-use self::counted_map::{CountedHashMap, Entry};
 
 /// Entry for an open stream
 ///
@@ -56,9 +57,7 @@ pub(super) struct EndSentStreamEnt {
 
 /// The entry for a stream.
 #[derive(Debug)]
-enum StreamEnt {
-    /// An open stream.
-    Open(OpenStreamEnt),
+enum ClosedStreamEnt {
     /// A stream for which we have received an END cell, but not yet
     /// had the stream object get dropped.
     EndReceived,
@@ -71,9 +70,6 @@ enum StreamEnt {
 }
 
 /// Mutable reference to a stream entry.
-///
-/// We don't expose `&mut StreamEnt` directly outside this module, to prevent
-/// other code from changing it from one of these variants to another: only this module is allowed to do that.
 pub(super) enum StreamEntMut<'a> {
     /// An open stream.
     Open(&'a mut OpenStreamEnt),
@@ -85,13 +81,18 @@ pub(super) enum StreamEntMut<'a> {
     EndSent(&'a mut EndSentStreamEnt),
 }
 
-impl<'a> From<&'a mut StreamEnt> for StreamEntMut<'a> {
-    fn from(value: &'a mut StreamEnt) -> Self {
+impl<'a> From<&'a mut ClosedStreamEnt> for StreamEntMut<'a> {
+    fn from(value: &'a mut ClosedStreamEnt) -> Self {
         match value {
-            StreamEnt::Open(e) => Self::Open(e),
-            StreamEnt::EndReceived => Self::EndReceived,
-            StreamEnt::EndSent(e) => Self::EndSent(e),
+            ClosedStreamEnt::EndReceived => Self::EndReceived,
+            ClosedStreamEnt::EndSent(e) => Self::EndSent(e),
         }
+    }
+}
+
+impl<'a> From<&'a mut OpenStreamEnt> for StreamEntMut<'a> {
+    fn from(value: &'a mut OpenStreamEnt) -> Self {
+        Self::Open(value)
     }
 }
 
@@ -105,16 +106,6 @@ pub(super) enum ShouldSendEnd {
     DontSend,
 }
 
-/// Predicate used with CountedHashMap to count open streams.
-struct IsOpen;
-impl counted_map::Predicate for IsOpen {
-    type Item = StreamEnt;
-
-    fn check(item: &Self::Item) -> bool {
-        matches!(item, StreamEnt::Open(_))
-    }
-}
-
 /// A priority for use with [`StreamPollSet`].
 #[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord)]
 struct Priority(u64);
@@ -122,15 +113,20 @@ struct Priority(u64);
 /// A map from stream IDs to stream entries. Each circuit has one for each
 /// hop.
 pub(super) struct StreamMap {
-    /// Map from StreamId to StreamEnt.  If there is no entry for a
-    /// StreamId, that stream doesn't exist.
+    /// Open streams.
     // Invariants:
+    // * Keys are disjoint with `closed_streams`.
     // * Every open stream also has an entry with the same `StreamId` in `rxs`.
-    m: CountedHashMap<StreamId, StreamEnt, IsOpen>,
+    // XXX: Subsequent commit will merge `rxs` into `open_streams`
+    open_streams: HashMap<StreamId, OpenStreamEnt>,
     /// Streams for cells that should be sent down this stream.
     // Invariants:
-    // * Every `StreamId` has an entry in `m` with an open stream.
+    // * Every `StreamId` has an entry in `open_streams`.
     rxs: StreamPollSet<StreamId, AnyRelayMsg, Priority, mpsc::Receiver<AnyRelayMsg>>,
+    /// Closed streams.
+    // Invariants:
+    // * Keys are disjoint with `open_streams`.
+    closed_streams: HashMap<StreamId, ClosedStreamEnt>,
     /// The next StreamId that we should use for a newly allocated
     /// circuit.
     next_stream_id: StreamId,
@@ -147,28 +143,17 @@ impl StreamMap {
         let mut rng = rand::thread_rng();
         let next_stream_id: NonZeroU16 = rng.gen();
         StreamMap {
-            m: CountedHashMap::new(),
+            open_streams: HashMap::new(),
             rxs: StreamPollSet::new(),
+            closed_streams: HashMap::new(),
             next_stream_id: next_stream_id.into(),
             next_priority: Priority(0),
         }
     }
 
-    /// Return an iterator over the entries in this StreamMap.
-    // TODO: Consider removing. May no longer be needed.
-    #[allow(dead_code)]
-    pub(super) fn iter_mut(&mut self) -> impl Iterator<Item = (StreamId, StreamEntMut<'_>)> {
-        // CORRECTNESS: before we return any of the value references from this iterator,
-        // we convert them into a StreamEntMut,
-        // to prevent any changes that would change their Open status.
-        self.m
-            .iter_mut_unchecked()
-            .map(|(id, ent)| (*id, ent.into()))
-    }
-
     /// Return the number of open streams in this map.
     pub(super) fn n_open_streams(&self) -> usize {
-        self.m.count()
+        self.open_streams.len()
     }
 
     /// Return the next available priority.
@@ -178,20 +163,43 @@ impl StreamMap {
         rv
     }
 
+    fn try_insert_open_stream(
+        &mut self,
+        id: StreamId,
+        stream: OpenStreamEnt,
+        rx: mpsc::Receiver<AnyRelayMsg>,
+    ) -> std::result::Result<(), (OpenStreamEnt, mpsc::Receiver<AnyRelayMsg>)> {
+        let hash_map::Entry::Vacant(ent) = self.open_streams.entry(id) else {
+            return Err((stream, rx));
+        };
+        if self.closed_streams.contains_key(&id) {
+            return Err((stream, rx));
+        }
+        ent.insert(stream);
+        let priority = self.take_next_priority();
+        self.rxs
+            .try_insert(id, priority, rx)
+            // By
+            // * rxs invariant that every key is also in `open_streams`
+            // * We verified this key is not in `open_streams`.
+            .expect("Unexpected rx entry for unused StreamId");
+        Ok(())
+    }
+
     /// Add an entry to this map; return the newly allocated StreamId.
     pub(super) fn add_ent(
         &mut self,
         sink: mpsc::Sender<UnparsedRelayMsg>,
-        rx: mpsc::Receiver<AnyRelayMsg>,
+        mut rx: mpsc::Receiver<AnyRelayMsg>,
         send_window: sendme::StreamSendWindow,
         cmd_checker: AnyCmdChecker,
     ) -> Result<StreamId> {
-        let stream_ent = StreamEnt::Open(OpenStreamEnt {
+        let mut stream_ent = OpenStreamEnt {
             sink,
             send_window,
             dropped: 0,
             cmd_checker,
-        });
+        };
         // This "65536" seems too aggressive, but it's what tor does.
         //
         // Also, going around in a loop here is (sadly) needed in order
@@ -199,18 +207,10 @@ impl StreamMap {
         for _ in 1..=65536 {
             let id: StreamId = self.next_stream_id;
             self.next_stream_id = wrapping_next_stream_id(self.next_stream_id);
-            let ent = self.m.entry(id);
-            if let Entry::Vacant(ent) = ent {
-                ent.insert(stream_ent);
-                let priority = self.take_next_priority();
-                self.rxs
-                    .try_insert(id, priority, rx)
-                    // By
-                    // * rxs invariant that every key is also in `m`
-                    // * We verified this key is not in `m`.
-                    .expect("Unexpected rx entry for unused StreamId");
-                return Ok(id);
-            }
+            (stream_ent, rx) = match self.try_insert_open_stream(id, stream_ent, rx) {
+                Ok(_) => return Ok(id),
+                Err((s, r)) => (s, r),
+            };
         }
 
         Err(Error::IdRangeFull)
@@ -226,35 +226,25 @@ impl StreamMap {
         id: StreamId,
         cmd_checker: AnyCmdChecker,
     ) -> Result<()> {
-        let stream_ent = StreamEnt::Open(OpenStreamEnt {
+        let stream_ent = OpenStreamEnt {
             sink,
             send_window,
             dropped: 0,
             cmd_checker,
-        });
-
-        let ent = self.m.entry(id);
-        if let Entry::Vacant(ent) = ent {
-            ent.insert(stream_ent);
-            let priority = self.take_next_priority();
-            self.rxs
-                .try_insert(id, priority, rx)
-                // By
-                // * rxs invariant that every key is also in `m`
-                // * We verified this key is not in `m`.
-                .expect("Unexpected rx");
-            Ok(())
-        } else {
-            Err(Error::IdUnavailable(id))
-        }
+        };
+        self.try_insert_open_stream(id, stream_ent, rx)
+            .map_err(|_| Error::IdUnavailable(id))
     }
 
     /// Return the entry for `id` in this map, if any.
     pub(super) fn get_mut(&mut self, id: StreamId) -> Option<StreamEntMut<'_>> {
-        // CORRECTNESS: before we return a reference to one of this map's values,
-        // we convert it into a StreamEntMut,
-        // to prevent any changes that would change its Open status.
-        self.m.get_mut_unchecked(&id).map(StreamEntMut::from)
+        if let Some(e) = self.open_streams.get_mut(&id) {
+            return Some(e.into());
+        }
+        if let Some(e) = self.closed_streams.get_mut(&id) {
+            return Some(e.into());
+        }
+        None
     }
 
     /// Note that we received an END message (or other message indicating the end of
@@ -262,36 +252,30 @@ impl StreamMap {
     ///
     /// Returns true if there was really a stream there.
     pub(super) fn ending_msg_received(&mut self, id: StreamId) -> Result<()> {
-        // Check the hashmap for the right stream. Bail if not found.
-        // Also keep the hashmap handle so that we can do more efficient inserts/removals
-        let mut stream_entry = match self.m.entry(id) {
-            Entry::Vacant(_) => {
-                return Err(Error::CircProto(
-                    "Received END cell on nonexistent stream".into(),
-                ))
-            }
-            Entry::Occupied(o) => o,
+        if self.open_streams.remove(&id).is_some() {
+            self.rxs
+                .remove(&id)
+                // By invariant on `self.m` that every open stream has an entry in `rxs`.
+                .expect("Missing stream for {id:?}");
+            let prev = self.closed_streams.insert(id, ClosedStreamEnt::EndReceived);
+            debug_assert!(prev.is_none(), "Unexpected duplicate entry for {id}");
+            return Ok(());
+        }
+        let hash_map::Entry::Occupied(closed_entry) = self.closed_streams.entry(id) else {
+            return Err(Error::CircProto(
+                "Received END cell on nonexistent stream".into(),
+            ));
         };
-
         // Progress the stream's state machine accordingly
-        match stream_entry.get() {
-            StreamEnt::EndReceived => Err(Error::CircProto(
+        match closed_entry.get() {
+            ClosedStreamEnt::EndReceived => Err(Error::CircProto(
                 "Received two END cells on same stream".into(),
             )),
-            StreamEnt::EndSent { .. } => {
+            ClosedStreamEnt::EndSent { .. } => {
                 debug!("Actually got an end cell on a half-closed stream!");
                 // We got an END, and we already sent an END. Great!
                 // we can forget about this stream.
-                stream_entry.remove_entry();
-                Ok(())
-            }
-            StreamEnt::Open { .. } => {
-                stream_entry.insert(StreamEnt::EndReceived);
-                self.rxs
-                    .remove(&id)
-                    // By invariant on `self.m` that every open stream has an entry in `rxs`.
-                    .expect("Missing stream for {id:?}");
-
+                closed_entry.remove_entry();
                 Ok(())
             }
         }
@@ -307,46 +291,46 @@ impl StreamMap {
     ) -> Result<ShouldSendEnd> {
         use TerminateReason as TR;
 
+        if let Some(OpenStreamEnt {
+            send_window,
+            dropped,
+            cmd_checker,
+            // notably absent: the channels for sink and stream, which will get dropped and
+            // closed (meaning reads/writes from/to this stream will now fail)
+            ..
+        }) = self.open_streams.remove(&id)
+        {
+            self.rxs
+                .remove(&id)
+                // By invariant on `self.m` that every open stream has an entry in `rxs`.
+                .expect("Missing rx for {id:?}");
+            // FIXME(eta): we don't copy the receive window, instead just creating a new one,
+            //             so a malicious peer can send us slightly more data than they should
+            //             be able to; see arti#230.
+            let mut recv_window = StreamRecvWindow::new(RECV_WINDOW_INIT);
+            recv_window.decrement_n(dropped)?;
+            // TODO: would be nice to avoid new_ref.
+            let half_stream = HalfStream::new(send_window, recv_window, cmd_checker);
+            let explicitly_dropped = why == TR::StreamTargetClosed;
+            let prev = self.closed_streams.insert(
+                id,
+                ClosedStreamEnt::EndSent(EndSentStreamEnt {
+                    half_stream,
+                    explicitly_dropped,
+                }),
+            );
+            debug_assert!(prev.is_none(), "Unexpected duplicate entry for {id}");
+            return Ok(ShouldSendEnd::Send);
+        }
+
         // Progress the stream's state machine accordingly
         match self
-            .m
+            .closed_streams
             .remove(&id)
             .ok_or_else(|| Error::from(internal!("Somehow we terminated a nonexistent stream?")))?
         {
-            StreamEnt::EndReceived => Ok(ShouldSendEnd::DontSend),
-            StreamEnt::Open(OpenStreamEnt {
-                send_window,
-                dropped,
-                cmd_checker,
-                // notably absent: the channels for sink and stream, which will get dropped and
-                // closed (meaning reads/writes from/to this stream will now fail)
-                ..
-            }) => {
-                // FIXME(eta): we don't copy the receive window, instead just creating a new one,
-                //             so a malicious peer can send us slightly more data than they should
-                //             be able to; see arti#230.
-                let mut recv_window = StreamRecvWindow::new(RECV_WINDOW_INIT);
-                recv_window.decrement_n(dropped)?;
-                // TODO: would be nice to avoid new_ref.
-                let half_stream = HalfStream::new(send_window, recv_window, cmd_checker);
-                let explicitly_dropped = why == TR::StreamTargetClosed;
-                self.m.insert(
-                    id,
-                    StreamEnt::EndSent(EndSentStreamEnt {
-                        half_stream,
-                        explicitly_dropped,
-                    }),
-                );
-                self.rxs
-                    .remove(&id)
-                    // By:
-                    // * Invariant on `m` that every open stream has a corresponding entry in `rxs`.
-                    // * We verified above that this id had an open stream in `m`.
-                    .expect("Missing receiver");
-
-                Ok(ShouldSendEnd::Send)
-            }
-            StreamEnt::EndSent(EndSentStreamEnt {
+            ClosedStreamEnt::EndReceived => Ok(ShouldSendEnd::DontSend),
+            ClosedStreamEnt::EndSent(EndSentStreamEnt {
                 ref mut explicitly_dropped,
                 ..
             }) => match (*explicitly_dropped, why) {
@@ -382,8 +366,8 @@ impl StreamMap {
         self.rxs
             .poll_ready_iter(cx)
             .map(|(sid, msg, _priority, _rx)| {
-                let ent = self.m.get(sid);
-                let Some(StreamEnt::Open(o)) = ent else {
+                let ent = self.open_streams.get(sid);
+                let Some(o) = ent else {
                     // By:
                     // * Invariant on `rxs` that every key has a corresponding open strema in `m`.
                     panic!("Missing open stream for {sid:?}: {ent:?}");
