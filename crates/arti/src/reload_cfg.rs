6227e51080852621f8160009a8b65023cc89e367
Gabriela Moldovan <gabi@torproject.org>
2024-08-12 18:34:18 +0100

    tor-config: Move FileWatcher to tor-config.
    For the most part, this is just code motion.
    
    The only change here is that `prepare` is no longer a method on
    `FileWatcher`. This decouples `FileWatcher` from `ConfigurationSources`,
    enabling us to use it to watch files and directories that aren't
    configuration.

--- crates/arti/src/reload_cfg.rs
+++ crates/arti/src/reload_cfg.rs
@@ -1,16 +1,14 @@
 //! Code to watch configuration files for any changes.
 
-use std::collections::HashSet;
-use std::path::{Path, PathBuf};
-use std::sync::mpsc::{channel as std_channel, Sender};
+use std::sync::mpsc::{channel as std_channel};
 use std::sync::Weak;
 use std::time::Duration;
 
 use anyhow::Context;
 use arti_client::config::Reconfigure;
 use arti_client::TorClient;
-use notify::Watcher;
-use tor_config::{sources::FoundConfigFiles, ConfigurationSource, ConfigurationSources};
+use tor_config::file_watcher::FileWatcherBuilder;
+use tor_config::{sources::FoundConfigFiles, ConfigurationSource, ConfigurationSources, file_watcher::{Event, FileWatcher}};
 use tor_rtcompat::Runtime;
 use tracing::{debug, error, info, warn};
 
@@ -18,18 +16,6 @@ use crate::{ArtiCombinedConfig, ArtiConfig};
 
 /// How long to wait after an event got received, before we try to process it.
 const DEBOUNCE_INTERVAL: Duration = Duration::from_secs(1);
-
-/// Event possibly triggering a configuration reload
-#[derive(Debug)]
-enum Event {
-    /// SIGHUP has been received.
-    #[cfg(target_family = "unix")]
-    SigHup,
-    /// Some files may have been modified.
-    FileChanged,
-    /// Some filesystem events may have been missed.
-    Rescan,
-}
 
 /// An object that can be reconfigured when our configuration changes.
 ///
@@ -75,7 +61,7 @@ pub(crate) fn watch_for_config_changes<R: Runtime>(
         // ignore send error, rx can't be disconnected if we are here
         let _ = tx.send(Event::Rescan);
         let mut watcher = FileWatcher::builder();
-        watcher.prepare(&sources)?;
+        prepare(&mut watcher, &sources)?;
         Some(watcher.start_watching(tx.clone())?)
     } else {
         None
@@ -124,8 +110,7 @@ pub(crate) fn watch_for_config_changes<R: Runtime>(
 
                 let found_files = if watcher.is_some() {
                     let mut new_watcher = FileWatcher::builder();
-                    let found_files = new_watcher
-                        .prepare(&sources)
+                    let found_files = prepare(&mut new_watcher, &sources)
                         .context("FS watch: failed to rescan config and re-establish watch")?;
                     let new_watcher = new_watcher
                         .start_watching(tx.clone())
@@ -148,7 +133,7 @@ pub(crate) fn watch_for_config_changes<R: Runtime>(
                             // ignore send error, rx can't be disconnected if we are here
                             let _ = tx.send(Event::Rescan);
                             let mut new_watcher = FileWatcher::builder();
-                            let _found_files = new_watcher.prepare(&sources).context(
+                            let _found_files = prepare(&mut new_watcher, &sources).context(
                                 "FS watch: failed to rescan config and re-establish watch: {}",
                             )?;
                             let new_watcher = new_watcher.start_watching(tx.clone()).context(
@@ -234,6 +219,22 @@ impl ReconfigurableModule for Application {
     }
 }
 
+/// Find the configuration files and prepare the watcher
+fn prepare<'a>(
+    watcher: &mut FileWatcherBuilder,
+    sources: &'a ConfigurationSources,
+) -> anyhow::Result<FoundConfigFiles<'a>> {
+    let sources = sources.scan()?;
+    for source in sources.iter() {
+        match source {
+            ConfigurationSource::Dir(dir) => watcher.watch_dir(dir)?,
+            ConfigurationSource::File(file) => watcher.watch_file(file)?,
+            ConfigurationSource::Verbatim(_) => {}
+        }
+    }
+    Ok(sources)
+}
+
 /// Reload the configuration files, apply the runtime configuration, and
 /// reconfigure the client as much as we can.
 ///
@@ -260,168 +261,4 @@ fn reconfigure(
     }
 
     Ok(has_modules && config.0.application().watch_configuration)
-}
-
-/// A wrapper around `notify::RecommendedWatcher` to watch a set of parent
-/// directories in order to learn about changes in some specific files that they
-/// contain.
-///
-/// The `Watcher` implementation in `notify` has a weakness: it gives sensible
-/// results when you're watching directories, but if you start watching
-/// non-directory files, it won't notice when those files get replaced.  That's
-/// a problem for users who want to change their configuration atomically by
-/// making new files and then moving them into place over the old ones.
-///
-/// For more background on the issues with `notify`, see
-/// <https://github.com/notify-rs/notify/issues/165> and
-/// <https://github.com/notify-rs/notify/pull/166>.
-///
-/// TODO: Someday we might want to make this code exported someplace.  If we do,
-/// we should test it, and improve its API a lot.
-struct FileWatcher {
-    /// An underlying `notify` watcher that tells us about directory changes.
-    // this field is kept only so the watcher is not dropped
-    _watcher: notify::RecommendedWatcher,
-}
-
-impl FileWatcher {
-    /// Create a `FileWatcherBuilder`
-    fn builder() -> FileWatcherBuilder {
-        FileWatcherBuilder::new()
-    }
-}
-
-/// Builder used to configure a [`FileWatcher`] before it starts watching for changes.
-struct FileWatcherBuilder {
-    /// The list of directories that we're currently watching.
-    watching_dirs: HashSet<PathBuf>,
-    /// The list of files we actually care about.
-    watching_files: HashSet<PathBuf>,
-}
-
-impl FileWatcherBuilder {
-    /// Create a `FileWatcherBuilder`
-    fn new() -> Self {
-        FileWatcherBuilder {
-            watching_dirs: HashSet::new(),
-            watching_files: HashSet::new(),
-        }
-    }
-
-    /// Find the configuration files and prepare the watcher
-    fn prepare<'a>(
-        &mut self,
-        sources: &'a ConfigurationSources,
-    ) -> anyhow::Result<FoundConfigFiles<'a>> {
-        let sources = sources.scan()?;
-        for source in sources.iter() {
-            match source {
-                ConfigurationSource::Dir(dir) => self.watch_dir(dir)?,
-                ConfigurationSource::File(file) => self.watch_file(file)?,
-                ConfigurationSource::Verbatim(_) => {}
-            }
-        }
-        Ok(sources)
-    }
-
-    /// Add a single file (not a directory) to the list of things to watch.
-    ///
-    /// Idempotent: does nothing if we're already watching that file.
-    fn watch_file<P: AsRef<Path>>(&mut self, path: P) -> anyhow::Result<()> {
-        self.watch_just_parents(path.as_ref())?;
-        Ok(())
-    }
-
-    /// Add a directory (but not any subdirs) to the list of things to watch.
-    ///
-    /// Idempotent.
-    fn watch_dir<P: AsRef<Path>>(&mut self, path: P) -> anyhow::Result<()> {
-        let path = self.watch_just_parents(path.as_ref())?;
-        self.watch_just_abs_dir(&path);
-        Ok(())
-    }
-
-    /// Add the parents of `path` to the list of things to watch.
-    ///
-    /// Returns the absolute path of `path`.
-    ///
-    /// Idempotent.
-    fn watch_just_parents(&mut self, path: &Path) -> anyhow::Result<PathBuf> {
-        // Make the path absolute (without necessarily making it canonical).
-        //
-        // We do this because `notify` reports all of its events in terms of
-        // absolute paths, so if we were to tell it to watch a directory by its
-        // relative path, we'd get reports about the absolute paths of the files
-        // in that directory.
-        let cwd = std::env::current_dir()?;
-        let path = cwd.join(path);
-        debug_assert!(path.is_absolute());
-
-        // See what directory we should watch in order to watch this file.
-        let watch_target = match path.parent() {
-            // The file has a parent, so watch that.
-            Some(parent) => parent,
-            // The file has no parent.  Given that it's absolute, that means
-            // that we're looking at the root directory.  There's nowhere to go
-            // "up" from there.
-            None => path.as_ref(),
-        };
-
-        self.watch_just_abs_dir(watch_target);
-
-        // Note this file as one that we're watching, so that we can see changes
-        // to it later on.
-        self.watching_files.insert(path.clone());
-
-        Ok(path)
-    }
-
-    /// Add just this (absolute) directory to the list of things to watch.
-    ///
-    /// Does not watch any of the parents.
-    ///
-    /// Idempotent.
-    fn watch_just_abs_dir(&mut self, watch_target: &Path) {
-        self.watching_dirs.insert(watch_target.into());
-    }
-
-    /// Build a `FileWatcher` and start sending events to `tx`.
-    ///
-    /// For the watching to be reliably effective (race-free), the config must be read
-    /// *after* this point, using the `FoundConfigFiles` returned by `prepare`.
-    fn start_watching(self, tx: Sender<Event>) -> anyhow::Result<FileWatcher> {
-        let event_sender = move |event: notify::Result<notify::Event>| {
-            let watching = |f| self.watching_files.contains(f);
-            // filter events we don't want and map to event code
-            let event = match event {
-                Ok(event) => {
-                    if event.need_rescan() {
-                        Some(Event::Rescan)
-                    } else if event.paths.iter().any(watching) {
-                        Some(Event::FileChanged)
-                    } else {
-                        None
-                    }
-                }
-                Err(error) => {
-                    if error.paths.iter().any(watching) {
-                        Some(Event::FileChanged)
-                    } else {
-                        None
-                    }
-                }
-            };
-            if let Some(event) = event {
-                let _ = tx.send(event);
-            };
-        };
-
-        let mut watcher = notify::RecommendedWatcher::new(event_sender, notify::Config::default())?;
-
-        for dir in self.watching_dirs {
-            watcher.watch(&dir, notify::RecursiveMode::NonRecursive)?;
-        }
-
-        Ok(FileWatcher { _watcher: watcher })
-    }
 }
---
+++ crates/tor-config/src/file_watcher.rs
@@ -0,0 +1,195 @@
+//! Code to watch configuration files for any changes.
+
+use std::collections::HashSet;
+use std::io;
+use std::path::{Path, PathBuf};
+use std::sync::{mpsc, Arc};
+
+use notify::Watcher;
+
+/// `Result` whose `Err` is [`FileWatcherBuildError`].
+pub type Result<T> = std::result::Result<T, FileWatcherBuildError>;
+
+/// A wrapper around `notify::RecommendedWatcher` to watch a set of parent
+/// directories in order to learn about changes in some specific files that they
+/// contain.
+///
+/// The `Watcher` implementation in `notify` has a weakness: it gives sensible
+/// results when you're watching directories, but if you start watching
+/// non-directory files, it won't notice when those files get replaced.  That's
+/// a problem for users who want to change their configuration atomically by
+/// making new files and then moving them into place over the old ones.
+///
+/// For more background on the issues with `notify`, see
+/// <https://github.com/notify-rs/notify/issues/165> and
+/// <https://github.com/notify-rs/notify/pull/166>.
+///
+/// TODO: Someday we might want to make this code exported someplace.  If we do,
+/// we should test it, and improve its API a lot.
+pub struct FileWatcher {
+    /// An underlying `notify` watcher that tells us about directory changes.
+    // this field is kept only so the watcher is not dropped
+    _watcher: notify::RecommendedWatcher,
+}
+
+impl FileWatcher {
+    /// Create a `FileWatcherBuilder`
+    pub fn builder() -> FileWatcherBuilder {
+        FileWatcherBuilder::new()
+    }
+}
+
+/// Event possibly triggering a configuration reload
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum Event {
+    /// SIGHUP has been received.
+    #[cfg(target_family = "unix")]
+    SigHup,
+    /// Some files may have been modified.
+    FileChanged,
+    /// Some filesystem events may have been missed.
+    Rescan,
+}
+
+/// Builder used to configure a [`FileWatcher`] before it starts watching for changes.
+pub struct FileWatcherBuilder {
+    /// The list of directories that we're currently watching.
+    watching_dirs: HashSet<PathBuf>,
+    /// The list of files we actually care about.
+    watching_files: HashSet<PathBuf>,
+}
+
+impl FileWatcherBuilder {
+    /// Create a `FileWatcherBuilder`
+    pub fn new() -> Self {
+        FileWatcherBuilder {
+            watching_dirs: HashSet::new(),
+            watching_files: HashSet::new(),
+        }
+    }
+
+    /// Add a single file (not a directory) to the list of things to watch.
+    ///
+    /// Idempotent: does nothing if we're already watching that file.
+    pub fn watch_file<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
+        self.watch_just_parents(path.as_ref())?;
+        Ok(())
+    }
+
+    /// Add a directory (but not any subdirs) to the list of things to watch.
+    ///
+    /// Idempotent.
+    pub fn watch_dir<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
+        let path = self.watch_just_parents(path.as_ref())?;
+        self.watch_just_abs_dir(&path);
+        Ok(())
+    }
+
+    /// Add the parents of `path` to the list of things to watch.
+    ///
+    /// Returns the absolute path of `path`.
+    ///
+    /// Idempotent.
+    fn watch_just_parents(&mut self, path: &Path) -> Result<PathBuf> {
+        // Make the path absolute (without necessarily making it canonical).
+        //
+        // We do this because `notify` reports all of its events in terms of
+        // absolute paths, so if we were to tell it to watch a directory by its
+        // relative path, we'd get reports about the absolute paths of the files
+        // in that directory.
+        let cwd = std::env::current_dir()
+            .map_err(|e| FileWatcherBuildError::CurrentDirectory(Arc::new(e)))?;
+        let path = cwd.join(path);
+        debug_assert!(path.is_absolute());
+
+        // See what directory we should watch in order to watch this file.
+        let watch_target = match path.parent() {
+            // The file has a parent, so watch that.
+            Some(parent) => parent,
+            // The file has no parent.  Given that it's absolute, that means
+            // that we're looking at the root directory.  There's nowhere to go
+            // "up" from there.
+            None => path.as_ref(),
+        };
+
+        self.watch_just_abs_dir(watch_target);
+
+        // Note this file as one that we're watching, so that we can see changes
+        // to it later on.
+        self.watching_files.insert(path.clone());
+
+        Ok(path)
+    }
+
+    /// Add just this (absolute) directory to the list of things to watch.
+    ///
+    /// Does not watch any of the parents.
+    ///
+    /// Idempotent.
+    fn watch_just_abs_dir(&mut self, watch_target: &Path) {
+        self.watching_dirs.insert(watch_target.into());
+    }
+
+    /// Build a `FileWatcher` and start sending events to `tx`.
+    pub fn start_watching(self, tx: mpsc::Sender<Event>) -> Result<FileWatcher> {
+        let event_sender = move |event: notify::Result<notify::Event>| {
+            let watching = |f| self.watching_files.contains(f);
+            // filter events we don't want and map to event code
+            let event = match event {
+                Ok(event) => {
+                    if event.need_rescan() {
+                        Some(Event::Rescan)
+                    } else if event.paths.iter().any(watching) {
+                        Some(Event::FileChanged)
+                    } else {
+                        None
+                    }
+                }
+                Err(error) => {
+                    if error.paths.iter().any(watching) {
+                        Some(Event::FileChanged)
+                    } else {
+                        None
+                    }
+                }
+            };
+            if let Some(event) = event {
+                let _ = tx.send(event);
+            };
+        };
+
+        let mut watcher = notify::RecommendedWatcher::new(event_sender, notify::Config::default())
+            .map_err(Arc::new)?;
+
+        for dir in self.watching_dirs {
+            watcher
+                .watch(&dir, notify::RecursiveMode::NonRecursive)
+                .map_err(Arc::new)?;
+        }
+
+        Ok(FileWatcher { _watcher: watcher })
+    }
+}
+
+impl Default for FileWatcherBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// An error coming from a [`FileWatcherBuilder`].
+#[derive(Debug, Clone, thiserror::Error)]
+#[non_exhaustive]
+pub enum FileWatcherBuildError {
+    /// Invalid current working directory.
+    ///
+    /// This error can happen if the current directory does not exist,
+    /// or if we don't have the necessary permissions to access it.
+    #[error("Invalid current working directory")]
+    CurrentDirectory(#[source] Arc<io::Error>),
+
+    /// Encountered a problem while creating a `Watcher`.
+    #[error("Problem creating Watcher")]
+    Notify(#[from] Arc<notify::Error>),
+}
