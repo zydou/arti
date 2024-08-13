3d0c1bb3809af575d0c519efb43f5e2d197047c5
Nick Mathewson <nickm@torproject.org>
2024-08-13 08:25:52 -0400

    Add a missing incantation to fix doc(cfg=...).
    Without this, we get a warning when we run `cargo doc`.

--- crates/tor-rpcbase/src/lib.rs
+++ crates/tor-rpcbase/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
 #![doc = include_str!("../README.md")]
 // @@ begin lint list maintained by maint/add_warning @@
 #![allow(renamed_and_removed_lints)] // @@REMOVE_WHEN(ci_arti_stable)
