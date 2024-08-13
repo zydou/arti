2720feea6bb1de82c8a72100536edde64d8a1ea9
Gabriela Moldovan <gabi@torproject.org>
2024-08-13 11:00:02 +0100

    tor-hsservice: Move authorized_clients out of RunningOnionService.
    We now create the authorized_clients in the publisher (we don't need the
    authorized_clients anywhere else, so it makes little sense to keep them
    in `RunningOnionService`).

--- crates/tor-hsservice/src/lib.rs
+++ crates/tor-hsservice/src/lib.rs
@@ -82,7 +82,6 @@ pub mod time_store_for_doctests_unstable_no_semver_guarantees {
     pub use crate::time_store::*;
 }
 
-use config::restricted_discovery::RestrictedDiscoveryKeys;
 use internal_prelude::*;
 
 // ---------- public exports ----------
@@ -127,13 +126,6 @@ pub struct RunningOnionService {
     nickname: HsNickname,
     /// The key manager, used for accessing the underlying key stores.
     keymgr: Arc<KeyMgr>,
-    /// The restricted discovery authorized clients.
-    ///
-    /// Set to `None` if restricted discovery mode is disabled.
-    //
-    // Not currently used (will be used when we add support for live updating the client keys).
-    #[allow(unused)]
-    authorized_clients: Arc<Mutex<Option<RestrictedDiscoveryKeys>>>,
 }
 
 /// Implementation details for an onion service.
@@ -308,16 +300,6 @@ impl OnionService {
             .storage_handle("iptpub")
             .map_err(StartupError::StateDirectoryInaccessible)?;
 
-        let authorized_clients = config.restricted_discovery.read_keys();
-
-        if matches!(authorized_clients.as_ref(), Some(c) if c.is_empty()) {
-            warn!(
-                "Running in restricted discovery mode, but we have no authorized clients. Service will be unreachable"
-            );
-        }
-
-        let authorized_clients = Arc::new(Mutex::new(authorized_clients));
-
         let (rend_req_tx, rend_req_rx) = mpsc::channel(32);
         let (shutdown_tx, shutdown_rx) = broadcast::channel(0);
         let (config_tx, config_rx) = postage::watch::channel_with(Arc::new(config));
@@ -351,13 +333,11 @@ impl OnionService {
             config_rx,
             status_tx.clone().into(),
             Arc::clone(&keymgr),
-            Arc::clone(&authorized_clients),
         );
 
         let svc = Arc::new(RunningOnionService {
             nickname,
             keymgr,
-            authorized_clients: Arc::clone(&authorized_clients),
             inner: Mutex::new(SvcInner {
                 config_tx,
                 _shutdown_tx: shutdown_tx,
