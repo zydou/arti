fb120c9cbe874c55984bf4363a0227fb9fa2223e
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-08-22 14:42:56 +0100

    byte_qty: provide TryFrom<u64> and <f64>

--- crates/tor-basic-utils/src/byte_qty.rs
+++ crates/tor-basic-utils/src/byte_qty.rs
@@ -7,11 +7,14 @@
 // There is also humansize, but that just does printing.
 
 use derive_more::{Deref, DerefMut, From, Into};
+use thiserror::Error;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
 use std::fmt::{self, Display};
+
+use InvalidByteQty as IBQ;
 
 /// Quantity of memory used, measured in bytes.
 ///
@@ -26,6 +29,25 @@ use std::fmt::{self, Display};
 #[allow(clippy::exhaustive_structs)] // this is a behavioural newtype wrapper
 pub struct ByteQty(pub usize);
 
+/// Error parsing (or deserialising) a [`ByteQty`]
+#[derive(Error, Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub enum InvalidByteQty {
+    /// Value bigger than `usize::MAX`
+    #[error(
+        "size/quantity outside range supported on this system (max is {} B)",
+        usize::MAX
+    )]
+    Overflow,
+    /// Negative value
+    #[error("size/quantity cannot be negative")]
+    Negative,
+    /// NaN
+    #[error("size/quantity cannot be obtained from a floating point NaN")]
+    NaN,
+}
+
+//---------- inherent methods ----------
+
 impl ByteQty {
     /// Maximum for the type
     pub const MAX: ByteQty = ByteQty(usize::MAX);
@@ -39,10 +61,41 @@ impl ByteQty {
     }
 }
 
+//---------- printing ----------
+
 impl Display for ByteQty {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mb = self.0 as f32 / (1024. * 1024.);
         write!(f, "{:.2}MiB", mb)
+    }
+}
+
+//---------- incoming conversions ----------
+
+// We don't provide Into<u64> or Into<f64> because they're actually quite faffsome
+// due to all the corner cases.  We only provide these two, because we need them
+// ourselves for parsing and deserialisation.
+
+impl TryFrom<u64> for ByteQty {
+    type Error = InvalidByteQty;
+    fn try_from(v: u64) -> Result<ByteQty, IBQ> {
+        let v = v.try_into().map_err(|_| IBQ::Overflow)?;
+        Ok(ByteQty(v))
+    }
+}
+
+impl TryFrom<f64> for ByteQty {
+    type Error = InvalidByteQty;
+    fn try_from(f: f64) -> Result<ByteQty, IBQ> {
+        if f.is_nan() {
+            Err(IBQ::NaN)
+        } else if f > (usize::MAX as f64) {
+            Err(IBQ::Overflow)
+        } else if f >= 0. {
+            Ok(ByteQty(f as usize))
+        } else {
+            Err(IBQ::Negative)
+        }
     }
 }
 
@@ -71,5 +124,28 @@ mod test {
         chk(10 * 1024, "0.01MiB");
         chk(1024 * 1024, "1.00MiB");
         chk(1000 * 1024 * 1024, "1000.00MiB");
+    }
+
+    #[test]
+    fn convert() {
+        fn chk(a: impl TryInto<ByteQty, Error = IBQ>, b: Result<ByteQty, IBQ>) {
+            assert_eq!(a.try_into(), b);
+        }
+        fn chk_y(a: impl TryInto<ByteQty, Error = IBQ>, v: usize) {
+            chk(a, Ok(ByteQty(v)));
+        }
+
+        chk_y(0.0_f64, 0);
+        chk_y(1.0_f64, 1);
+        chk_y(f64::from(u32::MAX), u32::MAX as usize);
+        chk_y(-0.0_f64, 0);
+
+        chk(-0.01_f64, Err(IBQ::Negative));
+        chk(1.0e100_f64, Err(IBQ::Overflow));
+        chk(f64::NAN, Err(IBQ::NaN));
+
+        chk_y(0_u64, 0);
+        chk_y(u64::from(u32::MAX), u32::MAX as usize);
+        // we can't easily test the u64 overflow case without getting arch-specific
     }
 }
