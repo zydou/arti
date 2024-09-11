81d39096a511305c16db5fcade8886ea8b9854b3
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-09-11 11:07:55 +0100

    tor-bytes: derive impl PartialEq for Error
    I just perpetrated a bug by adding a variant to this which ended up
    not PartialEq to itself.  This open-coded match is a footgun.
    
    No functional change.

--- crates/tor-bytes/src/err.rs
+++ crates/tor-bytes/src/err.rs
@@ -3,16 +3,41 @@
 use std::borrow::Cow;
 use std::num::NonZeroUsize;
 
+use derive_deftly::{define_derive_deftly, Deftly};
 use safelog::Sensitive;
 use thiserror::Error;
 use tor_error::{into_internal, Bug};
+
+define_derive_deftly! {
+    /// `impl PartialEq for Error`
+    PartialEqForError expect items:
+
+    impl PartialEq for $ttype {
+        fn eq(&self, other: &Self) -> bool {
+            match (self, other) {
+              $(
+                ${when not(vmeta(never_eq))}
+                #[allow(deprecated)]
+                (${vpat fprefix=a_}, ${vpat fprefix=b_}) => {
+                  $(
+                    if $<a_ $fname> != $<b_ $fname> { return false; }
+                  )
+                    return true;
+                },
+              )
+                (_, _) => false,
+            }
+        }
+    }
+}
 
 /// Error type for decoding Tor objects from bytes.
 //
 // TODO(nickm): This error type could use a redesign: it doesn't do a good job
 // of preserving context.  At the least it should say what kind of object it
 // found any given problem in.
-#[derive(Error, Debug, Clone)]
+#[derive(Error, Debug, Clone, Deftly)]
+#[derive_deftly(PartialEqForError)]
 #[non_exhaustive]
 pub enum Error {
     /// Tried to read something, but we didn't find enough bytes.
@@ -51,23 +76,8 @@ pub enum Error {
     /// We use this one in lieu of calling assert() and expect() and
     /// unwrap() from within parsing code.
     #[error("Internal error")]
+    #[deftly(never_eq)] // an internal error is equal to nothing, not even itself.
     Bug(#[from] tor_error::Bug),
-}
-
-impl PartialEq for Error {
-    fn eq(&self, other: &Self) -> bool {
-        use Error::*;
-        match (self, other) {
-            (Truncated { deficit: a }, Truncated { deficit: b }) => a == b,
-            (ExtraneousBytes, ExtraneousBytes) => true,
-            #[allow(deprecated)]
-            (BadMessage(a), BadMessage(b)) => a == b,
-            (InvalidMessage(a), InvalidMessage(b)) => a == b,
-            (BadLengthValue, BadLengthValue) => true,
-            // notably, this means that an internal error is equal to nothing, not even itself.
-            (_, _) => false,
-        }
-    }
 }
 
 impl Error {
