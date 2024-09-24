b6237cfee240ba6f840c02e7cf209a59aa7ed1f4
Steven Engler <opara@torproject.org>
2024-09-23 22:50:45 -0400

    obfs4-checker: update axum to 0.7.6

--- examples/gsoc2023/obfs4-checker/src/main.rs
+++ examples/gsoc2023/obfs4-checker/src/main.rs
@@ -173,8 +173,10 @@ async fn main() {
         .route("/updates", get(updates_callback));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 5000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+
+    let listener = tokio::net::TcpListener::bind(addr)
         .await
-        .unwrap();
+        .expect("failed to bind to TCP address");
+
+    axum::serve(listener, app).await.expect("failed to serve");
 }
