0f4e019cece14029a8ca385340604938a0aa8527
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-10-07 15:58:41 +0100

    mq docs: Move arti queues text to tor_proto::memquota

--- crates/tor-proto/src/memquota.rs
+++ crates/tor-proto/src/memquota.rs
@@ -2,6 +2,30 @@
 //!
 //! We make these newtypes because we otherwise have a confusing a maze of
 //! identical-looking, but supposedly semantically different, [`Account`]s.
+//!
+//! # Memory tracking architecture in Arti
+//!
+//! Here are some things that have queued data:
+//!
+//!   * Each Tor stream has a queue or two.
+//!   * Each circuit also has one or more queues _not_ associated with a single stream.
+//!   * Tor channels themselves also can have one or more queues.
+//!   * Each TLS connection can itself have internal buffers.  We can also consider these queues.
+//!   * Each TCP socket can also be buffered.  We can also consider these buffers to be queues.
+//!
+//! When we run out of memory, we find the queues above that have the oldest data.
+//! When we find one, we will kill it.
+//!
+//! If we kill a stream, we will also kill the circuit that it is on.
+//! Killing a circuit queue kills every queue associated with that circuit,
+//! and every queue associated with every one of its Tor streams.
+//! Killing a channel kills every queue associated with that channel,
+//! as well as every circuit associated with that channel,
+//! and every Tor stream associated with one of those circuits.
+//!
+//! Thus, killing a single queue will reclaim the memory associated with several other queues.
+//!
+//! **TODO - this is not yet actually implemented**
 
 use derive_deftly::{define_derive_deftly, Deftly};
 use tor_memquota::Account;
