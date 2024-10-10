b01ddafc880cc9d476fdf020f128a8f28372adae
David Goulet <dgoulet@torproject.org>
2024-10-10 10:53:01 -0400

    arti-relay: Add a binary to the crate
    At the moment, it is an empty main() acting as a place holder for this
    crate to become solely a binary crate.
    
    Write up a basic README.md in order to explain the current state. Next
    commit will remove the libary component by renaming lib.rs
    
    Part of #1674
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

---
+++ crates/arti-relay/src/main.rs
@@ -0,0 +1,8 @@
+//! A relay binary use to join the Tor network to relay anonymous communication.
+//!
+//! NOTE: This binary is still highly experimental as in in active development, not stable and
+//! without any type of guarantee of running or even working.
+
+fn main() {
+    todo!()
+}
