24f3fe2e0fedccb1e1e40620c5570e0b66a1b490
Gabriela Moldovan <gabi@torproject.org>
2024-10-15 10:46:01 +0100

    tor-keymgr: Add missing docs for keypair_specifier.
    This is a follow-up to !2393, which added support for the
    `key_specifier` top-level attribute.

--- crates/tor-keymgr/src/key_specifier/derive.rs
+++ crates/tor-keymgr/src/key_specifier/derive.rs
@@ -386,6 +386,18 @@ define_derive_deftly! {
     ///    (Can be even used before a denotator component,
     ///    to add a final fixed path component.)
     ///
+    ///  * **`#[deftly(key_specifier = "type")]`** (field):
+    ///    If this is the specifier for a public key, the specifier for
+    ///    the corresponding keypair type.
+    ///
+    ///    If not specified, the generated [`KeySpecifier::keypair_specifier`]
+    ///    implementation will always return `None`.
+    //
+    //     NOTE: The `KeySpecifier::keypair_specifier` implementation
+    //     of the `ArtiPath` of a public key will always return `None`,
+    //     even if the public key specifier it represents has a keypair specifier.
+    //
+    ///
     export KeySpecifier for struct:
 
     // A condition that evaluates to `true` for path fields.
