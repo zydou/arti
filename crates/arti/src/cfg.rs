3f62b31991b8bacd47e195579e5b2dd5ecf157ed
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-10-22 13:28:35 +0100

    arti cfg tests: Clarify here and there
    As per
      https://gitlab.torproject.org/tpo/core/arti/-/merge_requests/2561#note_3097443

--- crates/arti/src/cfg.rs
+++ crates/arti/src/cfg.rs
@@ -1245,7 +1245,8 @@ example config file {which:?}, uncommented={uncommented:?}
                 let m = result.unwrap_err().report().to_string();
                 assert!(m.contains("cargo feature `memquota` disabled"), "{m:?}");
             } else {
-                // The `tor-memquota/memquota` feature is enabled by default there, but not here.
+                // The `tor-memquota/memquota` feature is enabled by default in tor-memquota,
+                // but the corresponding `memquota` feature is but not enabled here in `arti`.
                 // (TODO #351 ^ it isn't yet, actually, but it will be)
                 // so cargo --workspace enables it in a way we can't tell.  See arti/build.rs.
                 println!("not testing memquota config, cannot figure out if it's enabled");
