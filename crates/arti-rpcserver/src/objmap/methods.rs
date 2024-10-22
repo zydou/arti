fe1c2672d45391480a402b7cf3d2f75f4c23a2ac
Nick Mathewson <nickm@torproject.org>
2024-10-22 10:23:10 -0400

    rpc: Move "rpc:release" to be a method on the rpc object.
    Now, instead of telling the session or the connection to drop the
    object ID, we tell the object ID to go away.
    
    Closes #1663.

--- crates/arti-rpcserver/src/objmap/methods.rs
+++ crates/arti-rpcserver/src/objmap/methods.rs
@@ -5,10 +5,9 @@
 //! because they interact with the object map system in special ways.)
 
 use derive_deftly::Deftly;
+use futures::FutureExt as _;
 use std::sync::Arc;
 use tor_rpcbase::{self as rpc, templates::*};
-
-use crate::RpcSession;
 
 /// Release a single ObjectID.
 ///
@@ -19,34 +18,35 @@ use crate::RpcSession;
 /// After calling this method, the provided ObjectID will no longer be usable,
 /// but other ObjectIDs for the same object may still exist.
 ///
-/// TODO RPC BREAKING: We're going to move this method, turn it into a
-/// globally implemented
+/// TODO RPC: Releasing a weak reference is perilous and hard-to-define
+/// based on how we have implemented our object ids.  If you tell the objmap
+/// to "release" a single name for a weak reference, you are releasing every
+/// name for that weak reference, which may have surprising results.
+/// See also #838.
 #[derive(Debug, serde::Deserialize, Deftly)]
 #[derive_deftly(DynMethod)]
-#[deftly(rpc(method_name = "rpc:release"))]
-struct RpcRelease {
-    /// The object to release. Must be a strong reference.
-    ///
-    /// TODO RPC: Releasing a weak reference is perilous and hard-to-define
-    /// based on how we have implemented our object ids.  If you tell the objmap
-    /// to "release" a single name for a weak reference, you are releasing every
-    /// name for that weak reference, which may have surprising results.
-    ///
-    /// This might be a sign of a design problem.
-    obj: rpc::ObjectId,
-}
+#[deftly(rpc(method_name = "rpc:release", bypass_method_dispatch))]
+struct RpcRelease {}
 
 impl rpc::RpcMethod for RpcRelease {
     type Output = rpc::Nil;
     type Update = rpc::NoUpdates;
 }
 
-/// Implementation for calling "release" on a Session.
-async fn rpc_release(
-    _obj: Arc<RpcSession>,
-    method: Box<RpcRelease>,
-    ctx: Arc<dyn rpc::Context>,
-) -> Result<rpc::Nil, rpc::RpcError> {
-    ctx.release_owned(&method.obj)?;
-    Ok(rpc::Nil::default())
+impl rpc::DynMethod for RpcRelease {
+    fn bypass_method_dispatch(&self) -> bool {
+        true
+    }
+
+    fn invoke_without_dispatch(
+        &self,
+        ctx: Arc<dyn rpc::Context>,
+        obj_id: &rpc::ObjectId,
+    ) -> Result<tor_rpcbase::dispatch::RpcResultFuture, tor_rpcbase::InvokeError> {
+        let result = match ctx.release_owned(obj_id) {
+            Ok(()) => Ok(Box::new(rpc::NIL) as _),
+            Err(e) => Err(rpc::RpcError::from(e)),
+        };
+        Ok(futures::future::ready(result).boxed())
+    }
 }
