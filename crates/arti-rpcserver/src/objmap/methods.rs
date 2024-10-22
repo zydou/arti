318aaa2cfc571d99bcd711484e233420b0985c48
Nick Mathewson <nickm@torproject.org>
2024-10-22 08:09:09 -0400

    rpc: Move some object-id-manipulation methods to a new module.
    These will have different implementations soon; this is a more
    logical place for them.

---
+++ crates/arti-rpcserver/src/objmap/methods.rs
@@ -0,0 +1,61 @@
+//! Implementations for rpc methods that interact with
+//! object IDs directly.
+//!
+//! (These methods do not use the regular dispatch system
+//! because they interact with the object map system in special ways.)
+
+use derive_deftly::Deftly;
+use std::sync::Arc;
+use tor_rpcbase::{self as rpc, templates::*};
+
+use crate::RpcSession;
+
+/// Release a single ObjectID.
+///
+/// Only works if the ObjectID is strong reference (also known as a "handle"):
+/// see RPC specification for more information on the distinction.
+/// (We intend to relax this requirement in the future.)
+///
+/// After calling this method, the provided ObjectID will no longer be usable,
+/// but other ObjectIDs for the same object may still exist.
+///
+/// TODO RPC BREAKING: We're going to move this method, turn it into a
+/// globally implemented
+#[derive(Debug, serde::Deserialize, Deftly)]
+#[derive_deftly(DynMethod)]
+#[deftly(rpc(method_name = "rpc:release"))]
+struct RpcRelease {
+    /// The object to release. Must be a strong reference.
+    ///
+    /// TODO RPC: Releasing a weak reference is perilous and hard-to-define
+    /// based on how we have implemented our object ids.  If you tell the objmap
+    /// to "release" a single name for a weak reference, you are releasing every
+    /// name for that weak reference, which may have surprising results.
+    ///
+    /// This might be a sign of a design problem.
+    obj: rpc::ObjectId,
+}
+
+/// RPC method to release a single strong reference, creating a weak reference
+/// in its place.
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[allow(unused)] //TODO RPC
+struct RpcDowngrade {
+    /// The object to downgrade
+    obj: rpc::ObjectId,
+}
+
+impl rpc::RpcMethod for RpcRelease {
+    type Output = rpc::Nil;
+    type Update = rpc::NoUpdates;
+}
+
+/// Implementation for calling "release" on a Session.
+async fn rpc_release(
+    _obj: Arc<RpcSession>,
+    method: Box<RpcRelease>,
+    ctx: Arc<dyn rpc::Context>,
+) -> Result<rpc::Nil, rpc::RpcError> {
+    ctx.release_owned(&method.obj)?;
+    Ok(rpc::Nil::default())
+}
--- crates/arti-rpcserver/src/session.rs
+++ crates/arti-rpcserver/src/session.rs
@@ -78,55 +78,6 @@ impl RpcSession {
     }
 }
 
-/// Release a single ObjectID.
-///
-/// Only works if the ObjectID is strong reference (also known as a "handle"):
-/// see RPC specification for more information on the distinction.
-/// (We intend to relax this requirement in the future.)
-///
-/// After calling this method, the provided ObjectID will no longer be usable,
-/// but other ObjectIDs for the same object may still exist.
-///
-/// TODO RPC BREAKING: We're going to move this method, turn it into a
-/// globally implemented
-#[derive(Debug, serde::Deserialize, Deftly)]
-#[derive_deftly(DynMethod)]
-#[deftly(rpc(method_name = "rpc:release"))]
-struct RpcRelease {
-    /// The object to release. Must be a strong reference.
-    ///
-    /// TODO RPC: Releasing a weak reference is perilous and hard-to-define
-    /// based on how we have implemented our object ids.  If you tell the objmap
-    /// to "release" a single name for a weak reference, you are releasing every
-    /// name for that weak reference, which may have surprising results.
-    ///
-    /// This might be a sign of a design problem.
-    obj: rpc::ObjectId,
-}
-/// RPC method to release a single strong reference, creating a weak reference
-/// in its place.
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-#[allow(unused)] //TODO RPC
-struct RpcDowngrade {
-    /// The object to downgrade
-    obj: rpc::ObjectId,
-}
-
-impl rpc::RpcMethod for RpcRelease {
-    type Output = rpc::Nil;
-    type Update = rpc::NoUpdates;
-}
-
-/// Implementation for calling "release" on a Session.
-async fn rpc_release(
-    _obj: Arc<RpcSession>,
-    method: Box<RpcRelease>,
-    ctx: Arc<dyn rpc::Context>,
-) -> Result<rpc::Nil, rpc::RpcError> {
-    ctx.release_owned(&method.obj)?;
-    Ok(rpc::Nil::default())
-}
-
 /// A simple temporary method to echo a reply.
 #[derive(Debug, serde::Deserialize, serde::Serialize, Deftly)]
 #[derive_deftly(DynMethod)]
@@ -229,7 +180,6 @@ async fn session_resolve_ptr_with_prefs(
         .map_err(|e| Box::new(into_internal!("unable to delegate to TorClient")(e)) as _)?
 }
 static_rpc_invoke_fn! {
-    rpc_release;
     echo_on_session;
     get_client_on_session;
     isolated_client_on_session;
