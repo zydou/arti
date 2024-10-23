f8e5b7452cd97c58bed91707cf42f29b3b9754de
Gabriela Moldovan <gabi@torproject.org>
2024-10-23 11:27:32 +0100

    tor-relay-crypto: Add a specifier for the public part of the identity keypair.
    We'll eventually need this specifier (for example, if the
    `KS_relayid_ed` private key stored offline, the public part of the
    keypair will be stored in the online keystore, and will be retrievable
    using the new `RelayIdentityPublicKeySpecifier` specifier).

--- crates/tor-relay-crypto/src/pk.rs
+++ crates/tor-relay-crypto/src/pk.rs
@@ -24,6 +24,15 @@ define_ed25519_keypair!(
 /// The key specifier of the relay long-term identity key (RelayIdentityKeypair)
 pub struct RelayIdentityKeypairSpecifier;
 
+#[non_exhaustive]
+#[derive(Deftly, PartialEq, Debug, Constructor)]
+#[derive_deftly(KeySpecifier)]
+#[deftly(prefix = "relay")]
+#[deftly(role = "KP_relayid_ed")]
+#[deftly(summary = "Public part of the relay long-term identity keypair")]
+/// The public part of the long-term identity key of the relay.
+pub struct RelayIdentityPublicKeySpecifier;
+
 define_ed25519_keypair!(
     /// [KP_relaysign_ed] Medium-term signing keypair. Rotated periodically.
     pub RelaySigning
