8f074b049d2f23467bea73dc986f597929f635d6
Steven Engler <opara@torproject.org>
2024-11-13 12:54:07 -0500

    arti-relay: add clap for cli
    The options are inspired by the arti cli interface.

---
+++ crates/arti-relay/src/cli.rs
@@ -0,0 +1,86 @@
+use std::ffi::OsString;
+
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
+/// A Rust Tor relay implementation.
+#[derive(Clone, Debug, Parser)]
+#[command(author = "The Tor Project Developers")]
+#[command(version)]
+pub(crate) struct Cli {
+    #[command(subcommand)]
+    pub(crate) command: Commands,
+
+    /// Log level.
+    #[arg(long, short, global = true)]
+    #[arg(value_name = "LEVEL")]
+    #[clap(default_value_t = LogLevel::Info)]
+    pub(crate) log_level: LogLevel,
+
+    /// Don't check permissions on the files we use.
+    #[arg(long, global = true)]
+    pub(crate) disable_fs_permission_checks: bool,
+
+    /// Override config file parameters, using TOML-like syntax.
+    #[arg(long = "option", short, global = true)]
+    #[arg(value_name = "KEY=VALUE")]
+    pub(crate) options: Vec<String>,
+
+    /// Config file(s) to read.
+    #[arg(long, short, global = true)]
+    #[arg(value_name = "FILE")]
+    #[clap(default_values_t = default_config_files().into_iter().map(CliOsString))]
+    pub(crate) config: Vec<CliOsString>,
+}
+
+/// Main subcommands.
+#[derive(Clone, Debug, Subcommand)]
+pub(crate) enum Commands {
+    /// Run the relay.
+    Run(RunArgs),
+}
+
+/// Arguments when running an Arti relay.
+#[derive(Clone, Debug, Args)]
+pub(crate) struct RunArgs {}
+
+/// Paths used for default configuration files.
+fn default_config_files() -> Vec<OsString> {
+    // TODO: these are temporary default paths
+    vec![
+        "~/.config/arti-relay/arti-relay.toml".into(),
+        "~/.config/arti-relay/arti-relay.d/".into(),
+    ]
+}
+
+/// Log levels allowed by the cli.
+#[derive(Clone, Debug, Eq, PartialEq, ValueEnum)]
+pub(crate) enum LogLevel {
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+impl std::fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Error => write!(f, "error"),
+            Self::Warn => write!(f, "warn"),
+            Self::Info => write!(f, "info"),
+            Self::Debug => write!(f, "debug"),
+            Self::Trace => write!(f, "trace"),
+        }
+    }
+}
+
+/// An [`OsString`] wrapper which implements `Display`; designed for use with the cli help text.
+#[derive(Debug, Clone, Eq, PartialEq, derive_more::From)]
+pub(crate) struct CliOsString(pub(crate) OsString);
+
+impl std::fmt::Display for CliOsString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // we can't (and don't want to) write non-utf-8 bytes in the cli help output
+        self.0.to_string_lossy().fmt(f)
+    }
+}
--- crates/arti-relay/src/main.rs
+++ crates/arti-relay/src/main.rs
@@ -4,10 +4,31 @@
 //! without any type of guarantee of running or even working.
 
 mod builder;
+mod cli;
 mod config;
 mod err;
 mod relay;
 
-fn main() {
-    todo!()
+use clap::Parser;
+
+use crate::relay::TorRelay;
+
+fn main() -> anyhow::Result<()> {
+    let cli = cli::Cli::parse();
+
+    let runtime = create_runtime()?;
+
+    match cli.command {
+        cli::Commands::Run(_args) => {
+            let _relay = TorRelay::with_runtime(runtime).create()?;
+        }
+    }
+
+    Ok(())
+}
+
+/// Create the runtime for the relay.
+fn create_runtime() -> std::io::Result<impl tor_rtcompat::Runtime> {
+    // TODO(arti#1744): we may want to support multiple runtimes
+    tor_rtcompat::PreferredRuntime::create()
 }
