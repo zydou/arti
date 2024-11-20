28b9def7584cdc6265b30cb9d0fd2a1f1e8b2d01
Steven Engler <opara@torproject.org>
2024-11-19 22:58:48 -0500

    tor-netdoc: add clippy allows and `#[non_exhaustive]`

--- crates/tor-netdoc/src/doc/hsdesc/pow/v1_stub.rs
+++ crates/tor-netdoc/src/doc/hsdesc/pow/v1_stub.rs
@@ -12,11 +12,13 @@ use crate::Result;
 /// compiled differently.
 ///
 #[derive(Debug, Clone)]
+#[non_exhaustive]
 pub struct PowParamsV1;
 
 impl PowParamsV1 {
     /// Accept any Item as a 'PowParamsV1' without actually parsing it
     ///
+    #[allow(clippy::unnecessary_wraps)]
     pub(super) fn from_item(_item: &Item<'_, HsInnerKwd>) -> Result<Self> {
         Ok(Self)
     }
