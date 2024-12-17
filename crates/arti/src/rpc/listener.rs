2c60f735f220e8834c016f32f74f2ad6308efd04
Nick Mathewson <nickm@torproject.org>
2024-12-17 12:32:31 -0500

    arti::rpc: Refactor listener options to support defaults properly
    We want to have every option that applies to a connect point (other
    than file or dir) exist both as a default version, and as a per-file
    override.  This approach lets us do so: We store _builders_ for
    these options in the config, and then use ExtendBuilder to merge
    them together.
    
    (There aren't yet any options other than 'enable',
    but there will be before long.)

--- crates/arti/src/rpc/listener.rs
+++ crates/arti/src/rpc/listener.rs
@@ -14,8 +14,8 @@ use fs_mistrust::{anon_home::PathExt as _, Mistrust};
 use serde::{Deserialize, Serialize};
 use tor_basic_utils::PathExt as _;
 use tor_config::{
-    define_map_builder, derive_deftly_template_ExtendBuilder, impl_standard_builder,
-    ConfigBuildError,
+    define_map_builder, derive_deftly_template_ExtendBuilder, extend_builder::ExtendBuilder as _,
+    extend_builder::ExtendStrategy, impl_standard_builder, ConfigBuildError,
 };
 use tor_config_path::{CfgPath, CfgPathResolver};
 use tor_error::internal;
@@ -58,9 +58,26 @@ fn listener_map_defaults() -> BTreeMap<String, RpcListenerConfigBuilder> {
 #[builder(derive(Debug, Serialize, Deserialize))]
 #[derive_deftly(ExtendBuilder)]
 pub(crate) struct RpcListenerConfig {
-    /// If true (the default), arti will try to listen at this connect point.
-    #[builder(default = "true")]
-    enable: bool,
+    /// An builder to determine default connect point options.
+    ///
+    /// If `file` is set, this builder is used directly
+    /// to determine the options for the connect points.
+    ///
+    /// If `dir` is set, this builder defines a set of defaults
+    /// that we can override for each connect point in `file_options`.
+    #[builder(
+        sub_builder(fn_name = "build"),
+        field(
+            // This lets us hold a Builder in the Config too,
+            // so we can use `ExtendBuilder` on it.
+            type = "ConnectPointOptionsBuilder",
+            build = "self.listener_options.clone()"
+        )
+    )]
+    #[builder_field_attr(serde(flatten, default))]
+    #[deftly(extend_builder(sub_builder))]
+    listener_options: ConnectPointOptionsBuilder,
+
     /// A path to a file on disk containing a connect string.
     ///
     /// Exactly one of `file` or `dir` may be set.
@@ -73,15 +90,19 @@ pub(crate) struct RpcListenerConfig {
     /// Exactly one of `file` or `dir` may be set.
     #[builder(setter(strip_option), default)]
     dir: Option<CfgPath>,
-    /// Map from file name within `dir` to options to be set on the individual files.
+    /// Map from file name within `dir` to builders for options on the individual files.
     ///
-    /// If this option is set, `dir` must be set.
-    //
-    // XXXX We want a way to set each of these options on RpcListenerConfig.  Will this involve duplication?
-    #[builder(sub_builder(fn_name = "build"))]
+    /// We hold builders here so that we can use `ExtendBuilder` to derive settings
+    /// using `listener_options` as the defaults.
+    #[builder(
+        sub_builder(fn_name = "build"),
+        // This lets us hold a Builder in the Config too,
+        // so we can use `ExtendBuilder` on it.
+        field(type = "FileOptionsMapBuilder", build = "self.file_options.clone()")
+    )]
     #[builder_field_attr(serde(default))]
     #[deftly(extend_builder(sub_builder))]
-    file_options: FileOptionsMap,
+    file_options: FileOptionsMapBuilder,
 }
 impl_standard_builder! { RpcListenerConfig: !Deserialize !Default }
 
@@ -106,7 +127,8 @@ impl RpcListenerConfigBuilder {
 }
 
 define_map_builder! {
-    /// Builder for the `OverrideMap` within an `RpcListenerConfig`.
+    /// Builder for the `FileOptionsMap` within an `RpcListenerConfig`.
+    #[derive(Eq, PartialEq)]
     struct FileOptionsMapBuilder =>
     type FileOptionsMap = BTreeMap<String, ConnectPointOptions>;
 }
@@ -129,22 +151,27 @@ define_map_builder! {
 #[derive(Debug, Clone, Builder, Eq, PartialEq, Deftly)]
 #[derive_deftly(ExtendBuilder)]
 #[builder(build_fn(error = "ConfigBuildError"))]
-#[builder(derive(Debug, Serialize, Deserialize))]
-struct ConnectPointOptions {
+#[builder(derive(Debug, Serialize, Deserialize, Eq, PartialEq))]
+pub(super) struct ConnectPointOptions {
     /// Used to explicitly disable an entry in a connect point directory.
     #[builder(default = "true")]
     enable: bool,
 }
 impl_standard_builder! { ConnectPointOptions }
 
-impl ConnectPointOptions {
+impl ConnectPointOptionsBuilder {
+    /// Return true if this builder represents an enabled connect point.
+    fn is_enabled(&self) -> bool {
+        self.enable != Some(false)
+    }
+
     /// Return a [`LoadOptions`] corresponding to this OverrideConfig.
     ///
     /// The `LoadOptions` will contain a subset of our own options,
     /// set in order to make [`ParsedConnectPoint::load_dir`] behaved as configured here.
     fn load_options(&self) -> LoadOptions {
         LoadOptionsBuilder::default()
-            .disable(!self.enable)
+            .disable(!self.is_enabled())
             .build()
             .expect("Somehow constructed an invalid LoadOptions")
     }
@@ -162,6 +189,9 @@ pub(super) struct RpcConnInfo {
     pub(super) name: String,
     /// The authentication we require for this RPC connection.
     pub(super) auth: RpcAuth,
+    /// The options for this connect point.
+    #[allow(unused)] // TODO: Once there are more options than "enable", this will be used.
+    pub(super) options: ConnectPointOptions,
 }
 
 impl RpcConnInfo {
@@ -171,28 +201,24 @@ impl RpcConnInfo {
     /// and `filename` (a filename within a connect point directory)
     /// to name the connect point.
     ///
-    /// Uses `auth` and `file_options` as settings to initialize new connections.
+    /// Uses `auth` and `options` as settings to initialize new connections.
     #[allow(clippy::unnecessary_wraps)]
     fn new(
         config_key: &str,
         filename: Option<&Path>,
         auth: RpcAuth,
-        file_options: Option<&BTreeMap<String, ConnectPointOptions>>,
+        options: ConnectPointOptions,
     ) -> anyhow::Result<Self> {
         let name = match filename {
             Some(p) => format!("{} ({})", config_key, p.display_lossy()),
             None => config_key.to_string(),
         };
 
-        if let (Some(fname), Some(file_options)) = (filename, file_options) {
-            // TODO RPC: We'll want to look at options here soon, once there are some.
-            // We'll use them to configure permissions and so forth.
-            let _ignore = fname
-                .to_str()
-                .and_then(|fname_as_str| file_options.get(fname_as_str));
-        }
-
-        Ok(Self { name, auth })
+        Ok(Self {
+            name,
+            auth,
+            options,
+        })
     }
 }
 
@@ -210,7 +236,7 @@ impl RpcListenerConfig {
         resolver: &CfgPathResolver,
         mistrust: &Mistrust,
     ) -> anyhow::Result<Vec<(general::Listener, Arc<RpcConnInfo>, Guard)>> {
-        if !self.enable {
+        if !self.listener_options.is_enabled() {
             return Ok(vec![]);
         }
 
@@ -223,6 +249,11 @@ impl RpcListenerConfig {
                     file.anonymize_home()
                 )
             };
+            let options = self
+                .listener_options
+                .build()
+                .with_context(|| ctx("interpret options"))?;
+
             let conn_pt = ParsedConnectPoint::load_file(file.as_ref(), mistrust)
                 .with_context(|| ctx("load"))?
                 .resolve(resolver)
@@ -238,7 +269,7 @@ impl RpcListenerConfig {
                 .with_context(|| ctx("bind to"))?;
             return Ok(vec![(
                 listener,
-                Arc::new(RpcConnInfo::new(config_key, None, auth, None)?),
+                Arc::new(RpcConnInfo::new(config_key, None, auth, options)?),
                 guard,
             )]);
         }
@@ -267,6 +298,19 @@ impl RpcListenerConfig {
                         dir.anonymize_home()
                     )
                 };
+
+                let options = {
+                    let mut bld = self.listener_options.clone();
+
+                    if let Some(override_options) = path
+                        .to_str()
+                        .and_then(|fname_as_str| self.file_options.get(fname_as_str))
+                    {
+                        bld.extend_from(override_options.clone(), ExtendStrategy::ReplaceLists);
+                    }
+                    bld.build().with_context(|| ctx("interpret options"))?
+                };
+
                 let conn_pt = conn_pt_result
                     .with_context(|| ctx("load"))?
                     .resolve(resolver)
@@ -287,7 +331,7 @@ impl RpcListenerConfig {
                         config_key,
                         Some(path.as_ref()),
                         auth,
-                        Some(&self.file_options),
+                        options,
                     )?),
                     guard,
                 ));
@@ -329,7 +373,7 @@ pub(super) async fn bind_string<R: Runtime>(
             "<default>",
             Some(format!("#{index}").as_ref()),
             auth,
-            None,
+            ConnectPointOptions::default(),
         )?),
         guard,
     ))
