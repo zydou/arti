69d4df8eca23637f68d1e83027dc4f3ebcc52a43
David Goulet <dgoulet@torproject.org>
2025-01-08 14:04:07 -0500

    congestion: Add standard clippy checks to tests
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- crates/tor-proto/src/congestion/rtt.rs
+++ crates/tor-proto/src/congestion/rtt.rs
@@ -231,6 +231,20 @@ impl RoundtripTimeEstimator {
 #[cfg(test)]
 #[allow(clippy::print_stderr)]
 mod test {
+    // @@ begin test lint list maintained by maint/add_warning @@
+    #![allow(clippy::bool_assert_comparison)]
+    #![allow(clippy::clone_on_copy)]
+    #![allow(clippy::dbg_macro)]
+    #![allow(clippy::mixed_attributes_style)]
+    #![allow(clippy::print_stderr)]
+    #![allow(clippy::print_stdout)]
+    #![allow(clippy::single_char_pattern)]
+    #![allow(clippy::unwrap_used)]
+    #![allow(clippy::unchecked_duration_subtraction)]
+    #![allow(clippy::useless_vec)]
+    #![allow(clippy::needless_pass_by_value)]
+    //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
+
     use std::time::{Duration, Instant};
 
     use crate::congestion::test_utils::{new_cwnd, new_rtt_estimator};
