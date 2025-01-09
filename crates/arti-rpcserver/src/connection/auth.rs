b73c57140d23bffc414700fa573bb4416ae87ac8
Nick Mathewson <nickm@torproject.org>
2025-01-09 12:18:15 -0500

    arti-rpcserver: move inherent authentication to its own module.

--- crates/arti-rpcserver/src/connection/auth.rs
+++ crates/arti-rpcserver/src/connection/auth.rs
@@ -11,6 +11,8 @@ use derive_deftly::Deftly;
 use tor_rpcbase as rpc;
 use tor_rpcbase::templates::*;
 
+mod inherent;
+
 /// Information about how an RPC session has been authenticated.
 ///
 /// Currently, this isn't actually used for anything, since there's only one way
@@ -76,75 +78,10 @@ rpc::static_rpc_invoke_fn! {
     conn_authquery;
 }
 
-/// Authenticate on an RPC Connection, returning a new Session.
-///
-/// After connecting to Arti, clients use this method to create a Session,
-/// which they then use to access other functionality.
-///
-/// For now, only the `inherent:unix_path` method is supported;
-/// other methods will be implemented in the future.
-///
-/// You typically won't need to invoke this method yourself;
-/// instead, your RPC library (such as `arti-rpc-client-core`)
-/// should handle it for you.
-#[derive(Debug, serde::Deserialize, Deftly)]
-#[derive_deftly(DynMethod)]
-#[deftly(rpc(method_name = "auth:authenticate"))]
-struct Authenticate {
-    /// The authentication scheme as enumerated in the spec.
-    ///
-    /// TODO RPC: The only supported one for now is "inherent:unix_path"
-    scheme: AuthenticationScheme,
-}
-
-/// A reply from the `Authenticate` method.
-#[derive(Debug, serde::Serialize)]
-struct AuthenticateReply {
-    /// An handle for a `Session` object.
-    session: rpc::ObjectId,
-}
-
-impl rpc::RpcMethod for Authenticate {
-    type Output = AuthenticateReply;
-    type Update = rpc::NoUpdates;
-}
-
 /// An error during authentication.
 #[derive(Debug, Clone, thiserror::Error, serde::Serialize)]
-#[allow(dead_code)] // TODO RPC
-enum AuthenticationFailure {}
-
-impl tor_error::HasKind for AuthenticationFailure {
-    fn kind(&self) -> tor_error::ErrorKind {
-        // TODO RPC not right.
-        tor_error::ErrorKind::LocalProtocolViolation
-    }
-}
-
-/// Invoke the "authenticate" method on a connection.
-///
-/// TODO RPC: This behavior is wrong; we'll need to fix it to be all
-/// capabilities-like.
-async fn authenticate_connection(
-    unauth: Arc<Connection>,
-    method: Box<Authenticate>,
-    ctx: Arc<dyn rpc::Context>,
-) -> Result<AuthenticateReply, rpc::RpcError> {
-    match method.scheme {
-        // For now, we only support AF_UNIX connections, and we assume that if
-        // you have permission to open such a connection to us, you have
-        // permission to use Arti. We will refine this later on!
-        AuthenticationScheme::InherentUnixPath => {}
-    }
-
-    let auth = RpcAuthentication {};
-    let session = {
-        let mgr = unauth.mgr()?;
-        mgr.create_session(&auth)
-    };
-    let session = ctx.register_owned(session);
-    Ok(AuthenticateReply { session })
-}
-rpc::static_rpc_invoke_fn! {
-    authenticate_connection;
+enum AuthenticationFailure {
+    /// The authentication method wasn't one we support.
+    #[error("Tried to use unexpected authentication method")]
+    IncorrectMethod,
 }
