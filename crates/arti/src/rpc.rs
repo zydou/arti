4b2a10c3562f3d990cea1835176f03b2b40b6bbf
Nick Mathewson <nickm@torproject.org>
2025-01-23 11:54:21 -0500

    arti: remove an obsolete rpc todo.
    Information _is_ passed to the RpcMgr, via the argument to new_connection.

--- crates/arti/src/rpc.rs
+++ crates/arti/src/rpc.rs
@@ -184,7 +184,6 @@ async fn run_rpc_listener<R: Runtime>(
     rpc_mgr: Arc<RpcMgr>,
 ) -> Result<()> {
     while let Some((stream, _addr, info)) = incoming.next().await.transpose()? {
-        // TODO RPC: We'll need to pass info (or part of it?) to rpc_mgr.
         debug!("Received incoming RPC connection from {}", &info.name);
 
         let connection = rpc_mgr.new_connection(info.auth.clone());
