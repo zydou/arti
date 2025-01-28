88cfc8968a29ec3d74fdde5be41a9b1e68f62707
Gabriela Moldovan <gabi@torproject.org>
2025-01-28 11:30:33 +0000

    tor-proto: Move CtrlMsg to its own module.
    No functional changes, just code motion.

--- crates/tor-proto/src/circuit/reactor.rs
+++ crates/tor-proto/src/circuit/reactor.rs
@@ -16,6 +16,7 @@
 //!    For half-closed streams, the reactor handles it by calling
 //!    `consume_checked_msg()`.
 
+mod control;
 pub(super) mod syncview;
 
 use super::handshake::RelayCryptLayerProtocol;
@@ -86,6 +87,8 @@ use tor_memquota::derive_deftly_template_HasMemoryCost;
 use tor_memquota::mq_queue::{self, ChannelSpec as _, MpscSpec};
 use tracing::{debug, trace, warn};
 
+pub(super) use control::CtrlMsg;
+
 /// Initial value for outbound flow-control window on streams.
 pub(super) const SEND_WINDOW_INIT: u16 = 500;
 /// Initial value for inbound flow-control window on streams.
@@ -146,187 +149,6 @@ impl Default for CloseStreamBehavior {
     }
 }
 
-/// A message telling the reactor to do something.
-#[derive(educe::Educe)]
-#[educe(Debug)]
-pub(super) enum CtrlMsg {
-    /// Create the first hop of this circuit.
-    Create {
-        /// A oneshot channel on which we'll receive the creation response.
-        recv_created: oneshot::Receiver<CreateResponse>,
-        /// The handshake type to use for the first hop.
-        handshake: CircuitHandshake,
-        /// Other parameters relevant for circuit creation.
-        params: CircParameters,
-        /// Oneshot channel to notify on completion.
-        done: ReactorResultChannel<()>,
-    },
-    /// Extend a circuit by one hop, using the ntor handshake.
-    ExtendNtor {
-        /// The peer that we're extending to.
-        ///
-        /// Used to extend our record of the circuit's path.
-        peer_id: OwnedChanTarget,
-        /// The handshake type to use for this hop.
-        public_key: NtorPublicKey,
-        /// Information about how to connect to the relay we're extending to.
-        linkspecs: Vec<EncodedLinkSpec>,
-        /// Other parameters relevant for circuit extension.
-        params: CircParameters,
-        /// Oneshot channel to notify on completion.
-        done: ReactorResultChannel<()>,
-    },
-    /// Extend a circuit by one hop, using the ntorv3 handshake.
-    #[cfg(feature = "ntor_v3")]
-    ExtendNtorV3 {
-        /// The peer that we're extending to.
-        ///
-        /// Used to extend our record of the circuit's path.
-        peer_id: OwnedChanTarget,
-        /// The handshake type to use for this hop.
-        public_key: NtorV3PublicKey,
-        /// Information about how to connect to the relay we're extending to.
-        linkspecs: Vec<EncodedLinkSpec>,
-        /// Other parameters relevant for circuit extension.
-        params: CircParameters,
-        /// Oneshot channel to notify on completion.
-        done: ReactorResultChannel<()>,
-    },
-    /// Extend the circuit by one hop, in response to an out-of-band handshake.
-    ///
-    /// (This is used for onion services, where the negotiation takes place in
-    /// INTRODUCE and RENDEZVOUS messages.)
-    #[cfg(feature = "hs-common")]
-    ExtendVirtual {
-        /// Which relay cell format to use for this hop.
-        relay_cell_format: RelayCellFormat,
-        /// The cryptographic algorithms and keys to use when communicating with
-        /// the newly added hop.
-        #[educe(Debug(ignore))]
-        cell_crypto: (
-            Box<dyn OutboundClientLayer + Send>,
-            Box<dyn InboundClientLayer + Send>,
-            Option<CircuitBinding>,
-        ),
-        /// A set of parameters used to configure this hop.
-        params: CircParameters,
-        /// Oneshot channel to notify on completion.
-        done: ReactorResultChannel<()>,
-    },
-    /// Begin a stream with the provided hop in this circuit.
-    ///
-    /// Allocates a stream ID, and sends the provided message to that hop.
-    BeginStream {
-        /// The hop number to begin the stream with.
-        hop_num: HopNum,
-        /// The message to send.
-        message: AnyRelayMsg,
-        /// A channel to send messages on this stream down.
-        ///
-        /// This sender shouldn't ever block, because we use congestion control and only send
-        /// SENDME cells once we've read enough out of the other end. If it *does* block, we
-        /// can assume someone is trying to send us more cells than they should, and abort
-        /// the stream.
-        sender: StreamMpscSender<UnparsedRelayMsg>,
-        /// A channel to receive messages to send on this stream from.
-        rx: StreamMpscReceiver<AnyRelayMsg>,
-        /// Oneshot channel to notify on completion, with the allocated stream ID.
-        done: ReactorResultChannel<StreamId>,
-        /// A `CmdChecker` to keep track of which message types are acceptable.
-        cmd_checker: AnyCmdChecker,
-    },
-    /// Close the specified pending incoming stream, sending the provided END message.
-    ///
-    /// A stream is said to be pending if the message for initiating the stream was received but
-    /// not has not been responded to yet.
-    ///
-    /// This should be used by responders for closing pending incoming streams initiated by the
-    /// other party on the circuit.
-    #[cfg(feature = "hs-service")]
-    ClosePendingStream {
-        /// The hop number the stream is on.
-        hop_num: HopNum,
-        /// The stream ID to send the END for.
-        stream_id: StreamId,
-        /// The END message to send, if any.
-        message: CloseStreamBehavior,
-        /// Oneshot channel to notify on completion.
-        done: ReactorResultChannel<()>,
-    },
-    /// Begin accepting streams on this circuit.
-    #[cfg(feature = "hs-service")]
-    AwaitStreamRequest {
-        /// A channel for sending information about an incoming stream request.
-        incoming_sender: StreamReqSender,
-        /// A `CmdChecker` to keep track of which message types are acceptable.
-        cmd_checker: AnyCmdChecker,
-        /// Oneshot channel to notify on completion.
-        done: ReactorResultChannel<()>,
-        /// The hop that is allowed to create streams.
-        hop_num: HopNum,
-        /// A filter used to check requests before passing them on.
-        #[educe(Debug(ignore))]
-        #[cfg(feature = "hs-service")]
-        filter: Box<dyn IncomingStreamRequestFilter>,
-    },
-    /// Send a given control message on this circuit.
-    #[cfg(feature = "send-control-msg")]
-    SendMsg {
-        /// The hop to receive this message.
-        hop_num: HopNum,
-        /// The message to send.
-        msg: AnyRelayMsg,
-        /// A sender that we use to tell the caller that the message was sent
-        /// and the handler installed.
-        sender: oneshot::Sender<Result<()>>,
-    },
-    /// Send a given control message on this circuit, and install a control-message handler to
-    /// receive responses.
-    #[cfg(feature = "send-control-msg")]
-    SendMsgAndInstallHandler {
-        /// The message to send, if any
-        msg: Option<AnyRelayMsgOuter>,
-        /// A message handler to install.
-        ///
-        /// If this is `None`, there must already be a message handler installed
-        #[educe(Debug(ignore))]
-        handler: Option<Box<dyn MetaCellHandler + Send + 'static>>,
-        /// A sender that we use to tell the caller that the message was sent
-        /// and the handler installed.
-        sender: oneshot::Sender<Result<()>>,
-    },
-    /// Send a SENDME cell (used to ask for more data to be sent) on the given stream.
-    SendSendme {
-        /// The stream ID to send a SENDME for.
-        stream_id: StreamId,
-        /// The hop number the stream is on.
-        hop_num: HopNum,
-    },
-    /// Shut down the reactor.
-    Shutdown,
-    /// (tests only) Add a hop to the list of hops on this circuit, with dummy cryptography.
-    #[cfg(test)]
-    AddFakeHop {
-        relay_cell_format: RelayCellFormat,
-        fwd_lasthop: bool,
-        rev_lasthop: bool,
-        params: CircParameters,
-        done: ReactorResultChannel<()>,
-    },
-    /// (tests only) Get the send window and expected tags for a given hop.
-    #[cfg(test)]
-    QuerySendWindow {
-        hop: HopNum,
-        done: ReactorResultChannel<(u32, Vec<CircTag>)>,
-    },
-    /// (tests only) Send a raw relay cell with send_relay_cell().
-    #[cfg(test)]
-    SendRelayCell {
-        hop: HopNum,
-        early: bool,
-        cell: AnyRelayMsgOuter,
-    },
-}
 /// Represents the reactor's view of a single hop.
 pub(super) struct CircHop {
     /// Map from stream IDs to streams.
