a8e968fb35cbf0dac719c662c0cb6e49d0d254f1
Gabriela Moldovan <gabi@torproject.org>
2025-03-13 15:30:47 +0000

    tor-proto: Add helper for checking if cmd counts towards conflux seqnos.
    This is a direct translation of C Tor's `conflux_should_multiplex()`.

--- crates/tor-proto/src/tunnel/reactor/conflux.rs
+++ crates/tor-proto/src/tunnel/reactor/conflux.rs
@@ -13,6 +13,7 @@ use slotmap_careful::SlotMap;
 
 use tor_async_utils::SinkPrepareExt as _;
 use tor_basic_utils::flatten;
+use tor_cell::relaycell::RelayCmd;
 use tor_error::{bad_api_usage, internal, into_bad_api_usage, Bug};
 
 use crate::circuit::path::HopDetail;
@@ -348,6 +349,56 @@ impl From<NotSingleError> for NotSingleLegError {
         match e {
             NotSingleError::None => Self::EmptyConfluxSet,
             NotSingleError::Multiple => Self::IsMultipath,
+        }
+    }
+}
+
+/// Whether the specified `cmd` counts towards the conflux sequence numbers.
+fn cmd_counts_towards_seqno(cmd: RelayCmd) -> bool {
+    // Note: copy-pasted from c-tor
+    match cmd {
+        // These are all fine to multiplex, and must be so that ordering is preserved
+        RelayCmd::BEGIN | RelayCmd::DATA | RelayCmd::END | RelayCmd::CONNECTED => true,
+
+        // We can't multiplex these because they are circuit-specific
+        RelayCmd::SENDME
+        | RelayCmd::EXTEND
+        | RelayCmd::EXTENDED
+        | RelayCmd::TRUNCATE
+        | RelayCmd::TRUNCATED
+        | RelayCmd::DROP => false,
+
+        //  We must multiplex RESOLVEs because their ordering impacts begin/end.
+        RelayCmd::RESOLVE | RelayCmd::RESOLVED => true,
+
+        // These are all circuit-specific
+        RelayCmd::BEGIN_DIR
+        | RelayCmd::EXTEND2
+        | RelayCmd::EXTENDED2
+        | RelayCmd::ESTABLISH_INTRO
+        | RelayCmd::ESTABLISH_RENDEZVOUS
+        | RelayCmd::INTRODUCE1
+        | RelayCmd::INTRODUCE2
+        | RelayCmd::RENDEZVOUS1
+        | RelayCmd::RENDEZVOUS2
+        | RelayCmd::INTRO_ESTABLISHED
+        | RelayCmd::RENDEZVOUS_ESTABLISHED
+        | RelayCmd::INTRODUCE_ACK
+        | RelayCmd::PADDING_NEGOTIATE
+        | RelayCmd::PADDING_NEGOTIATED => false,
+
+        // TODO(cc): XOFF/XON
+
+        // These two are not multiplexed, because they must be processed immediately
+        // to update sequence numbers before any other cells are processed on the circuit
+        RelayCmd::CONFLUX_SWITCH
+        | RelayCmd::CONFLUX_LINK
+        | RelayCmd::CONFLUX_LINKED
+        | RelayCmd::CONFLUX_LINKED_ACK => false,
+
+        _ => {
+            tracing::warn!("Conflux asked to multiplex unknown relay command {cmd}");
+            false
         }
     }
 }
