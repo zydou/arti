91d6b0ed2116e27a9764753a314911a9fc0ba814
nield <contact@ndvibes.com>
2025-04-09 10:59:19 +0000

    arti-ureq: New library which integrates with ureq
    Fixes #1519

---
+++ examples/ureq-examples/src/bin/arti-ureq-simple-get-request.rs
@@ -0,0 +1,28 @@
+// Make GET request over the Tor network using ureq.
+
+const TEST_URL: &str = "https://check.torproject.org/api/ip";
+
+fn main() {
+    // Get the ureq agent.
+    let ureq_agent = arti_ureq::default_agent().expect("Failed to create ureq agent.");
+
+    // Make request.
+    let request = ureq_agent.get(TEST_URL).call();
+
+    let mut request = match request {
+        Ok(request) => request,
+        Err(err) => {
+            eprintln!("Failed to make request: {}", err);
+            return;
+        }
+    };
+
+    // Get response body.
+    let response = request
+        .body_mut()
+        .read_to_string()
+        .expect("Failed to read body.");
+
+    // Will output if request was made using Tor.
+    println!("Response: {}", response);
+}
---
+++ examples/ureq-examples/src/bin/arti-ureq-tor-client.rs
@@ -0,0 +1,34 @@
+// Use your own instance of an `arti_client::TorClient` with arti-ureq to make a GET request.
+
+use anyhow::Context;
+
+const TEST_URL: &str = "https://check.torproject.org/api/ip";
+
+fn main() -> anyhow::Result<()> {
+    // Create your own instance of a Tor client.
+    let tor_client = arti_ureq::arti_client::TorClient::with_runtime(
+        arti_ureq::tor_rtcompat::PreferredRuntime::create().context("Failed to create runtime.")?,
+    )
+    .create_unbootstrapped()
+    .context("Error creating Tor Client.")?;
+
+    // Make a Connector and get ureq agent.
+    let ureq_agent = arti_ureq::Connector::with_tor_client(tor_client).agent();
+
+    // Make request.
+    let mut request = ureq_agent
+        .get(TEST_URL)
+        .call()
+        .context("Failed to make request.")?;
+
+    // Get response body.
+    let response = request
+        .body_mut()
+        .read_to_string()
+        .context("Failed to read body.")?;
+
+    // Will output if request was made using Tor.
+    println!("Response: {}", response);
+
+    Ok(())
+}
