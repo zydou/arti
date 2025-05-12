f91be5a9195253c7a22e07eff39f5ad8dfb92c64
hashcatHitman <3924-hashcatHitman@gitlab.torproject.org>
2025-05-12 15:08:40 -0400

    tor-circmgr: Modified MSRV TODOs for `once_cell` removal
    - Shortened the TODO added in e9ef7bea96c2860fa81d03e8d8da2605d0661425 in the
    style of [this maintainer request] for consistency.
    
    [this maintainer request]: https://gitlab.torproject.org/tpo/core/arti/-/merge_requests/2953#note_3197719
    
    Signed-off-by: hashcatHitman <3924-hashcatHitman@gitlab.torproject.org>

--- crates/tor-circmgr/src/hspool.rs
+++ crates/tor-circmgr/src/hspool.rs
@@ -374,13 +374,7 @@ pub(crate) struct HsCircPoolInner<B: AbstractTunnelBuilder<R> + 'static, R: Runt
     // eventually.  But for now, this is fine, since it's just an implementation
     // detail.
     //
-    // TODO MSRV TBD: If still relevant, see about replacing this usage of
-    // [`once_cell::sync::OnceCell`] with [`std::sync::OnceLock`]. Waiting on
-    // [`std::sync::OnceLock::get_or_try_init`] to stabilize and fall within our
-    // MSRV. See [1] and [2] for more information.
-    //
-    // [1]: https://github.com/rust-lang/rust/issues/109737
-    // [2]: https://doc.rust-lang.org/std/sync/struct.OnceLock.html#method.get_or_try_init
+    // TODO MSRV TBD: Replace with OnceLock (#1996)
     launcher_handle: OnceCell<TaskHandle>,
     /// The mutable state of this pool.
     inner: Mutex<Inner<B::Tunnel>>,
