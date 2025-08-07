9ee6d17f3245fd4e9b63c9cf6c11453e5ce04e91
Ian Jackson <ijackson@chiark.greenend.org.uk>
2025-08-07 17:05:29 +0100

    tor-netdoc: rs: Abolish implement_accessors macro (fmt)

--- crates/tor-netdoc/src/doc/netstatus/rs/per_species.rs
+++ crates/tor-netdoc/src/doc/netstatus/rs/per_species.rs
@@ -45,73 +45,73 @@ impl From<GenericRouterStatus<DocDigest>> for ConsensusRouterStatus {
 
 // TODO: These methods should probably become, in whole or in part,
 // methods on the RouterStatus trait.
-        impl ConsensusRouterStatus {
-            /// Return an iterator of ORPort addresses for this routerstatus
-            pub fn orport_addrs(&self) -> impl Iterator<Item = &net::SocketAddr> {
-                self.rs.addrs.iter()
-            }
-            /// Return the declared weight of this routerstatus in the directory.
-            pub fn weight(&self) -> &RelayWeight {
-                &self.rs.weight
-            }
-            /// Return the ORPort addresses of this routerstatus
-            pub fn addrs(&self) -> &[net::SocketAddr] {
-                &self.rs.addrs[..]
-            }
-            /// Return the protovers that this routerstatus says it implements.
-            pub fn protovers(&self) -> &Protocols {
-                &self.rs.protos
-            }
-            /// Return the nickname of this routerstatus.
-            pub fn nickname(&self) -> &str {
-                self.rs.nickname.as_str()
-            }
-            /// Return the relay flags of this routerstatus.
-            pub fn flags(&self) -> &RelayFlags {
-                &self.rs.flags
-            }
-            /// Return the version of this routerstatus.
-            pub fn version(&self) -> Option<&crate::doc::netstatus::rs::Version> {
-                self.rs.version.as_ref()
-            }
-            /// Return true if the ed25519 identity on this relay reflects a
-            /// true consensus among the authorities.
-            pub fn ed25519_id_is_usable(&self) -> bool {
-                !self.rs.flags.contains(RelayFlags::NO_ED_CONSENSUS)
-            }
-            /// Return true if this routerstatus is listed with the BadExit flag.
-            pub fn is_flagged_bad_exit(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::BAD_EXIT)
-            }
-            /// Return true if this routerstatus is listed with the v2dir flag.
-            pub fn is_flagged_v2dir(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::V2DIR)
-            }
-            /// Return true if this routerstatus is listed with the Exit flag.
-            pub fn is_flagged_exit(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::EXIT)
-            }
-            /// Return true if this routerstatus is listed with the Guard flag.
-            pub fn is_flagged_guard(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::GUARD)
-            }
-            /// Return true if this routerstatus is listed with the HSDir flag.
-            pub fn is_flagged_hsdir(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::HSDIR)
-            }
-            /// Return true if this routerstatus is listed with the Stable flag.
-            pub fn is_flagged_stable(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::STABLE)
-            }
-            /// Return true if this routerstatus is listed with the Fast flag.
-            pub fn is_flagged_fast(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::FAST)
-            }
-            /// Return true if this routerstatus is listed with the MiddleOnly flag.
-            pub fn is_flagged_middle_only(&self) -> bool {
-                self.rs.flags.contains(RelayFlags::MIDDLE_ONLY)
-            }
-        }
+impl ConsensusRouterStatus {
+    /// Return an iterator of ORPort addresses for this routerstatus
+    pub fn orport_addrs(&self) -> impl Iterator<Item = &net::SocketAddr> {
+        self.rs.addrs.iter()
+    }
+    /// Return the declared weight of this routerstatus in the directory.
+    pub fn weight(&self) -> &RelayWeight {
+        &self.rs.weight
+    }
+    /// Return the ORPort addresses of this routerstatus
+    pub fn addrs(&self) -> &[net::SocketAddr] {
+        &self.rs.addrs[..]
+    }
+    /// Return the protovers that this routerstatus says it implements.
+    pub fn protovers(&self) -> &Protocols {
+        &self.rs.protos
+    }
+    /// Return the nickname of this routerstatus.
+    pub fn nickname(&self) -> &str {
+        self.rs.nickname.as_str()
+    }
+    /// Return the relay flags of this routerstatus.
+    pub fn flags(&self) -> &RelayFlags {
+        &self.rs.flags
+    }
+    /// Return the version of this routerstatus.
+    pub fn version(&self) -> Option<&crate::doc::netstatus::rs::Version> {
+        self.rs.version.as_ref()
+    }
+    /// Return true if the ed25519 identity on this relay reflects a
+    /// true consensus among the authorities.
+    pub fn ed25519_id_is_usable(&self) -> bool {
+        !self.rs.flags.contains(RelayFlags::NO_ED_CONSENSUS)
+    }
+    /// Return true if this routerstatus is listed with the BadExit flag.
+    pub fn is_flagged_bad_exit(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::BAD_EXIT)
+    }
+    /// Return true if this routerstatus is listed with the v2dir flag.
+    pub fn is_flagged_v2dir(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::V2DIR)
+    }
+    /// Return true if this routerstatus is listed with the Exit flag.
+    pub fn is_flagged_exit(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::EXIT)
+    }
+    /// Return true if this routerstatus is listed with the Guard flag.
+    pub fn is_flagged_guard(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::GUARD)
+    }
+    /// Return true if this routerstatus is listed with the HSDir flag.
+    pub fn is_flagged_hsdir(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::HSDIR)
+    }
+    /// Return true if this routerstatus is listed with the Stable flag.
+    pub fn is_flagged_stable(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::STABLE)
+    }
+    /// Return true if this routerstatus is listed with the Fast flag.
+    pub fn is_flagged_fast(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::FAST)
+    }
+    /// Return true if this routerstatus is listed with the MiddleOnly flag.
+    pub fn is_flagged_middle_only(&self) -> bool {
+        self.rs.flags.contains(RelayFlags::MIDDLE_ONLY)
+    }
+}
 
 impl Sealed for ConsensusRouterStatus {}
 
