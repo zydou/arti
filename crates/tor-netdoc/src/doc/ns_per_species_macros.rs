7ea1bd512b81a861825de81cb542c6547e5847e1
Ian Jackson <ijackson@chiark.greenend.org.uk>
2025-08-14 16:33:18 +0100

    tor-netdoc: ns_per_species_macros: Swap order, make vote optional

--- crates/tor-netdoc/src/doc/ns_per_species_macros.rs
+++ crates/tor-netdoc/src/doc/ns_per_species_macros.rs
@@ -42,7 +42,7 @@
 //!
 //! * **[`ns_ty_name!`](crate::doc::netstatus::rs::md::ns_ty_name)`( BaseTtypeName )`**:
 //!
-//!   Expands to `VoteBaseTypeName`, `NsBaseTypeName`, or `MdBaseTypeName`.
+//!   Expands to `NsBaseTypeName`, `MdBaseTypeName`, or `VoteBaseTypeName`.
 // XXXX should be PlainBaseTypeName not Ns_
 //!
 //!   Cannot be used to *define* a type.
@@ -53,23 +53,28 @@
 //!
 //! * **`ns_const_name!( BASE_CONST_NAME )`**:
 //!
-//!   Expands to `VOTE_BASE_CONST_NAME`, `NS_BASE_CONST_NAME`, or `MD_BASE_CONST_NAME`.
+//!   Expands to `NS_BASE_CONST_NAME`, `VOTE_BASE_CONST_NAME`, or `MD_BASE_CONST_NAME`.
 // XXXX should be PLAIN_BASE_CONST_NAME not NS_
 //!
-//! * **`ns_type!( TypeForVote, TypeForPlainConsensus, [TypeForMdConsensus] )`**:
+//! * **`ns_type!( TypeForPlainConsensus, TypeForMdConsensus, [TypeForVote] )`**:
 //!
 //!   Expands to the appropriate one of the two or three specified types.
-//!   If `TypeForMdConsensus` is not specified, `TypeForNsConsensus` is used.
+//!   If `TypeForVote` is not specified, it is a compile error
+//!   for this `per_species` file to be used for votes.
 //!
-//! * **`ns_expr!( value_for_vote, value_for_plain_consensus, [value_for_md_consensus] )`**:
+//! * **`ns_expr!( value_for_plain_consensus, value_for_md_consensus, [value_for_vote] )`**:
 //!
 //!   Expands to the appropriate one of the two or three specified expressions.
-//!   If `value_for_consensus_md` is not specified, `TypeForConsensus` is used.
+//!   If `value_for_vote` is not specified, it is a compile error
+//!   for this `per_species` file to be used for votes.
 //!
-//! * **`ns_choose!( ( FOR VOTE.. )( FOR PLAIN CONSENSUS.. )( FOR MD CONSENSUS.. ) )`**:
+//! * **`ns_choose!( ( FOR PLAIN CONSENSUS.. )( FOR MD CONSENSUS.. )[( FOR VOTE.. )] )`**:
 //!
 //!   Expands to the appropriate one of the two or three specified token streams.
 //!   (The `( )` surrounding each argument are discarded.)
+//!
+//!   If `FOR VOTE` is not specified, it is a compile error
+//!   for this `per_species` file to be used for votes.
 //!
 //!   When defining whole items, prefer to put the species-specific items directly
 //!   in each of the species-specific modules.
@@ -101,32 +106,32 @@
 ///
 ///  * `$abbrev` is one of `vote`, `plain`, or `md` as applicable.
 ///
-///  * `: $vote $plain $md $d` is always `: vote plain md $`.
+///  * `: $plain $md $vote $d` is always `: plain md vote $`.
 ///    `$d` is needed because it's not possible to write a literal `$`
 ///    in the expansion part of a proc macro, except at the end of a group (!)
 ///    (`$$` can do that but is is not stable.)
 ///    `$vote` etc. are needed to match the identifier hygiene of `$abbrev`.
 macro_rules! ns_do_one_species { {
-    $abbrev:ident : $vote:ident $plain:ident $md:ident $d:tt
+    $abbrev:ident : $plain:ident $md:ident $vote:ident $d:tt
 } => {
     // ----- Define the selector macros (see the module top-level comment -----
 
     #[allow(unused)]
     macro_rules! ns_choose {
         {
-            ( $d( $d $vote:tt )* )
             ( $d( $d $plain:tt )* )
             ( $d( $d $md  :tt )* )
+            ( $d( $d $vote:tt )* )
         } => {
             $d( $d $abbrev )*
         };
         {
-            ( $d( $d vote:tt )* )
             ( $d( $d plain:tt )* )
+            ( $d( $d $md  :tt )* )
         } => { ns_choose! {
             ( $d( $d vote    )* )
             ( $d( $d plain   )* )
-            ( $d( $d plain   )* )
+            ( compile_error!("missing definition of what to do for the vote variety") )
         } }
     }
 
@@ -168,24 +173,24 @@ macro_rules! ns_do_one_species { {
     pub use per_species::*;
 } }
 
-/// Include species-agnostic items, for a vote, from `per_species.rs`.
-///
-/// Use within `vote.rs`.
-#[allow(unused)] // TODO feature = "ns-vote"
-macro_rules! ns_do_species_vote { {} => { ns_do_one_species! { vote : vote plain md $ } } }
-use ns_do_species_vote;
-
 /// Include species-agnostic items, for a full consensus, from `per_species.rs`.
 ///
 /// Use within `plain.rs`.
-macro_rules! ns_do_species_plain { {} => { ns_do_one_species! { plain : vote plain md $ } } }
+macro_rules! ns_do_species_plain { {} => { ns_do_one_species! { plain : plain md vote $ } } }
 use ns_do_species_plain;
 
 /// Include species-agnostic items, for an md consensus, from `per_species.rs`.
 ///
 /// Use within `md.rs`.
-macro_rules! ns_do_species_md   { {} => { ns_do_one_species! { md   : vote plain md $ } } }
+macro_rules! ns_do_species_md   { {} => { ns_do_one_species! { md   : plain md vote $ } } }
 use ns_do_species_md;
+
+/// Include species-agnostic items, for a vote, from `per_species.rs`.
+///
+/// Use within `vote.rs`.
+#[allow(unused)] // TODO feature = "ns-vote"
+macro_rules! ns_do_species_vote { {} => { ns_do_one_species! { vote : plain md vote $ } } }
+use ns_do_species_vote;
 
 /// Export species-specific names from each module.
 ///
@@ -219,13 +224,13 @@ macro_rules! ns_export_per_species {
     {
         @ [ $($case:tt)* ] [$($infix:tt)*] $id:ident
     } => { paste::paste!{
-        #[cfg(feature = "ns-vote")] // TODO ns-vote this feature doesn't exist yet
-        #[cfg_attr(docsrs, doc(cfg(feature = "ns-vote")))]
-        pub use { vote::$id as [<vote $($case)* $($infix)* $id>] };
         #[cfg(feature = "ns-consensus")]
         #[cfg_attr(docsrs, doc(cfg(feature = "ns-consensus")))]
         pub use { plain ::$id as [<ns   $($case)* $($infix)* $id>] };
         // unconditional
         pub use { md  ::$id as [<md   $($case)* $($infix)* $id>] };
+        #[cfg(feature = "ns-vote")] // TODO ns-vote this feature doesn't exist yet
+        #[cfg_attr(docsrs, doc(cfg(feature = "ns-vote")))]
+        pub use { vote::$id as [<vote $($case)* $($infix)* $id>] };
     } };
 }
