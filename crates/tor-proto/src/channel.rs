LeonMskRu/arti_1-4-4_20250614

proto: Make the OutboundClientHandshake use new cell handler
Use the ChannelFrame<> for the entirety of the outbound client handshake
that is the ClientInitiator channel type.

With this change, the codec.rs code is not needed anymore along its
CodecError as well which has been normalized onto the crate::Error
instead in order to simplify error handling and avoid duplication of
error types.

Unit tests have been modified to reflect this change of what can be done
with a channel frame. Also renamed to focus on client behavior.

Part of #1597

Signed-off-by: David Goulet <dgoulet@torproject.org>

crates/tor-proto/src/channel.rs
@@ -56,7 +56,6 @@
 pub const CHANNEL_BUFFER_SIZE: usize = 128;
 
 mod circmap;
-mod codec;
 mod handler;
 mod handshake;
 mod msg;
@@ -78,8 +77,8 @@ use std::pin::Pin;
 use std::sync::{Mutex, MutexGuard};
 use std::time::Duration;
 use tor_cell::chancell::msg::AnyChanMsg;
+use tor_cell::chancell::ChanMsg;
 use tor_cell::chancell::{msg::PaddingNegotiate, AnyChanCell, CircId};
-use tor_cell::chancell::{ChanCell, ChanMsg};
 use tor_cell::restricted_msg;
 use tor_error::internal;
 use tor_linkspec::{HasRelayIds, OwnedChanTarget};
@@ -116,8 +115,6 @@ use tracing::trace;
 
 // reexport
 use crate::channel::unique_id::CircUniqIdContext;
-#[cfg(test)]
-pub(crate) use codec::CodecError;
 pub use handshake::{OutboundClientHandshake, UnverifiedChannel, VerifiedChannel};
 
 restricted_msg! {
@@ -209,15 +206,6 @@ where
 {
     futures_codec::Framed::new(tls, ty.into())
 }
-
-/// A channel cell that we allot to be sent on an open channel from
-/// a server to a client.
-pub(crate) type OpenChanCellS2C = ChanCell<OpenChanMsgS2C>;
-
-/// Type alias: A Sink and Stream that transforms a TLS connection into
-/// a cell-based communication mechanism.
-type CellFrame<T> =
-    futures_codec::Framed<T, crate::channel::codec::ChannelCodec<OpenChanMsgS2C, AnyChanMsg>>;
 
 /// An open client channel, ready to send and receive Tor cells.
 ///
@@ -907,7 +895,7 @@ pub(crate) mod test {
     #![allow(clippy::unwrap_used)]
     use super::*;
     use crate::channel::codec::test::MsgBuf;
-    pub(crate) use crate::channel::reactor::test::new_reactor;
+    pub(crate) use crate::channel::reactor::test::{new_reactor, CodecResult};
     use crate::util::fake_mq;
     use tor_cell::chancell::msg::HandshakeType;
     use tor_cell::chancell::{msg, AnyChanCell};

crates/tor-proto/src/circuit.rs
@@ -1361,8 +1361,7 @@ mod test {
     //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
 
     use super::*;
-    use crate::channel::OpenChanCellS2C;
-    use crate::channel::{test::new_reactor, CodecError};
+    use crate::channel::test::{new_reactor, CodecResult};
     use crate::crypto::cell::RelayCellBody;
     #[cfg(feature = "ntor_v3")]
     use crate::crypto::handshake::ntor_v3::NtorV3Server;
@@ -1458,11 +1457,7 @@ mod test {
 
     fn working_fake_channel<R: Runtime>(
         rt: &R,
-    ) -> (
-        Arc<Channel>,
-        Receiver<AnyChanCell>,
-        Sender<std::result::Result<OpenChanCellS2C, CodecError>>,
-    ) {
+    ) -> (Arc<Channel>, Receiver<AnyChanCell>, Sender<CodecResult>) {
         let (channel, chan_reactor, rx, tx) = new_reactor(rt.clone());
         rt.spawn(async {
             let _ignore = chan_reactor.run().await;
@@ -2083,7 +2078,7 @@ mod test {
         Option<StreamId>,
         usize,
         Receiver<AnyChanCell>,
-        Sender<std::result::Result<OpenChanCellS2C, CodecError>>,
+        Sender<CodecResult>,
     ) {
         let (chan, mut rx, sink2) = working_fake_channel(rt);
         let (circ, mut sink) = newcirc(rt, chan).await;

zydou/arti

crates/tor-proto/src/channel.rs
@@ -56,7 +56,6 @@
 pub const CHANNEL_BUFFER_SIZE: usize = 128;
 
 mod circmap;
-mod codec;
 mod handler;
 mod handshake;
 pub mod kist;
@@ -82,9 +81,9 @@ use std::future::{Future, IntoFuture};
 use std::pin::Pin;
 use std::sync::{Mutex, MutexGuard};
 use std::time::Duration;
+use tor_cell::chancell::ChanMsg;
 use tor_cell::chancell::msg::AnyChanMsg;
 use tor_cell::chancell::{AnyChanCell, CircId, msg::PaddingNegotiate};
-use tor_cell::chancell::{ChanCell, ChanMsg};
 use tor_cell::restricted_msg;
 use tor_error::internal;
 use tor_linkspec::{HasRelayIds, OwnedChanTarget};
@@ -120,8 +119,6 @@ use tracing::trace;
 
 // reexport
 use crate::channel::unique_id::CircUniqIdContext;
-#[cfg(test)]
-pub(crate) use codec::CodecError;
 pub use handshake::{OutboundClientHandshake, UnverifiedChannel, VerifiedChannel};
 
 use kist::KistParams;
@@ -204,15 +201,6 @@ where
 {
     asynchronous_codec::Framed::new(tls, ty.into())
 }
-
-/// A channel cell that we allot to be sent on an open channel from
-/// a server to a client.
-pub(crate) type OpenChanCellS2C = ChanCell<OpenChanMsgS2C>;
-
-/// Type alias: A Sink and Stream that transforms a TLS connection into
-/// a cell-based communication mechanism.
-type CellFrame<T> =
-    asynchronous_codec::Framed<T, crate::channel::codec::ChannelCodec<OpenChanMsgS2C, AnyChanMsg>>;
 
 /// An open client channel, ready to send and receive Tor cells.
 ///
@@ -941,8 +929,8 @@ pub(crate) mod test {
     // reactor code; there are just a few more cases to examine here.
     #![allow(clippy::unwrap_used)]
     use super::*;
-    use crate::channel::codec::test::MsgBuf;
-    pub(crate) use crate::channel::reactor::test::new_reactor;
+    use crate::channel::handler::test::MsgBuf;
+    pub(crate) use crate::channel::reactor::test::{CodecResult, new_reactor};
     use crate::util::fake_mq;
     use tor_cell::chancell::msg::HandshakeType;
     use tor_cell::chancell::{AnyChanCell, msg};

crates/tor-proto/src/client/circuit.rs
@@ -1110,8 +1110,7 @@ pub(crate) mod test {
     //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
 
     use super::*;
-    use crate::channel::OpenChanCellS2C;
-    use crate::channel::{CodecError, test::new_reactor};
+    use crate::channel::test::{CodecResult, new_reactor};
     use crate::client::stream::DataStream;
     #[cfg(feature = "hs-service")]
     use crate::client::stream::IncomingStreamRequestFilter;
@@ -1241,11 +1240,7 @@ pub(crate) mod test {
 
     fn working_fake_channel<R: Runtime>(
         rt: &R,
-    ) -> (
-        Arc<Channel>,
-        Receiver<AnyChanCell>,
-        Sender<std::result::Result<OpenChanCellS2C, CodecError>>,
-    ) {
+    ) -> (Arc<Channel>, Receiver<AnyChanCell>, Sender<CodecResult>) {
         let (channel, chan_reactor, rx, tx) = new_reactor(rt.clone());
         rt.spawn(async {
             let _ignore = chan_reactor.run().await;
@@ -1963,7 +1958,7 @@ pub(crate) mod test {
         Option<StreamId>,
         usize,
         Receiver<AnyChanCell>,
-        Sender<std::result::Result<OpenChanCellS2C, CodecError>>,
+        Sender<CodecResult>,
     ) {
         let (chan, mut rx, sink2) = working_fake_channel(rt);
         let (tunnel, mut sink) = newtunnel(rt, chan).await;
@@ -2592,7 +2587,7 @@ pub(crate) mod test {
     #[cfg(feature = "conflux")]
     struct TestCircuitCtx {
         chan_rx: Receiver<AnyChanCell>,
-        chan_tx: Sender<std::result::Result<OpenChanCellS2C, CodecError>>,
+        chan_tx: Sender<std::result::Result<AnyChanCell, Error>>,
         circ_tx: CircuitRxSender,
         unique_id: UniqId,
     }