fe490a80a2bcaab76aa7d5b9537f1153d5f37b86
Ian Jackson <ijackson@chiark.greenend.org.uk>
2025-08-20 18:04:32 +0100

    tor-netdoc: Fix some out-of-date comments
    We rename the files from per_ to each_.

--- crates/tor-netdoc/src/doc/netstatus/rs/each_variety.rs
+++ crates/tor-netdoc/src/doc/netstatus/rs/each_variety.rs
@@ -3,7 +3,7 @@
 //! **This file is reincluded multiple times**,
 //! by the macros in [`crate::doc::ns_variety_definition_macros`],
 //! once for votes, and once for each consensus flavour.
-//! It is *not* a module `crate::doc::netstatus::rs::per_variety`.
+//! It is *not* a module `crate::doc::netstatus::rs::each_variety`.
 //!
 //! Each time this file is included by one of the macros mentioned above,
 //! the `ns_***` macros (such as `ns_const_name!`) may expand to different values.
