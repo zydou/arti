5e07b005f1e007424125a03aa55121dcca7ad88e
Clara Engler <cve@cve.cx>
2025-08-28 17:28:08 +0200

    tor-dircommon: Add dirports field to authority

--- crates/tor-dircommon/src/authority.rs
+++ crates/tor-dircommon/src/authority.rs
@@ -3,9 +3,14 @@
 //! From a client's point of view, an authority's role is to sign the
 //! consensus directory.
 
+use std::net::SocketAddr;
+
 use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
-use tor_config::{ConfigBuildError, define_list_builder_helper, impl_standard_builder};
+use tor_config::{
+    ConfigBuildError, define_list_builder_accessors, define_list_builder_helper,
+    impl_standard_builder, list_builder::VecBuilder,
+};
 use tor_llcrypto::pk::rsa::RsaIdentity;
 
 /// A single authority that signs a consensus directory.
@@ -24,9 +29,18 @@ pub struct Authority {
     /// this authority.
     // TODO: It would be lovely to use a better hash for these identities.
     v3ident: RsaIdentity,
+    /// List of [`SocketAddr`] on where the directory service is publicly reachable.
+    #[builder(sub_builder(fn_name = "build"), setter(custom))]
+    dirports: Vec<SocketAddr>,
 }
 
 impl_standard_builder! { Authority: !Default }
+
+define_list_builder_accessors! {
+    struct AuthorityBuilder {
+        pub dirports: [SocketAddr],
+    }
+}
 
 /// Authority list, built
 pub type AuthorityList = Vec<Authority>;
@@ -42,25 +56,71 @@ define_list_builder_helper! {
 /// Return a vector of the default directory authorities.
 pub fn default_authorities() -> Vec<AuthorityBuilder> {
     /// Build an authority; panic if input is bad.
-    fn auth(name: &str, key: &str) -> AuthorityBuilder {
+    fn auth(name: &str, key: &str, dirports: &[&str]) -> AuthorityBuilder {
         let v3ident =
             RsaIdentity::from_hex(key).expect("Built-in authority identity had bad hex!?");
         let mut auth = AuthorityBuilder::new();
         auth.name(name).v3ident(v3ident);
+        auth.dirports().append(
+            &mut dirports
+                .iter()
+                .map(|dirport| {
+                    dirport
+                        .parse()
+                        .expect("Built-in authority has invalid dirport?")
+                })
+                .collect(),
+        );
         auth
     }
 
     // (Last updated on May 30th, 2024)
     vec![
-        auth("bastet", "27102BC123E7AF1D4741AE047E160C91ADC76B21"),
-        auth("dannenberg", "0232AF901C31A04EE9848595AF9BB7620D4C5B2E"),
-        auth("dizum", "E8A9C45EDE6D711294FADF8E7951F4DE6CA56B58"),
-        auth("faravahar", "70849B868D606BAECFB6128C5E3D782029AA394F"),
-        auth("gabelmoo", "ED03BB616EB2F60BEC80151114BB25CEF515B226"),
-        auth("longclaw", "23D15D965BC35114467363C165C4F724B64B4F66"),
-        auth("maatuska", "49015F787433103580E3B66A1707A00E60F2D15B"),
-        auth("moria1", "F533C81CEF0BC0267857C99B2F471ADF249FA232"),
-        auth("tor26", "2F3DF9CA0E5D36F2685A2DA67184EB8DCB8CBA8C"),
+        auth(
+            "bastet",
+            "27102BC123E7AF1D4741AE047E160C91ADC76B21",
+            &["204.13.164.118:80", "[2620:13:4000:6000::1000:118]:80"],
+        ),
+        auth(
+            "dannenberg",
+            "0232AF901C31A04EE9848595AF9BB7620D4C5B2E",
+            &["193.23.244.244:80", "[2001:678:558:1000::244]:80"],
+        ),
+        auth(
+            "dizum",
+            "E8A9C45EDE6D711294FADF8E7951F4DE6CA56B58",
+            &["45.66.35.11:80", "[2a09:61c0::1337]:80"],
+        ),
+        auth(
+            "faravahar",
+            "70849B868D606BAECFB6128C5E3D782029AA394F",
+            &["216.218.219.41:80", "[2001:470:164:2::2]:80"],
+        ),
+        auth(
+            "gabelmoo",
+            "ED03BB616EB2F60BEC80151114BB25CEF515B226",
+            &["131.188.40.189:80", "[2001:638:a000:4140::ffff:189]:80"],
+        ),
+        auth(
+            "longclaw",
+            "23D15D965BC35114467363C165C4F724B64B4F66",
+            &["199.58.81.140:80"],
+        ),
+        auth(
+            "maatuska",
+            "49015F787433103580E3B66A1707A00E60F2D15B",
+            &["171.25.193.9:443", "[2001:67c:289c::9]:443"],
+        ),
+        auth(
+            "moria1",
+            "F533C81CEF0BC0267857C99B2F471ADF249FA232",
+            &["128.31.0.39:9231"],
+        ),
+        auth(
+            "tor26",
+            "2F3DF9CA0E5D36F2685A2DA67184EB8DCB8CBA8C",
+            &["217.196.147.77:80", "[2a02:16a8:662:2203::1]:80"],
+        ),
     ]
 }
 
@@ -73,6 +133,12 @@ impl Authority {
     /// Returns the directory specific [`RsaIdentity`] of the authority.
     pub fn v3ident(&self) -> RsaIdentity {
         self.v3ident
+    }
+
+    /// Returns a list of [`SocketAddr`] referencing to the IP and port where
+    /// the authorities' directory service is listening upon.
+    pub fn dirports(&self) -> &[SocketAddr] {
+        &self.dirports
     }
 }
 
@@ -98,6 +164,8 @@ mod test {
     #![allow(clippy::useless_vec)]
     #![allow(clippy::needless_pass_by_value)]
     //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
+    use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
+
     use super::*;
     use tor_netdoc::doc::authcert::AuthCertKeyIds;
 
@@ -112,11 +180,18 @@ mod test {
     fn authority() {
         let key1: RsaIdentity = [9_u8; 20].into();
         let key2: RsaIdentity = [10_u8; 20].into();
-        let auth = Authority::builder()
-            .name("example")
-            .v3ident(key1)
-            .build()
-            .unwrap();
+        let mut auth = Authority::builder();
+        auth.name("example");
+        auth.v3ident(key1);
+        auth.dirports()
+            .push(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 80)));
+        auth.dirports().push(SocketAddr::V6(SocketAddrV6::new(
+            Ipv6Addr::LOCALHOST,
+            80,
+            0,
+            0,
+        )));
+        let auth = auth.build().unwrap();
 
         assert_eq!(&auth.v3ident, &key1);
 
