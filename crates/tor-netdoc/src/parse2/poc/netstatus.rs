12b8df35b3ff39cf8524a0ace668827646d860a3
Ian Jackson <ijackson@chiark.greenend.org.uk>
2025-10-07 12:57:26 +0100

    tor-netdoc: parse2: Replace EP with AE in NdiDirectorySignature

--- crates/tor-netdoc/src/parse2/poc/netstatus.rs
+++ crates/tor-netdoc/src/parse2/poc/netstatus.rs
@@ -141,13 +141,13 @@ impl SignatureItemParseable for NdiDirectorySignature {
 
         let rsa_signature = object.ok_or(EP::MissingObject)?.decode_data()?;
 
-        let mut fingerprint_arg = |field: &'static str| {
+        let mut fingerprint_arg = |field: &'static str| (|| {
             args.next()
-                .ok_or(EP::MissingArgument { field })?
+                .ok_or(AE::Missing)?
                 .parse::<types::Fingerprint>()
-                .map_err(|_e| EP::InvalidArgument { field })
+                .map_err(|_e| AE::Invalid)
                 .map(pk::rsa::RsaIdentity::from)
-        };
+        })().map_err(args.error_handler(field));
 
         Ok(NdiDirectorySignature::Known {
             rsa_signature,
