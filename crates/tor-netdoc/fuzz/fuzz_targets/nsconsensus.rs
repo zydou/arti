34df43cc8d10b3f320a4ce5d192cff1232770d3b
5225225 <5225225@mailbox.org>
2025-10-07 10:48:21 +0100

    Undo change to use ns_consensus feature, fix target to not need it

--- crates/tor-netdoc/fuzz/fuzz_targets/nsconsensus.rs
+++ crates/tor-netdoc/fuzz/fuzz_targets/nsconsensus.rs
@@ -1,7 +1,7 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use tor_netdoc::doc::netstatus::NsConsensus;
+use tor_netdoc::doc::netstatus::PlainConsensus;
 
 fuzz_target!(|data: &str| {
-    let _ = NsConsensus::parse(data);
+    let _ = PlainConsensus::parse(data);
 });
