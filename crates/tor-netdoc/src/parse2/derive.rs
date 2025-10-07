9f1d2bcac3007a290da16d24540d6e02c837f3f8
Ian Jackson <ijackson@chiark.greenend.org.uk>
2025-10-07 12:59:54 +0100

    tor-netdoc: parse2: Replace EP with AE in derive fields rest path

--- crates/tor-netdoc/src/parse2/derive.rs
+++ crates/tor-netdoc/src/parse2/derive.rs
@@ -944,7 +944,8 @@ define_derive_deftly! {
                   ${fmeta(netdoc(with))
                     as path,
                     default { <$ftype as FromStr>::from_str }}(args_consume.into_remaining())
-                      .map_err(|_| EP::InvalidArgument { field: stringify!($fname) })?
+                      .map_err(|_| AE::Invalid)
+                      .map_err(args_consume.error_handler(stringify!($fname)))?
               } }
               F_SIG_HASH { {
                   #[allow(unused_imports)]
