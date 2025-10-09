624f0c17eaae16ecfb8dd4c957a49a9bff627a14
Nick Mathewson <nickm@torproject.org>
2025-10-09 10:41:43 -0400

    x509: Support generating the keys and certs that relays need.
    Although we never need to actually check the signature on the other
    party's x509 certificate, A relay does need a certificate and a
    private key in order to be a proper TLS server.
    
    In this function, I've added support for making an ersatz P-256
    certificate certifying a P-256 key.  See the code for info about the
    rationale here.  (Tor supports this, since it doesn't look at the
    key at all: only the TLS layer cares about that.)
    
    native_tls and rustls expect to get their keys and certs in
    different forms, so this code provides them.
    
    (Note that we don't expect to use native_tls with relays in the
    first place, but it might be useful for lower-level interop
    testing.)
    
    Closes #2205.

--- crates/tor-cert/src/x509.rs
+++ crates/tor-cert/src/x509.rs
@@ -16,13 +16,14 @@ use std::{
     time::{Duration, SystemTime},
 };
 
+use digest::Digest;
 use rand::CryptoRng;
-use rsa::pkcs8::SubjectPublicKeyInfo;
+use rsa::pkcs8::{EncodePrivateKey as _, SubjectPublicKeyInfo};
 use tor_error::into_internal;
-use tor_llcrypto::pk::rsa::KeyPair as RsaKeypair;
+use tor_llcrypto::{pk::rsa::KeyPair as RsaKeypair, util::rng::RngCompat};
 use x509_cert::{
     builder::{Builder, CertificateBuilder, Profile},
-    der::{DateTime, Encode, asn1::GeneralizedTime},
+    der::{DateTime, Encode, asn1::GeneralizedTime, zeroize::Zeroizing},
     ext::pkix::{KeyUsage, KeyUsages},
     serial_number::SerialNumber,
     time::Validity,
@@ -65,7 +66,7 @@ pub fn create_legacy_rsa_id_cert<Rng: CryptoRng>(
 
     let self_signed_profile = Profile::Manual { issuer: None };
     let serial_number = random_serial_number(rng)?;
-    let validity = cert_validity(now, ID_CERT_LIFETIME_DAYS)?;
+    let (validity, _) = cert_validity(now, ID_CERT_LIFETIME_DAYS)?;
     // NOTE: This is how C Tor builds its DNs, but that doesn't mean it's a good idea.
     let subject: x509_cert::name::Name = format!("CN={hostname}")
         .parse()
@@ -99,14 +100,173 @@ pub fn create_legacy_rsa_id_cert<Rng: CryptoRng>(
     Ok(output)
 }
 
-// TODO: We'll need a method to generate a certificate or two for use with TLS.
+/// A set of x.509 certificate information and keys for use with a TLS library.
+///
+/// Only relays need this: They should set these as the certificate(s) to be used
+/// for incoming TLS connections.
+///
+/// This is not necessarily the most convenient form to manipulate certificates in:
+/// rather, it is intended to provide the formats that TLS libraries generally
+/// expect to get.
+#[derive(Clone, Debug)]
+#[non_exhaustive]
+pub struct TlsCertifiedKey {
+    /// A list of certificates in DER form.
+    ///
+    /// (This may contain more than one certificate, but for now only one certificate is used.)
+    certificates: Vec<Vec<u8>>,
+
+    /// A private key for use in the TLS handshake.
+    private_key: ecdsa::SigningKey<p256::NistP256>,
+
+    /// A SHA256 digest of the link certificate
+    /// (the one certifying the private key's public component).
+    ///
+    /// This digest is the one what will be certified by the relay's
+    /// [`SIGNING_V_TLS_CERT`](crate::CertType::SIGNING_V_TLS_CERT)
+    /// certificate.
+    sha256_digest: [u8; 32],
+
+    /// A time after which this set of link information won't be valid,
+    /// and another should be generated.
+    expiration: SystemTime,
+}
+
+/// What lifetime do we pick for a TLS certificate, in days?
+const TLS_CERT_LIFETIME_DAYS: u32 = 30;
+
+impl TlsCertifiedKey {
+    /// Return the certificates as a list of DER-encoded values.
+    pub fn certificates_der(&self) -> Vec<&[u8]> {
+        self.certificates.iter().map(|der| der.as_ref()).collect()
+    }
+    /// Return the certificates as a concatenated list in PEM ("BEGIN CERTIFICATE") format.
+    pub fn certificate_pem(&self) -> String {
+        let config = pem::EncodeConfig::new().set_line_ending(pem::LineEnding::LF);
+        self.certificates
+            .iter()
+            .map(|der| pem::encode_config(&pem::Pem::new("CERTIFICATE", &der[..]), config))
+            .collect()
+    }
+    /// Return the private key in (unencrypted) PKCS8 DER format.
+    pub fn private_key_pkcs8_der(&self) -> Result<Zeroizing<Vec<u8>>, X509CertError> {
+        Ok(self
+            .private_key
+            .to_pkcs8_der()
+            .map_err(X509CertError::CouldNotFormatPkcs8)?
+            .to_bytes())
+    }
+    /// Return the private key in (unencrypted) PKCS8 PEM ("BEGIN PRIVATE KEY") format.
+    pub fn private_key_pkcs8_pem(&self) -> Result<Zeroizing<String>, X509CertError> {
+        self.private_key
+            .to_pkcs8_pem(p256::pkcs8::LineEnding::LF)
+            .map_err(X509CertError::CouldNotFormatPkcs8)
+    }
+    /// Return the earliest time at which any of these certificates will expire.
+    pub fn expiration(&self) -> SystemTime {
+        self.expiration
+    }
+
+    /// Return the SHA256 digest of the link certificate
+    ///
+    /// This digest is the one certified with the relay's
+    /// [`SIGNING_V_TLS_CERT`](crate::CertType::SIGNING_V_TLS_CERT)
+    /// certificate.
+    pub fn link_cert_sha256(&self) -> &[u8; 32] {
+        &self.sha256_digest
+    }
+
+    /// Create a new TLS link key and associated certificate(s).
+    ///
+    /// The certificate will be valid at `now`, and for a while after.
+    ///
+    /// The certificate parameters and keys are chosen for reasonable security,
+    /// approximate conformance to RFC5280, and limited fingerprinting resistance.
+    ///
+    /// Note: The fingerprinting resistance is quite limited.
+    /// We will likely want to pursue these avenues for better fingerprinting resistance:
+    ///
+    /// - Encourage more use of TLS 1.3, where server certificates are encrypted.
+    ///   (This prevents passive fingerprinting only.)
+    /// - Adjust this function to make certificates look even more normal
+    /// - Integrate with ACME-supporting certificate issuers (Letsencrypt, etc)
+    ///   to get real certificates for Tor relays.
+    pub fn create<Rng: CryptoRng>(
+        rng: &mut Rng,
+        now: SystemTime,
+        issuer_hostname: &str,
+        subject_hostname: &str,
+    ) -> Result<Self, X509CertError> {
+        // We choose to use p256 here as the most commonly used elliptic curve
+        // group for X.509 web certificate signing, as of this writing.
+        //
+        // We want to use an elliptic curve here for its higher security/performance ratio than RSA,
+        // and for its _much_ faster key generation time.
+        let private_key = p256::ecdsa::SigningKey::random(&mut RngCompat::new(&mut *rng));
+        let public_key = p256::ecdsa::VerifyingKey::from(&private_key);
+
+        // Note that we'll discard this key after signing the certificate with it:
+        // The real certification for private_key is done in the SIGNING_V_TLS_LINK
+        // certificate.
+        let issuer_private_key = p256::ecdsa::SigningKey::random(&mut RngCompat::new(&mut *rng));
+
+        // NOTE: This is how C Tor builds its DNs, but that doesn't mean it's a good idea.
+        let issuer = format!("CN={issuer_hostname}")
+            .parse()
+            .map_err(X509CertError::InvalidHostname)?;
+        let subject: x509_cert::name::Name = format!("CN={subject_hostname}")
+            .parse()
+            .map_err(X509CertError::InvalidHostname)?;
+
+        let self_signed_profile = Profile::Leaf {
+            issuer,
+            enable_key_agreement: true,
+            enable_key_encipherment: true,
+            include_subject_key_identifier: true,
+        };
+        let serial_number = random_serial_number(rng)?;
+        let (validity, expiration) = cert_validity(now, TLS_CERT_LIFETIME_DAYS)?;
+        let spki = SubjectPublicKeyInfo::from_key(public_key)?;
+
+        let builder = CertificateBuilder::new(
+            self_signed_profile,
+            serial_number,
+            validity,
+            subject,
+            spki,
+            &issuer_private_key,
+        )?;
+
+        let cert = builder.build::<ecdsa::der::Signature<_>>()?;
+
+        let mut certificate_der = Vec::new();
+        let _ignore_length: x509_cert::der::Length = cert
+            .encode_to_vec(&mut certificate_der)
+            .map_err(X509CertError::CouldNotEncode)?;
+
+        let sha256_digest = tor_llcrypto::d::Sha256::digest(&certificate_der).into();
+        let certificates = vec![certificate_der];
+
+        Ok(TlsCertifiedKey {
+            certificates,
+            private_key,
+            sha256_digest,
+            expiration,
+        })
+    }
+}
 
 /// Return a Validity that includes `now`, and lasts for `lifetime_days` additionally.
+///
+/// Additionally, return the time at which the certificate expires.
 ///
 /// We ensure that our cert is valid at least a day into the past.
 ///
 /// We obfuscate our current time a little by rounding to the nearest midnight UTC.
-fn cert_validity(now: SystemTime, lifetime_days: u32) -> Result<Validity, X509CertError> {
+fn cert_validity(
+    now: SystemTime,
+    lifetime_days: u32,
+) -> Result<(Validity, SystemTime), X509CertError> {
     const ONE_DAY: Duration = Duration::new(86400, 0);
 
     let start_of_day_containing = |when| -> Result<_, X509CertError> {
@@ -122,10 +282,12 @@ fn cert_validity(now: SystemTime, lifetime_days: u32) -> Result<Validity, X509Ce
     let start_on_day = now - ONE_DAY;
     let end_on_day = start_on_day + ONE_DAY * lifetime_days;
 
-    Ok(Validity {
+    let validity = Validity {
         not_before: start_of_day_containing(start_on_day)?,
         not_after: start_of_day_containing(end_on_day)?,
-    })
+    };
+    let expiration = validity.not_after.into();
+    Ok((validity, expiration))
 }
 
 /// Return a random serial number for use in a new certificate.
@@ -160,6 +322,10 @@ pub enum X509CertError {
     /// We constructed the certificate, but couldn't encode it as DER.
     #[error("Unable to encode certificate")]
     CouldNotEncode(#[source] x509_cert::der::Error),
+
+    /// We constructed a key but couldn't format it as PKCS8.
+    #[error("Unable to format key as PKCS8")]
+    CouldNotFormatPkcs8(#[source] p256::pkcs8::Error),
 
     /// We've encountered some kind of a bug.
     #[error("Internal error while creating certificate")]
@@ -210,5 +376,18 @@ mod test {
         // but afaict most of them sensibly refuse to handle RSA1024.
         //
         // I've checked the above-generated cert using `openssl verify`, but that's it.
+    }
+
+    #[test]
+    fn tls_cert_info() {
+        let mut rng = testing_rng();
+        let certified = TlsCertifiedKey::create(
+            &mut rng,
+            SystemTime::now(),
+            "foo.example.com",
+            "bar.example.com",
+        )
+        .unwrap();
+        dbg!(certified);
     }
 }
