9414042270b1f61ab2e704bac98fd0a60098ef61
Gabriela Moldovan <gabi@torproject.org>
2026-01-06 10:54:03 +0000

    keymgr: Add a CTorKeySpecifier trait (fmt)

--- crates/tor-keymgr/src/lib.rs
+++ crates/tor-keymgr/src/lib.rs
@@ -72,10 +72,10 @@ pub use err::{
     UnrecognizedEntry, UnrecognizedEntryError,
 };
 pub use key_specifier::{
-    ArtiPathError, ArtiPathRange, ArtiPathUnavailableError, CTorPath, CTorKeySpecifier, CTorPathError,
-    InvalidKeyPathComponentValue, KeyCertificateSpecifier, KeyPath, KeyPathError, KeyPathInfo,
-    KeyPathInfoBuilder, KeyPathInfoExtractor, KeyPathPattern, KeySpecifier, KeySpecifierComponent,
-    KeySpecifierComponentViaDisplayFromStr, KeySpecifierPattern,
+    ArtiPathError, ArtiPathRange, ArtiPathUnavailableError, CTorKeySpecifier, CTorPath,
+    CTorPathError, InvalidKeyPathComponentValue, KeyCertificateSpecifier, KeyPath, KeyPathError,
+    KeyPathInfo, KeyPathInfoBuilder, KeyPathInfoExtractor, KeyPathPattern, KeySpecifier,
+    KeySpecifierComponent, KeySpecifierComponentViaDisplayFromStr, KeySpecifierPattern,
 };
 #[cfg(feature = "onion-service-cli-extra")]
 pub use raw::{RawEntryId, RawKeystoreEntry};
