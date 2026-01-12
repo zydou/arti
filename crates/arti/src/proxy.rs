zydou/arti

commit 0cc367b9fef37c0f7d7f04d54c4687b27ef251d0

arti: Change how proxy futures are returned.
Previously, the proxy and dns modules only had "be a proxy"
functions that ran forever.  Now they have functions that bind to
listeners and return a separate "be a proxy" future that runs
forever.

This lets us simplify some kludges in subcommands::proxy.  More
importantly, it will let us return the bound-to ports so that
subcommands::proxy can write them to disk.

This is a break in experimental-apis, which does not require a
semver change.

crates/arti/src/proxy.rs
@@ -1,6 +1,6 @@
 //! Implement a simple proxy that relays connections over Tor.
 //!
-//! A proxy is launched with [`run_proxy()`], which listens for new
+//! A proxy is launched with [`launch_proxy()`], which starts a task to listen for new
 //! connections, handles an appropriate handshake,
 //! and then relays traffic as appropriate.
 
@@ -334,14 +334,16 @@ fn accept_err_is_fatal(err: &IoError) -> bool {
 /// Requires a `runtime` to use for launching tasks and handling
 /// timeouts, and a `tor_client` to use in connecting over the Tor
 /// network.
+///
+/// Returns a future that actually instantiates the proxy.
 #[cfg_attr(feature = "experimental-api", visibility::make(pub))]
 #[instrument(skip_all, level = "trace")]
-pub(crate) async fn run_proxy<R: Runtime>(
+pub(crate) async fn launch_proxy<R: Runtime>(
     runtime: R,
     tor_client: TorClient<R>,
     listen: Listen,
     rpc_data: Option<RpcProxySupport>,
-) -> Result<()> {
+) -> Result<impl Future<Output = Result<()>>> {
     #[cfg(feature = "rpc")]
     let (rpc_mgr, mut rpc_state_sender) = match rpc_data {
         Some(RpcProxySupport {
@@ -407,7 +409,7 @@ pub(crate) async fn run_proxy<R: Runtime>(
         }
     }
 
-    run_proxy_with_listeners(tor_client, listeners, rpc_mgr).await
+    Ok(run_proxy_with_listeners(tor_client, listeners, rpc_mgr))
 }
 
 /// Launch a proxy from a given set of already bound listeners.

crates/arti/src/subcommands/proxy.rs
@@ -177,30 +177,34 @@ async fn run_proxy<R: ToplevelRuntime>(
         }
     }
 
-    let mut proxy: Vec<PinnedFuture<(Result<()>, &str)>> = Vec::new();
+    let mut proxy: Vec<PinnedFuture<Result<()>>> = Vec::new();
     if !socks_listen.is_empty() {
         let runtime = runtime.clone();
         let client = client.isolated_client();
         let socks_listen = socks_listen.clone();
-        proxy.push(Box::pin(async move {
-            let res = proxy::run_proxy(runtime, client, socks_listen, rpc_data).await;
-            #[cfg(feature = "http-connect")]
-            let listener_type = "SOCKS+HTTP";
-            #[cfg(not(feature = "http-connect"))]
-            let listener_type = "SOCKS";
+        #[cfg(feature = "http-connect")]
+        let listener_type = "SOCKS+HTTP";
+        #[cfg(not(feature = "http-connect"))]
+        let listener_type = "SOCKS";
 
-            (res, listener_type)
-        }));
+        let proxy_future = proxy::launch_proxy(runtime, client, socks_listen, rpc_data)
+            .await
+            .with_context(|| format!("Unable to launch {listener_type} proxy"))?;
+        let failure_message = format!("{listener_type} proxy died unexpectedly");
+        let proxy_future = proxy_future.map(|future_result| future_result.context(failure_message));
+        proxy.push(Box::pin(proxy_future));
     }
 
     #[cfg(feature = "dns-proxy")]
     if !dns_listen.is_empty() {
         let runtime = runtime.clone();
         let client = client.isolated_client();
-        proxy.push(Box::pin(async move {
-            let res = dns::run_dns_resolver(runtime, client, dns_listen).await;
-            (res, "DNS")
-        }));
+        let proxy_future = dns::launch_dns_resolver(runtime, client, dns_listen)
+            .await
+            .context("Unable to launch DNS proxy")?;
+        let proxy_future =
+            proxy_future.map(|future_result| future_result.context("DNS proxy died unexpectedly"));
+        proxy.push(Box::pin(proxy_future));
     }
 
     #[cfg(not(feature = "dns-proxy"))]
@@ -230,7 +234,7 @@ async fn run_proxy<R: ToplevelRuntime>(
         r = exit::wait_for_ctrl_c().fuse()
             => r.context("waiting for termination signal"),
         r = proxy.fuse()
-            => r.0.context(format!("{} proxy failure", r.1)),
+            => r,
         r = async {
             client.bootstrap().await?;
             if !socks_listen.is_empty() {

crates/arti/src/dns.rs
@@ -1,19 +1,19 @@
 //! Implement a simple DNS resolver that relay request over Tor.
 //!
-//! A resolver is launched with [`run_dns_resolver()`], which listens for new
-//! connections and then runs
+//! A resolver is launched with [`launch_dns_resolver()`], which launches a task to listen for
+//! DNS requests, and send back replies in response.
 
 use futures::lock::Mutex;
 use futures::stream::StreamExt;
-use tor_rtcompat::SpawnExt;
 use hickory_proto::op::{
-    header::MessageType, op_code::OpCode, response_code::ResponseCode, Message, Query,
+    Message, Query, header::MessageType, op_code::OpCode, response_code::ResponseCode,
 };
-use hickory_proto::rr::{rdata, DNSClass, Name, RData, Record, RecordType};
+use hickory_proto::rr::{DNSClass, Name, RData, Record, RecordType, rdata};
 use hickory_proto::serialize::binary::{BinDecodable, BinEncodable};
 use std::collections::HashMap;
 use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
+use tor_rtcompat::SpawnExt;
 use tracing::{debug, error, info, warn};
 
 use arti_client::{Error, HasKind, StreamPrefs, TorClient};
@@ -237,13 +237,15 @@ where
 }
 
 /// Launch a DNS resolver to listen on a given local port, and run indefinitely.
+///
+/// Return a future that implements the DNS resolver.
 #[cfg_attr(feature = "experimental-api", visibility::make(pub))]
 #[allow(clippy::cognitive_complexity)] // TODO: Refactor
-pub(crate) async fn run_dns_resolver<R: Runtime>(
+pub(crate) async fn launch_dns_resolver<R: Runtime>(
     runtime: R,
     tor_client: TorClient<R>,
     listen: Listen,
-) -> Result<()> {
+) -> Result<impl Future<Output = Result<()>>> {
     if !listen.is_loopback_only() {
         warn!(
             "Configured to listen for DNS on non-local addresses. This is usually insecure! We recommend listening on localhost only."
@@ -286,6 +288,17 @@ pub(crate) async fn run_dns_resolver<R: Runtime>(
         return Err(anyhow!("Couldn't open any DNS listeners"));
     }
 
+    Ok(run_dns_resolver_with_listeners(
+        runtime, tor_client, listeners,
+    ))
+}
+
+/// Inner task: Receive incoming DNS requests and process them.
+async fn run_dns_resolver_with_listeners<R: Runtime>(
+    runtime: R,
+    tor_client: TorClient<R>,
+    listeners: Vec<<R as tor_rtcompat::UdpProvider>::UdpSocket>,
+) -> Result<()> {
     let mut incoming = futures::stream::select_all(
         listeners
             .into_iter()

commit 572418264b53dd514a0a1aa9a686079b325290fc

Make sure that rpc_state_sender lives as long as the proxies.
Fixes bug introduced in 0cc367b9fef37c0f7d7f04d54c4687b27ef251d0.

Without this fix, RPC's get_proxy_info command wouldn't work.

crates/arti/src/proxy.rs
@@ -11,6 +11,7 @@ semipublic_mod! {
     pub(crate) mod port_info;
 }
 
+use futures::FutureExt as _;
 use futures::io::{AsyncRead, AsyncWrite, AsyncWriteExt, BufReader, Error as IoError};
 use futures::stream::StreamExt;
 use std::net::IpAddr;
@@ -355,7 +356,7 @@ pub(crate) async fn launch_proxy<R: Runtime>(
         None => (None, None),
     };
     #[cfg(not(feature = "rpc"))]
-    let rpc_mgr = None;
+    let (rpc_mgr, rpc_state_sender) = (None, None::<()>);
 
     if !listen.is_loopback_only() {
         warn!(
@@ -426,7 +427,12 @@ pub(crate) async fn launch_proxy<R: Runtime>(
         .collect();
 
     Ok((
-        run_proxy_with_listeners(tor_client, listeners, rpc_mgr),
+        run_proxy_with_listeners(tor_client, listeners, rpc_mgr).map(move |r| {
+            // Ensure that rpc_state_sender lasts as long the future.
+            #[cfg_attr(not(feature = "rpc"), allow(dropping_copy_types))]
+            drop(rpc_state_sender);
+            r
+        }),
         ports,
     ))
 }