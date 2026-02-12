c8636badda05d51204eca43275c51e49719b011e
Nick Mathewson <nickm@torproject.org>
2026-02-12 12:11:52 -0500

    tor-config: fix and improve ExtendBuilder override.
    1. When we are told to `extend_with`, we should obey that directive
       even if we have a sub_builder etc.
    
    2. Provide an `extend_with_replace` function for the common case
       where we want to just replace one object with another.

--- crates/tor-config/src/derive.rs
+++ crates/tor-config/src/derive.rs
@@ -2300,12 +2300,16 @@ define_derive_deftly! {
                 ${for fields {
                     ${when not(fmeta(tor_config(skip)))}
 
-                    ${if any(fmeta(tor_config(sub_builder)),
+                    ${if fmeta(tor_config(extend_with)) {
+                        ${fmeta(tor_config(extend_with)) as expr}(&mut self.$fname, other.$fname, strategy);
+                    } else if fmeta(tor_config(extend_with_replace)) {
+                        if let Some(other_val) = other.$fname {
+                            self.$fname = Some(other_val);
+                        }
+                    } else if any(fmeta(tor_config(sub_builder)),
                              fmeta(tor_config(list)),
                              fmeta(tor_config(map))) {
                         $E::ExtendBuilder::extend_from(&mut self.$fname, other.$fname, strategy);
-                    } else if fmeta(tor_config(extend_with)) {
-                        ${fmeta(tor_config(extend_with)) as expr}(&mut self.$fname, other.$fname, strategy);
                     } else {
                         if let Some(other_val) = other.$fname {
                             self.$fname = Some(other_val);
