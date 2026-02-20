1104e8627e8276390bd7c61e664436da3107355a
moumenalaoui <moumenalaoui@proton.me>
2026-02-19 16:02:07 -0800

    tor-rtcompat: unfold for async-std incoming streams

--- crates/tor-rtcompat/src/impls/async_std.rs
+++ crates/tor-rtcompat/src/impls/async_std.rs
@@ -13,8 +13,7 @@ mod net {
     #[cfg(unix)]
     use async_std_crate::os::unix::net::{UnixListener, UnixStream};
     use async_trait::async_trait;
-    use futures::future::Future;
-    use futures::stream::Stream;
+    use futures::stream::{self, Stream};
     use paste::paste;
     use std::io::Result as IoResult;
     use std::net::SocketAddr;
@@ -33,40 +32,18 @@ mod net {
             /// struct is a real type, and that it returns a stream and an address
             /// for each input.
             pub struct [<Incoming $kind Streams>] {
-                /// A state object, stored in an Option so we can take ownership of it
-                /// while poll is being called.
-                // TODO(nickm): I hate using this trick.  At some point in the
-                // future, once Rust has nice support for async traits, maybe
-                // we can refactor it.
-                state: Option<[<Incoming $kind StreamsState>]>,
-            }
-            /// The result type returned by `take_and_poll_*`.
-            ///
-            /// It has to include the Listener, since take_and_poll() has
-            /// ownership of the listener.
-            type [<$kind FResult>] = (IoResult<([<$kind Stream>], $addr)>, [<$kind Listener>]);
-            /// Helper to implement `Incoming*Streams`
-            ///
-            /// This function calls `Listener::accept` while owning the
-            /// listener.  Thus, it returns a future that itself owns the listener,
-            /// and we don't have lifetime troubles.
-            async fn [<take_and_poll_ $kind:lower>](lis: [<$kind Listener>]) -> [<$kind FResult>] {
-                let result = lis.accept().await;
-                (result, lis)
-            }
-            /// The possible states for an `Incoming*Streams`.
-            enum [<Incoming $kind StreamsState>] {
-                /// We're ready to call `accept` on the listener again.
-                Ready([<$kind Listener>]),
-                /// We've called `accept` on the listener, and we're waiting
-                /// for a future to complete.
-                Accepting(Pin<Box<dyn Future<Output = [<$kind FResult>]> + Send + Sync>>),
+                /// Underlying stream of incoming connections.
+                inner: Pin<Box<dyn Stream<Item = IoResult<([<$kind Stream>], $addr)>> + Send + Sync>>,
             }
             impl [<Incoming $kind Streams>] {
                 /// Create a new IncomingStreams from a Listener.
                 pub fn from_listener(lis: [<$kind Listener>]) -> [<Incoming $kind Streams>] {
+                    let stream = stream::unfold(lis, |lis| async move {
+                        let result = lis.accept().await;
+                        Some((result, lis))
+                    });
                     Self {
-                        state: Some([<Incoming $kind StreamsState>]::Ready(lis)),
+                        inner: Box::pin(stream),
                     }
                 }
             }
@@ -74,22 +51,7 @@ mod net {
                 type Item = IoResult<([<$kind Stream>], $addr)>;
 
                 fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-                    use [<Incoming $kind StreamsState>] as St;
-                    let state = self.state.take().expect("No valid state!");
-                    let mut future = match state {
-                        St::Ready(lis) => Box::pin([<take_and_poll_ $kind:lower>](lis)),
-                        St::Accepting(fut) => fut,
-                    };
-                    match future.as_mut().poll(cx) {
-                        Poll::Ready((val, lis)) => {
-                            self.state = Some(St::Ready(lis));
-                            Poll::Ready(Some(val))
-                        }
-                        Poll::Pending => {
-                            self.state = Some(St::Accepting(future));
-                            Poll::Pending
-                        }
-                    }
+                    self.inner.as_mut().poll_next(cx)
                 }
             }
             impl traits::NetStreamListener<$addr> for [<$kind Listener>] {
