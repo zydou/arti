9dd49dfdd5be85c397f137f51309652e437ad936
David Goulet <dgoulet@torproject.org>
2026-02-26 09:24:26 -0500

    proto: Remove/fix some very minor TODO(relay)
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- crates/tor-proto/src/relay/channel.rs
+++ crates/tor-proto/src/relay/channel.rs
@@ -203,7 +203,6 @@ pub(crate) struct ChannelAuthenticationData {
     pub(crate) scert: [u8; 32],
 }
 
-#[expect(unused)] // TODO(relay). remove
 impl ChannelAuthenticationData {
     /// Helper: return the authentication type string from the given link auth version.
     const fn auth_type_bytes(link_auth: u16) -> Result<&'static [u8]> {
@@ -249,7 +248,6 @@ impl ChannelAuthenticationData {
         body.extend_from_slice(tls_secrets.as_slice());
 
         // Add the random bytes.
-        let mut rng = rand::rng();
         let random: [u8; 24] = rand::rng().random();
         body.extend_from_slice(&random);
 
