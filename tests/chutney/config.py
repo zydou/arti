b4759fe2002f822bb49f3df9de1c95e117520c2f
Jim Newsome <jnewsome@torproject.org>
2026-03-02 18:20:05 -0600

    chutney bootstrap: convert to python
    This is primarily a proof-of-concept for converting these scripts before
    tackling the more complext 'test' script.

--- tests/chutney/config.py
+++ tests/chutney/config.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import dataclasses
 import json
+import os
 
 from pathlib import Path
 
@@ -15,6 +16,16 @@ class Config:
     chutney_data_dir: str
     network: str
 
+    def export_env(self) -> None:
+        os.environ["CHUTNEY_ARTI"] = self.arti
+        os.environ["CHUTNEY_DATA_DIR"] = self.chutney_data_dir
+
     def dump_json(self, dst: Path) -> None:
         with dst.open("w") as c:
             json.dump(dataclasses.asdict(self), c, indent=2)
+
+    @staticmethod
+    def load_json(src: Path) -> Config:
+        with src.open("r") as c:
+            j = json.load(c)
+        return Config(**j)
