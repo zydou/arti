07991dce5c426bd2078fb733693ee0b053a3b8ec
Steven Engler <opara@torproject.org>
2026-03-03 21:47:25 -0500

    tor-basic-utils: clean up `iter_join`

--- crates/tor-basic-utils/src/lib.rs
+++ crates/tor-basic-utils/src/lib.rs
@@ -107,23 +107,21 @@ pub fn skip_fmt<T>(_: &T, f: &mut fmt::Formatter) -> fmt::Result {
 
 // ----------------------------------------------------------------------
 
-use std::fmt::Display;
-
 /// Formats an iterator as an object whose display implementation is a `separator`-separated string
 /// of items from `iter`.
-// TODO: This can be replaced with `std::fmt::from_fn()` once stabilised and within our MSRV.
 pub fn iter_join(
     separator: &str,
-    iter: impl Iterator<Item: Display> + Clone,
-) -> impl Display {
-    struct Fmt<'a, I: Iterator<Item: Display> + Clone> {
+    iter: impl Iterator<Item: fmt::Display> + Clone,
+) -> impl fmt::Display {
+    // TODO: This can be replaced with `std::fmt::from_fn()` once stabilised and within our MSRV.
+    struct Fmt<'a, I: Iterator<Item: fmt::Display> + Clone> {
         /// Separates items in `iter`.
         separator: &'a str,
         /// Iterator to join.
         iter: I,
     }
-    impl<'a, I: Iterator<Item: Display> + Clone> Display for Fmt<'a, I> {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    impl<'a, I: Iterator<Item: fmt::Display> + Clone> fmt::Display for Fmt<'a, I> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             let Self { separator, iter } = self;
             let mut iter = iter.clone();
             if let Some(first) = iter.next() {
