artenax/arti2

rpc: API for event-driven IO.
This API provides the necessary functionality to use an RpcConn
inside a poll-like event loop.

This is part of #1856.

crates/arti-rpc-client-core/src/nb_stream.rs
@@ -25,6 +25,11 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+#[cfg(unix)]
+use std::os::fd::{AsFd as _, BorrowedFd as BorrowedOsHandle};
+#[cfg(windows)]
+use std::os::windows::io::{AsSocket as _, BorroedSocket as BorrowedOsHandle};
+
 /// An IO stream to Arti, along with any supporting logic necessary to check it for readiness.
 ///
 /// Internally, this uses `mio` along with a [`NonblockingStream`] to check for events.
@@ -187,6 +192,13 @@ impl PollingStream {
             }
         }
     }
+
+    /// Downgrade this stream into a [`NonblockingStream`] for use within an [`RpcPoll`](crate::RpcPoll).
+    pub(crate) fn into_nonblocking(self) -> NonblockingStream {
+        let mut result = self.stream;
+        result.stream = result.stream.remove_mio();
+        result
+    }
 }
 
 impl Drop for PollingStream {
@@ -285,7 +297,7 @@ pub(crate) struct NonblockingStream {
 
 /// Helper to return which events a [`NonblockingStream`] is interested in.
 #[derive(Clone, Debug, Default, Copy)]
-pub(crate) struct WantIo {
+pub struct WantIo {
     /// True if the stream is interested in writing.
     ///
     /// (It is always interested in reading.)
@@ -346,6 +358,21 @@ impl NonblockingStream {
     /// Return a new [`WriteHandle`] that can be used to queue messages to be sent via this stream.
     pub(crate) fn writer(&self) -> WriteHandle {
         self.write_handle.clone()
+    }
+
+    /// Try to return an OS-level handle for use with this stream.
+    ///
+    /// This is an fd on unix and a SOCKET on windows.
+    pub(crate) fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+        self.stream.try_as_handle()
+    }
+
+    /// Replace the existing waker for this [`NonblockingStream`].
+    ///
+    /// This should only be done while nothing else is interacting with the stream or the waker.
+    pub(crate) fn replace_waker(&mut self, new_waker: Box<dyn Waker>) {
+        let mut h = self.write_handle.inner.lock().expect("Poisoned lock");
+        h.waker = new_waker;
     }
 
     /// Try to exchange messages with the RPC server.
@@ -479,16 +506,24 @@ pub(crate) trait Stream: io::Read + io::Write + Send {
     ///
     /// Otherwise return None.
     fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream>;
+
+    /// Discard any mio-specific wrappers on this stream.
+    fn remove_mio(self: Box<Self>) -> Box<dyn Stream>;
+
+    /// Return an os-specific handle for using this stream type within a nonblocking event loop.
+    ///
+    /// (This will be an fd on unix and a SOCKET on windows.)
+    fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>>;
 }
 
 /// A [`Stream`] that we can use inside a [`PollingStream`].
 pub(crate) trait MioStream: Stream + mio::event::Source {}
 
 /// An object that can wake a pending IO poller.
-///
-/// When the underlying IO loop is `mio`, this is a [`mio::Waker`];
-/// otherwise, it is some user-provided type.
-pub(crate) trait Waker: Send + Sync {
+//
+// When the underlying IO loop is `mio`, this is a [`mio::Waker`];
+// otherwise, it is some user-provided type.
+pub trait Waker: Send + Sync {
     /// Alert the polling thread.
     fn wake(&mut self) -> io::Result<()>;
 }
@@ -506,13 +541,38 @@ macro_rules! impl_traits {
             fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream> {
                 None
             }
+            fn remove_mio(self: Box<Self>) -> Box<dyn Stream> {
+                self
+            }
+            fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+                cfg_if::cfg_if!{
+                    if #[cfg(unix)] {
+                        Ok(self.as_fd())
+                    } else if #[cfg(windows)] {
+                        Ok(self.as_socket())
+                    }
+                }
+            }
         }
         impl Stream for $mio_stream {
             fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream> {
                 Some(self as _)
             }
+            fn remove_mio(self: Box<Self>) -> Box<dyn Stream> {
+                Box::new(<$stream>::from(*self))
+            }
+            fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+                cfg_if::cfg_if!{
+                    if #[cfg(unix)] {
+                        Ok(self.as_fd())
+                    } else if #[cfg(windows)] {
+                        Ok(self.as_socket())
+                    }
+                }
+            }
         }
-        impl MioStream for $mio_stream {}
+        impl MioStream for $mio_stream {
+        }
     }
 }
 
@@ -665,6 +725,12 @@ mod test {
     impl Stream for TestStream {
         fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream> {
             None
+        }
+        fn remove_mio(self: Box<Self>) -> Box<dyn Stream> {
+            self
+        }
+        fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+            Err(io::Error::from(io::ErrorKind::Other))
         }
     }
 

zydou/arti

crates/arti-rpc-client-core/src/nb_stream.rs
@@ -25,6 +25,11 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+#[cfg(unix)]
+use std::os::fd::{AsFd as _, BorrowedFd as BorrowedOsHandle};
+#[cfg(windows)]
+use std::os::windows::io::{AsSocket as _, BorroedSocket as BorrowedOsHandle};
+
 /// An IO stream to Arti, along with any supporting logic necessary to check it for readiness.
 ///
 /// Internally, this uses `mio` along with a [`NonblockingStream`] to check for events.
@@ -82,7 +87,10 @@ pub(crate) struct PollingStream {
     ///
     /// Invariant: `stream.stream` is a [`MioStream`], so [`Stream::as_mio_stream`] will return
     /// Some when we call it.
-    stream: NonblockingStream,
+    ///
+    /// This is None only if we have called `into_nonblocking()` or `drop()`
+    /// we store this in an Option so `that we can move it out of this object.
+    stream: Option<NonblockingStream>,
 }
 
 /// A `mio` token corresponding to the Waker we use to tell the interactor about new writes.
@@ -105,13 +113,15 @@ impl PollingStream {
         let mut cio = Self {
             poll,
             events: mio::Events::with_capacity(4),
-            stream,
+            stream: Some(stream),
         };
 
         // We register the stream here, since we want to use it exclusively with `reregister`
         // later on.  We do not deregister the stream until `Drop::drop` is called.
         cio.poll.registry().register(
             cio.stream
+                .as_mut()
+                .expect("Logic error: stream not present")
                 .stream
                 .as_mio_stream()
                 .expect("logic error: not a mio stream."),
@@ -124,7 +134,10 @@ impl PollingStream {
 
     /// Return a new [`WriteHandle`] that can be used to queue messages to be sent via this stream.
     pub(crate) fn writer(&self) -> WriteHandle {
-        self.stream.writer()
+        self.stream
+            .as_ref()
+            .expect("logic error: stream not present")
+            .writer()
     }
 
     /// Interact with the peer until some response is received.
@@ -147,8 +160,13 @@ impl PollingStream {
         let mut try_reading = true;
 
         loop {
+            let stream = self
+                .stream
+                .as_mut()
+                .expect("logic error: stream not present!");
+
             // Try interacting with the underlying stream.
-            let want_io = match self.stream.interact_once(try_writing, try_reading)? {
+            let want_io = match stream.interact_once(try_writing, try_reading)? {
                 PollStatus::Closed => return Ok(None),
                 PollStatus::Msg(msg) => return Ok(Some(msg)),
                 PollStatus::WouldBlock(w) => w,
@@ -157,7 +175,7 @@ impl PollingStream {
             // We're blocking on reading and possibly writing.  Register our interest,
             // so that we get woken as appropriate.
             self.poll.registry().reregister(
-                self.stream
+                stream
                     .stream
                     .as_mio_stream()
                     .expect("logic error: not a mio stream!"),
@@ -187,13 +205,26 @@ impl PollingStream {
             }
         }
     }
-}
 
-impl Drop for PollingStream {
-    fn drop(&mut self) {
+    /// Downgrade this stream into a [`NonblockingStream`] for use within an [`RpcPoll`](crate::RpcPoll).
+    pub(crate) fn into_nonblocking(mut self) -> NonblockingStream {
+        let mut stream = self
+            .deregister_and_take_stream()
+            .expect("logic error: stream not present!");
+        stream.stream = stream.stream.remove_mio();
+        stream
+    }
+
+    /// Implementation helper for Drop and into_nonblocking:
+    ///
+    /// Deregisters the NonblockingStream with the mio Registry, removes it from this object,
+    /// and returns it.
+    ///
+    /// After this method is called, this object may no longer be used.
+    fn deregister_and_take_stream(&mut self) -> Option<NonblockingStream> {
         // IO SAFETY: See "IO Safety" note in documentation for PollingStream.
-        let s = self
-            .stream
+        let mut stream = self.stream.take()?;
+        let s: &mut _ = stream
             .stream
             .as_mio_stream()
             .expect("Logic error: Stream was not a MIO stream.");
@@ -201,6 +232,14 @@ impl Drop for PollingStream {
             .registry()
             .deregister(s)
             .expect("Deregister operation failed");
+        Some(stream)
+    }
+}
+
+impl Drop for PollingStream {
+    fn drop(&mut self) {
+        // IO SAFETY: See "IO Safety" note in documentation for PollingStream.
+        let _ = self.deregister_and_take_stream();
     }
 }
 
@@ -285,7 +324,7 @@ pub(crate) struct NonblockingStream {
 
 /// Helper to return which events a [`NonblockingStream`] is interested in.
 #[derive(Clone, Debug, Default, Copy)]
-pub(crate) struct WantIo {
+pub struct WantIo {
     /// True if the stream is interested in writing.
     ///
     /// (It is always interested in reading.)
@@ -346,6 +385,21 @@ impl NonblockingStream {
     /// Return a new [`WriteHandle`] that can be used to queue messages to be sent via this stream.
     pub(crate) fn writer(&self) -> WriteHandle {
         self.write_handle.clone()
+    }
+
+    /// Try to return an OS-level handle for use with this stream.
+    ///
+    /// This is an fd on unix and a SOCKET on windows.
+    pub(crate) fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+        self.stream.try_as_handle()
+    }
+
+    /// Replace the existing waker for this [`NonblockingStream`].
+    ///
+    /// This should only be done while nothing else is interacting with the stream or the waker.
+    pub(crate) fn replace_waker(&mut self, new_waker: Box<dyn Waker>) {
+        let mut h = self.write_handle.inner.lock().expect("Poisoned lock");
+        h.waker = new_waker;
     }
 
     /// Try to exchange messages with the RPC server.
@@ -479,16 +533,24 @@ pub(crate) trait Stream: io::Read + io::Write + Send {
     ///
     /// Otherwise return None.
     fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream>;
+
+    /// Discard any mio-specific wrappers on this stream.
+    fn remove_mio(self: Box<Self>) -> Box<dyn Stream>;
+
+    /// Return an os-specific handle for using this stream type within a nonblocking event loop.
+    ///
+    /// (This will be an fd on unix and a SOCKET on windows.)
+    fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>>;
 }
 
 /// A [`Stream`] that we can use inside a [`PollingStream`].
 pub(crate) trait MioStream: Stream + mio::event::Source {}
 
 /// An object that can wake a pending IO poller.
-///
-/// When the underlying IO loop is `mio`, this is a [`mio::Waker`];
-/// otherwise, it is some user-provided type.
-pub(crate) trait Waker: Send + Sync {
+//
+// When the underlying IO loop is `mio`, this is a [`mio::Waker`];
+// otherwise, it is some user-provided type.
+pub trait Waker: Send + Sync {
     /// Alert the polling thread.
     fn wake(&mut self) -> io::Result<()>;
 }
@@ -506,13 +568,38 @@ macro_rules! impl_traits {
             fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream> {
                 None
             }
+            fn remove_mio(self: Box<Self>) -> Box<dyn Stream> {
+                self
+            }
+            fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+                cfg_if::cfg_if!{
+                    if #[cfg(unix)] {
+                        Ok(self.as_fd())
+                    } else if #[cfg(windows)] {
+                        Ok(self.as_socket())
+                    }
+                }
+            }
         }
         impl Stream for $mio_stream {
             fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream> {
                 Some(self as _)
             }
+            fn remove_mio(self: Box<Self>) -> Box<dyn Stream> {
+                Box::new(<$stream>::from(*self))
+            }
+            fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+                cfg_if::cfg_if!{
+                    if #[cfg(unix)] {
+                        Ok(self.as_fd())
+                    } else if #[cfg(windows)] {
+                        Ok(self.as_socket())
+                    }
+                }
+            }
         }
-        impl MioStream for $mio_stream {}
+        impl MioStream for $mio_stream {
+        }
     }
 }
 
@@ -665,6 +752,12 @@ mod test {
     impl Stream for TestStream {
         fn as_mio_stream(&mut self) -> Option<&mut dyn MioStream> {
             None
+        }
+        fn remove_mio(self: Box<Self>) -> Box<dyn Stream> {
+            self
+        }
+        fn try_as_handle(&self) -> io::Result<BorrowedOsHandle<'_>> {
+            Err(io::Error::from(io::ErrorKind::Other))
         }
     }
 