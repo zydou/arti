9430a39c17844a7e20233a1cb330ca8ca0907aef
Jim Newsome <jnewsome@torproject.org>
2026-03-05 11:00:31 -0600

    Document tests/chutney/config.py

--- tests/chutney/config.py
+++ tests/chutney/config.py
@@ -9,22 +9,35 @@ from pathlib import Path
 
 @dataclasses.dataclass
 class Config:
+    """Test configuration
+
+    Generated and written out by the `setup` script,
+    and read by other test scripts."""
+
+    # path to the chutney CLI executable
     chutney: str
+    # path to the arti client executable
     arti: str
+    # path to the arti-bench executable
     arti_bench: str
+    # chutney data directory (CHUTNEY_DATA_DIR)
     chutney_data_dir: str
+    # chutney CLI-flag network specification (e.g. "--net=basic-min")
     network: str
 
     def export_env(self) -> None:
+        """Set environment variables based on this config"""
         os.environ["CHUTNEY_ARTI"] = self.arti
         os.environ["CHUTNEY_DATA_DIR"] = self.chutney_data_dir
 
     def dump_json(self, dst: Path) -> None:
+        """Dump this config as json to `dst`"""
         with dst.open("w") as c:
             json.dump(dataclasses.asdict(self), c, indent=2)
 
     @staticmethod
     def load_json(src: Path) -> Config:
+        """Load a config previously written via `dump_json` from `src`"""
         with src.open("r") as c:
             j = json.load(c)
         return Config(**j)
