3891ddd1d2a28f1a41a6e2dfc9f48d032b700a86
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-03-11 13:25:31 +0000

    tor-dirserver: Avoid using NetdocParseable for consensuses (prep)
    Formatting changes which make the next commit more readable.

--- crates/tor-dirserver/src/mirror/operation.rs
+++ crates/tor-dirserver/src/mirror/operation.rs
@@ -449,12 +449,18 @@ impl StaticEngine {
         // <https://gitlab.torproject.org/tpo/core/arti/-/merge_requests/3664#note_3352723>
         let consensus = match self.flavor {
             ConsensusFlavor::Plain => FlavoredConsensus::Ns(
-                parse2::parse_netdoc(&ParseInput::new(&consensus, ""))
-                    .map_err(into_internal!("invalid netdoc in database?"))?,
+                parse2::parse_netdoc(&ParseInput::new(
+                    &consensus, "",
+                ))
+                .map_err(into_internal!("invalid netdoc in database?"))?
+                ,
             ),
             ConsensusFlavor::Microdesc => FlavoredConsensus::Md(
-                parse2::parse_netdoc(&ParseInput::new(&consensus, ""))
-                    .map_err(into_internal!("invalid netdoc in database?"))?,
+                parse2::parse_netdoc(&ParseInput::new(
+                    &consensus, "",
+                ))
+                .map_err(into_internal!("invalid netdoc in database?"))?
+                ,
             ),
         };
 
