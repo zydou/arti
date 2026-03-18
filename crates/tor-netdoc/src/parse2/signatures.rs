d9c643e7869b0543c8b72483a58a5707d22d3d94
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-03-18 16:19:54 +0000

    tor-netdoc: signature orderliness: orderly signs don't cover other sigs
    This is implied by `body` being the body before the first signature
    item, but needs to be part of the definition.

--- crates/tor-netdoc/src/parse2/signatures.rs
+++ crates/tor-netdoc/src/parse2/signatures.rs
@@ -225,6 +225,7 @@ pub trait SignatureHashesAccumulator: Clone {
 ///
 /// This is used for both Orderly signatures
 /// where the hash does not contain any part of the signature Item
+/// nor of any further signatures.
 /// and Disorderly signatures
 /// where the hash contains part of the signature Item.
 /// (The Tor protocols currently only have Disorderly signatures.)
