9529d2ce6ca954464c4587f08657183200e581c1
Steven Engler <opara@torproject.org>
2026-03-24 22:07:15 -0400

    tor-proto: change `dyn ChannelProvider` to also be `Sync`
    An `Arc<T>` is only `Send` if `T` is `Send + Sync`,
    which is needed by the runtime.

--- crates/tor-proto/src/relay/reactor.rs
+++ crates/tor-proto/src/relay/reactor.rs
@@ -117,7 +117,7 @@ impl<R: Runtime> Reactor<R> {
         crypto_in: Box<dyn InboundRelayLayer + Send>,
         crypto_out: Box<dyn OutboundRelayLayer + Send>,
         settings: &HopSettings,
-        chan_provider: Arc<dyn ChannelProvider<BuildSpec = OwnedChanTarget> + Send>,
+        chan_provider: Arc<dyn ChannelProvider<BuildSpec = OwnedChanTarget> + Send + Sync>,
         padding_ctrl: PaddingController,
         padding_event_stream: PaddingEventStream,
         memquota: &CircuitAccount,
