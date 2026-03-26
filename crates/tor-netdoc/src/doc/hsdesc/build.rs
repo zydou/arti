cec5d8e8194a03ace7e45ebbeba95a7da0c3f7cb
Nick Mathewson <nickm@torproject.org>
2026-03-26 10:39:48 -0400

    tor-netdoc: Port to web-time-compat

--- crates/tor-netdoc/src/doc/hsdesc/build.rs
+++ crates/tor-netdoc/src/doc/hsdesc/build.rs
@@ -320,6 +320,7 @@ mod test {
     use tor_hscrypto::time::TimePeriod;
     use tor_linkspec::LinkSpec;
     use tor_llcrypto::pk::{curve25519, ed25519::ExpandedKeypair};
+    use web_time_compat::SystemTimeExt;
 
     // TODO: move the test helpers to a separate module and make them more broadly available if
     // necessary.
@@ -410,7 +411,7 @@ mod test {
             .compute_blinded_key(period)
             .unwrap();
 
-        let expiry = SystemTime::now() + Duration::from_secs(CERT_EXPIRY_SECS);
+        let expiry = SystemTime::get() + Duration::from_secs(CERT_EXPIRY_SECS);
         let mut rng = Config::Deterministic.into_rng();
         let intro_points = vec![IntroPointDesc {
             link_specifiers: vec![
