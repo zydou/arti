3400cbee08748cf8977e8bf976c8ee7124ba58d3
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-04-20 15:40:03 +0100

    tor-netdoc: Test for IPv6-mapped IPv4 address

--- crates/tor-netdoc/src/types/misc.rs
+++ crates/tor-netdoc/src/types/misc.rs
@@ -2109,6 +2109,7 @@ mod test {
         chk_addr("::");
         chk_addr("::1");
         chk_addr("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+        chk_addr("::ffff:192.0.2.3"); // IPv6-mapped IPv4 address
     }
 
     #[test]
