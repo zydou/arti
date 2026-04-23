45f23e48cad2473abd40d58e53fb6ca7463e7c84
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-04-23 10:49:54 +0100

    tor-netdoc: types/misc.rs: un-gate two helper methods

--- crates/tor-netdoc/src/types/parse2_encode.rs
+++ crates/tor-netdoc/src/types/parse2_encode.rs
@@ -9,13 +9,11 @@ pub mod raw_data_object {
     use super::*;
 
     /// "Parse" the data
-    #[cfg(feature = "parse2")]
     pub fn try_from(data: Vec<u8>) -> Result<Vec<u8>, Void> {
         Ok(data)
     }
 
     /// "Encode" the data
-    #[cfg(feature = "encode")]
     pub fn write_object_onto(self_: &[u8], out: &mut Vec<u8>) -> Result<(), Bug> {
         out.extend(self_);
         Ok(())
