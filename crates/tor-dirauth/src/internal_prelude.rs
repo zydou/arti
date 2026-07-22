2ecc111069f2a363376b656acb5356e6e0c8ee3f
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-07-22 11:22:39 +0100

    tor-dirauth: Introduce new TrackedConsensusMethod type

--- crates/tor-dirauth/src/internal_prelude.rs
+++ crates/tor-dirauth/src/internal_prelude.rs
@@ -1,11 +1,14 @@
 //! Shared imports for this crate
 
-pub(crate) use std::cmp;
+pub(crate) use std::cell::Cell;
+pub(crate) use std::cmp::{self, Ordering};
+pub(crate) use std::fmt::{self, Debug};
 pub(crate) use std::net::Ipv4Addr;
-pub(crate) use std::ops::RangeInclusive;
+pub(crate) use std::ops::{RangeBounds, RangeInclusive};
 
 pub(crate) use ipnet::{IpNet, Ipv4Net};
-pub(crate) use itertools::Itertools;
+pub(crate) use itertools::{Itertools, chain};
+pub(crate) use paste::paste;
 pub(crate) use rangemap::RangeInclusiveMap;
 
 pub(crate) use tor_error::{Bug, internal, into_internal};
