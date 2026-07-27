2ea541807eddb6ae9f676b2a6ad7eea5492f8335
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-07-27 13:16:55 +0100

    tor-hscrypto: Replace .chunks_exact with .as_chunks
    Placates recent clippy.  I find the API of both of these functions
    unsatisfactory and would prefer a chunks_exact that promises to panic,
    but I didn't find one in std or Itertools.

--- crates/tor-hscrypto/src/ope.rs
+++ crates/tor-hscrypto/src/ope.rs
@@ -122,10 +122,9 @@ pub struct SrvPeriodOffset(
 fn add_slice_as_le_u16(slice: &[u8]) -> u64 {
     assert_eq!(slice.len() % 2, 0);
     slice
-        .chunks_exact(2)
+        .as_chunks::<2>().0.iter()
         .map(|bytepair| {
-            let a: [u8; 2] = bytepair.try_into().expect("chunk was not of size 2!");
-            u64::from(u16::from_le_bytes(a))
+            u64::from(u16::from_le_bytes(*bytepair))
         })
         .sum()
 }
