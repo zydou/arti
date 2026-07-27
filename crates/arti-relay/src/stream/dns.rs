73d8de8e3dce14b8b325d144450075daa973bbac
Gabriela Moldovan <gabi@torproject.org>
2026-07-27 12:10:25 +0000

    arti-relay: Remove unused_async clippy escape hatch
    This was needed to work around two `unused_async`
    false-positives that would trigger on older Rust versions.
    This was fixed in https://github.com/rust-lang/rust-clippy/issues/15305
    and released in 1.90, according to the [changelog].
    
    Since these no longer trigger on 1.97.1, they are now causing the
    `expect(clippy::unused_async)` to fail. Removing the `expect`s seems
    like the most sensible fix.
    
    [changelog]: https://github.com/rust-lang/rust-clippy/issues/15305

--- crates/arti-relay/src/stream/dns.rs
+++ crates/arti-relay/src/stream/dns.rs
@@ -3,7 +3,6 @@
 use tor_proto::stream::IncomingStream;
 
 /// Handle an incoming DNS stream
-#[expect(clippy::unused_async)] // TODO(relay)
 pub(crate) async fn handle_resolve(_incoming: IncomingStream) -> anyhow::Result<()> {
     todo!()
 }
--- crates/arti-relay/src/stream/exit.rs
+++ crates/arti-relay/src/stream/exit.rs
@@ -3,7 +3,6 @@
 use tor_proto::stream::IncomingStream;
 
 /// Handle an incoming exit stream
-#[expect(clippy::unused_async)] // TODO(relay)
 pub(super) async fn handle_begin(_incoming: IncomingStream) -> anyhow::Result<()> {
     todo!()
 }
