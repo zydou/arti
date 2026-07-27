ecf9154d0170f5fe6b78c8cb04a61680e6901e6c
Gabriela Moldovan <gabi@torproject.org>
2026-07-27 12:33:04 +0000

    arti-relay: Add back allow(clippy::unused_async)
    The clippy CI job that uses RECENT_RUST_IMAGE fails without the `allow`
    (RECENT_RUST_IMAGE uses Rust 1.96, where the `unused_async`
    false-positive still appears to trigger).

--- crates/arti-relay/src/stream/dns.rs
+++ crates/arti-relay/src/stream/dns.rs
@@ -3,6 +3,7 @@
 use tor_proto::stream::IncomingStream;
 
 /// Handle an incoming DNS stream
+#[allow(clippy::unused_async)] // TODO(relay)
 pub(crate) async fn handle_resolve(_incoming: IncomingStream) -> anyhow::Result<()> {
     todo!()
 }
--- crates/arti-relay/src/stream/exit.rs
+++ crates/arti-relay/src/stream/exit.rs
@@ -3,6 +3,7 @@
 use tor_proto::stream::IncomingStream;
 
 /// Handle an incoming exit stream
+#[allow(clippy::unused_async)] // TODO(relay)
 pub(super) async fn handle_begin(_incoming: IncomingStream) -> anyhow::Result<()> {
     todo!()
 }
