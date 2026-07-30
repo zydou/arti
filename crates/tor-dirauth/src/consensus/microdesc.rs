89b99d1f34c164daa4dd21b58378a5bb4d02a7b1
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-07-30 12:09:18 +0100

    tor-dirauth: tests: Test microdescriptor calculation against live data

--- crates/tor-dirauth/src/consensus/microdesc.rs
+++ crates/tor-dirauth/src/consensus/microdesc.rs
@@ -49,3 +49,67 @@ pub fn compute_microdesc(
     };
     Ok(m)
 }
+
+#[cfg(test)]
+mod test {
+    // @@ begin test lint list maintained by maint/add_warning @@
+    #![allow(clippy::bool_assert_comparison)]
+    #![allow(clippy::clone_on_copy)]
+    #![allow(clippy::dbg_macro)]
+    #![allow(clippy::mixed_attributes_style)]
+    #![allow(clippy::print_stderr)]
+    #![allow(clippy::print_stdout)]
+    #![allow(clippy::single_char_pattern)]
+    #![allow(clippy::unwrap_used)]
+    #![allow(clippy::unchecked_time_subtraction)]
+    #![allow(clippy::useless_vec)]
+    #![allow(clippy::needless_pass_by_value)]
+    #![allow(clippy::string_slice)] // See arti#2571
+    //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
+    use super::*;
+    use tor_checkable::TimeBound as _;
+    use tor_netdoc::{
+        assert_eq_or_diff,
+        doc::routerdesc::RouterDescUnverified,
+        parse2::{ParseInput, parse_netdoc},
+        test_support::regsub,
+        testdata_live,
+    };
+
+    fn get_router_desc(relay: &testdata_live::PerRelay) -> anyhow::Result<RouterDesc> {
+        let rd_txt = relay.data.plain;
+        let rd: RouterDescUnverified = parse_netdoc(&ParseInput::new(rd_txt, relay.nick))?;
+        let rd = rd.verify()?.dangerously_assume_timely();
+        Ok(rd)
+    }
+
+    #[test]
+    fn microdescs() -> anyhow::Result<()> {
+        let method = TrackedConsensusMethod::new(
+            // Test with method 100; will need to bump this occasionally,
+            // and/or add more regsub fixups, if this test fails.
+            ConsensusMethod(100).try_into()?,
+        );
+
+        for relay in testdata_live::RELAY_DESCRIPTORS {
+            let rd = get_router_desc(relay)?;
+            let md = compute_microdesc(&rd, &method)?;
+            let md_txt = encode_netdoc_unsigned([&md])?;
+
+            let mut exp_md = relay.data.md.to_owned();
+            regsub(
+                &mut exp_md,
+                r#"(?x) ^ onion-key \n
+                          -----BEGIN\ .*----- \n
+                          [^-]+
+                          -----END\ .*----- \n
+                "#,
+                "onion-key\n",
+            );
+
+            assert_eq_or_diff!(md_txt, exp_md, "for relay {}", relay.nick);
+        }
+
+        Ok(())
+    }
+}
