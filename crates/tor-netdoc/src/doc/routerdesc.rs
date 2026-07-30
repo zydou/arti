mycodedoesnotcompile2/arti_mirror

tor-netdoc: Add large RouterDesc verification test
This commit adds a comprehensive test for router descriptor verification
that tests various valid and invalid edge cases.

crates/tor-netdoc/src/doc/routerdesc.rs
@@ -1323,11 +1323,15 @@ mod test {
     #![allow(clippy::needless_pass_by_value)]
     #![allow(clippy::string_slice)] // See arti#2571
     //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
+    use std::{net::Ipv4Addr, str::FromStr, time::Duration};
+
     use crate::{
         encode::{NetdocEncodable, NetdocEncoder},
         parse2::{self, NetdocParseableUnverified, ParseInput},
     };
-    use tor_llcrypto::pk::rsa;
+    use tor_basic_utils::test_rng::testing_rng;
+    use tor_checkable::TimeValidityError;
+    use tor_llcrypto::pk::{curve25519, ed25519::Ed25519PublicKey, rsa};
 
     use super::*;
     const TESTDATA: &str = include_str!("../../testdata/routerdesc1.txt");
@@ -1673,5 +1677,170 @@ mod test {
         .unwrap();
 
         out.finish().unwrap()
+    }
+
+    /// Test for various succeeding and failing verifications.
+    #[test]
+    fn test_verify() {
+        // Generate keys we will use later.
+        let rng = &mut testing_rng();
+        let rsa_id = rsa::KeyPair::generate(rng).unwrap();
+        let ed25519_id = ed25519::Keypair::generate(rng);
+        let ed25519_sign = ed25519::Keypair::generate(rng);
+        let curve25519_ntor = curve25519::StaticSecret::random_from_rng(&mut *rng);
+        let curve25519_ntor = curve25519::StaticKeypair {
+            secret: curve25519_ntor.clone(),
+            public: (&curve25519_ntor).into(),
+        };
+        let ed25519_ntor = tor_llcrypto::pk::keymanip::convert_curve25519_to_ed25519_private(
+            &curve25519_ntor.secret,
+        )
+        .unwrap();
+
+        // Values arbitrarily chosen for expirations.
+        let too_early = Iso8601TimeSp::from_str("2026-05-01 03:00:00").unwrap().0;
+        let published = Iso8601TimeSp::from_str("2026-06-01 03:00:00").unwrap().0;
+        let now = Iso8601TimeSp::from_str("2026-06-10 15:30:12").unwrap().0;
+        let expiration = Iso8601TimeSp::from_str("2026-06-20 03:00:00").unwrap().0;
+        let expired = expiration + Duration::from_secs(60 * 60 * 24);
+
+        // Very boilerplatey construction of a router descriptor.
+        let mut rd = RouterDesc {
+            router: RouterDescIntroItem {
+                nickname: "foo".parse().unwrap(),
+                address: Ipv4Addr::LOCALHOST,
+                orport: 9000,
+                socksport: 0,
+                dirport: 0,
+            },
+            identity_ed25519: Ed25519IdentityCert::new_signed(
+                &ed25519_id,
+                Ed25519Identity::from(ed25519_sign.public_key()),
+                expiration,
+            )
+            .unwrap(),
+            master_key_ed25519: Ed25519Identity::from(ed25519_id.public_key()).into(),
+            bandwidth: Bandwidth {
+                average: 0,
+                burst: 0,
+                observed: 0,
+            },
+            platform: None,
+            published: published.into(),
+            fingerprint: Some(rsa_id.to_public_key().to_rsa_identity().into()),
+            hibernating: NumericBoolean(false),
+            uptime: None,
+            onion_key: None,
+            ntor_onion_key: Curve25519Public(curve25519_ntor.public),
+            ntor_onion_key_crosscert: NtorOnionKeyCrossCert {
+                bit: NumericBoolean(ed25519_ntor.1 == 1),
+                cert: Ed25519NtorCrossCert::new_signed(
+                    &ed25519_ntor.0,
+                    ed25519_id.public_key().into(),
+                    expiration,
+                )
+                .unwrap(),
+            },
+            signing_key: rsa_id.to_public_key(),
+            ipv4_policy: AddrPolicy::default(),
+            ipv6_policy: Default::default(),
+            overload_general: None,
+            contact: None,
+            family: Default::default(),
+            family_cert: Default::default(),
+            caches_extra_info: Some(Default::default()),
+            extra_info_digest: None,
+            hidden_service_dir: Some(Default::default()),
+            or_address: Default::default(),
+            tunnelled_dir_server: Some(Default::default()),
+            proto: tor_protover::Protocols::new(),
+        };
+        let rd_original = rd.clone();
+
+        let verify =
+            |rd: &RouterDesc| -> std::result::Result<TimerangeBound<RouterDesc>, VerifyFailed> {
+                let encoded = rd_encode_sign(rd, &rsa_id, &ed25519_sign);
+                let decoded = parse2::parse_netdoc::<RouterDescUnverified>(&ParseInput::new(
+                    &encoded,
+                    "<test_invalid>",
+                ))
+                .unwrap();
+                decoded.verify()
+            };
+
+        // Test valid and invalid timestamps.
+        assert_eq!(
+            verify(&rd).unwrap().is_valid_at(&too_early).unwrap_err(),
+            TimeValidityError::NotYetValid(published.duration_since(too_early).unwrap())
+        );
+        verify(&rd).unwrap().is_valid_at(&published).unwrap();
+        // This is our good/"everything is working" test
+        verify(&rd).unwrap().is_valid_at(&now).unwrap();
+        verify(&rd).unwrap().is_valid_at(&expiration).unwrap();
+        assert_eq!(
+            verify(&rd).unwrap().is_valid_at(&expired).unwrap_err(),
+            TimeValidityError::Expired(expired.duration_since(expiration).unwrap())
+        );
+
+        // Let's make the certificate inconsistent by changing the master key.
+        rd.master_key_ed25519 = Ed25519Public(Ed25519Identity::from([0x12; 32]));
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::Inconsistent);
+        rd = rd_original.clone();
+
+        // Set the published to expired (weird on many levels).
+        rd.published = expired.into();
+        assert_eq!(
+            verify(&rd).unwrap().is_valid_at(&now).unwrap_err(),
+            TimeValidityError::NotYetValid(expired.duration_since(now).unwrap())
+        );
+        rd = rd_original.clone();
+
+        // Have an inconsistent fingerprint.
+        let other_rsa_key = rsa::KeyPair::generate(rng).unwrap();
+        rd.fingerprint = Some(other_rsa_key.to_public_key().to_rsa_identity().into());
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::Inconsistent);
+        // It should fail with a different error if we change the signing key.
+        // (No longer inconsistent but simply not validly signed)
+        rd.signing_key = other_rsa_key.to_public_key();
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
+        // It should work again if we set it to None.
+        rd.signing_key = rd_original.signing_key.clone();
+        rd.fingerprint = None;
+        verify(&rd).unwrap().is_valid_at(&now).unwrap();
+        rd = rd_original.clone();
+
+        // If we change the ntor-onion-key, the crosscert verification will fail.
+        // We must generate a valid random key here because otherwise, decompress
+        // will fail.
+        let other_curve25519 = curve25519::StaticSecret::random_from_rng(&mut *rng);
+        let other_curve25519 = (&other_curve25519).into();
+        rd.ntor_onion_key = Curve25519Public(other_curve25519);
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
+        rd = rd_original.clone();
+
+        // Testing for a signature key of the wrong size is hard because
+        // tor-llcrypto makes it purposely hard to generate key sizes other
+        // than 1024 bit.
+
+        // TODO: Test family certificates.
+
+        // Violate the outer ed25519 signatures, which can be done by swapping
+        // the signing key to something else.
+        let different_ed25519_sign = ed25519::Keypair::generate(rng);
+        rd.identity_ed25519 = Ed25519IdentityCert::new_signed(
+            &ed25519_id,
+            Ed25519Identity::from(different_ed25519_sign.public_key()),
+            expiration,
+        )
+        .unwrap();
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
+        rd = rd_original.clone();
+
+        // Violate the outer RSA signature by swapping the signing key and
+        // "disabling" the fingerprint.
+        let different_rsa_id = rsa::KeyPair::generate(rng).unwrap();
+        rd.signing_key = different_rsa_id.to_public_key();
+        rd.fingerprint = None;
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
     }
 }

zydou/arti

crates/tor-netdoc/src/doc/routerdesc.rs
@@ -1278,11 +1278,15 @@ mod test {
     #![allow(clippy::needless_pass_by_value)]
     #![allow(clippy::string_slice)] // See arti#2571
     //! <!-- @@ end test lint list maintained by maint/add_warning @@ -->
+    use std::{net::Ipv4Addr, str::FromStr, time::Duration};
+
     use crate::{
         encode::{NetdocEncodable, NetdocEncoder},
         parse2::{self, NetdocParseableUnverified, ParseInput},
     };
-    use tor_llcrypto::pk::rsa;
+    use tor_basic_utils::test_rng::testing_rng;
+    use tor_checkable::TimeValidityError;
+    use tor_llcrypto::pk::{curve25519, ed25519::Ed25519PublicKey, rsa};
 
     use super::*;
     const TESTDATA: &str = include_str!("../../testdata/routerdesc1.txt");
@@ -1626,5 +1630,169 @@ mod test {
         .unwrap();
 
         out.finish().unwrap()
+    }
+
+    /// Test for various succeeding and failing verifications.
+    #[test]
+    fn test_verify() {
+        // Generate keys we will use later.
+        let rng = &mut testing_rng();
+        let rsa_id = rsa::KeyPair::generate(rng).unwrap();
+        let ed25519_id = ed25519::Keypair::generate(rng);
+        let ed25519_sign = ed25519::Keypair::generate(rng);
+        let curve25519_ntor = curve25519::StaticSecret::random_from_rng(&mut *rng);
+        let curve25519_ntor = curve25519::StaticKeypair {
+            secret: curve25519_ntor.clone(),
+            public: (&curve25519_ntor).into(),
+        };
+        let ed25519_ntor = tor_llcrypto::pk::keymanip::convert_curve25519_to_ed25519_private(
+            &curve25519_ntor.secret,
+        )
+        .unwrap();
+
+        // Values arbitrarily chosen for expirations.
+        let too_early = Iso8601TimeSp::from_str("2026-05-01 03:00:00").unwrap().0;
+        let published = Iso8601TimeSp::from_str("2026-06-01 03:00:00").unwrap().0;
+        let now = Iso8601TimeSp::from_str("2026-06-10 15:30:12").unwrap().0;
+        let expiration = Iso8601TimeSp::from_str("2026-06-20 03:00:00").unwrap().0;
+        let expired = expiration + Duration::from_secs(60 * 60 * 24);
+
+        // Very boilerplatey construction of a router descriptor.
+        let mut rd = RouterDesc {
+            router: RouterDescIntroItem {
+                nickname: "foo".parse().unwrap(),
+                address: Ipv4Addr::LOCALHOST,
+                orport: 9000,
+                socksport: 0,
+                dirport: 0,
+            },
+            identity_ed25519: Ed25519IdentityCert::new_signed(
+                &ed25519_id,
+                Ed25519Identity::from(ed25519_sign.public_key()),
+                expiration,
+            )
+            .unwrap(),
+            master_key_ed25519: Ed25519Identity::from(ed25519_id.public_key()).into(),
+            bandwidth: Bandwidth {
+                average: 0,
+                burst: 0,
+                observed: 0,
+            },
+            platform: None,
+            published: published.into(),
+            fingerprint: Some(rsa_id.to_public_key().to_rsa_identity().into()),
+            hibernating: NumericBoolean(false),
+            uptime: None,
+            ntor_onion_key: Curve25519Public(curve25519_ntor.public),
+            ntor_onion_key_crosscert: NtorOnionKeyCrossCert {
+                bit: NumericBoolean(ed25519_ntor.1 == 1),
+                cert: Ed25519NtorCrossCert::new_signed(
+                    &ed25519_ntor.0,
+                    ed25519_id.public_key().into(),
+                    expiration,
+                )
+                .unwrap(),
+            },
+            signing_key: rsa_id.to_public_key(),
+            ipv4_policy: AddrPolicy::default(),
+            ipv6_policy: Default::default(),
+            overload_general: None,
+            contact: None,
+            family: Default::default(),
+            family_cert: Default::default(),
+            caches_extra_info: Some(Default::default()),
+            extra_info_digest: None,
+            hidden_service_dir: Some(Default::default()),
+            or_address: Default::default(),
+            tunnelled_dir_server: Some(Default::default()),
+            proto: tor_protover::Protocols::new(),
+        };
+        let rd_original = rd.clone();
+
+        let verify =
+            |rd: &RouterDesc| -> std::result::Result<TimeRangeBound<RouterDesc>, VerifyFailed> {
+                let encoded = rd_encode_sign(rd, &rsa_id, &ed25519_sign);
+                let decoded = parse2::parse_netdoc::<RouterDescUnverified>(&ParseInput::new(
+                    &encoded,
+                    "<test_invalid>",
+                ))
+                .unwrap();
+                decoded.verify()
+            };
+
+        // Test valid and invalid timestamps.
+        assert_eq!(
+            verify(&rd).unwrap().if_valid_at(&too_early).unwrap_err(),
+            TimeValidityError::NotYetValid(published.duration_since(too_early).unwrap())
+        );
+        verify(&rd).unwrap().if_valid_at(&published).unwrap();
+        // This is our good/"everything is working" test
+        verify(&rd).unwrap().if_valid_at(&now).unwrap();
+        verify(&rd).unwrap().if_valid_at(&expiration).unwrap();
+        assert_eq!(
+            verify(&rd).unwrap().if_valid_at(&expired).unwrap_err(),
+            TimeValidityError::Expired(expired.duration_since(expiration).unwrap())
+        );
+
+        // Let's make the certificate inconsistent by changing the master key.
+        rd.master_key_ed25519 = Ed25519Public(Ed25519Identity::from([0x12; 32]));
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::Inconsistent);
+        rd = rd_original.clone();
+
+        // Set the published to expired (weird on many levels).
+        rd.published = expired.into();
+        assert_eq!(
+            verify(&rd).unwrap().if_valid_at(&now).unwrap_err(),
+            TimeValidityError::NotYetValid(expired.duration_since(now).unwrap())
+        );
+        rd = rd_original.clone();
+
+        // Have an inconsistent fingerprint.
+        let other_rsa_key = rsa::KeyPair::generate(rng).unwrap();
+        rd.fingerprint = Some(other_rsa_key.to_public_key().to_rsa_identity().into());
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::Inconsistent);
+        // It should fail with a different error if we change the signing key.
+        // (No longer inconsistent but simply not validly signed)
+        rd.signing_key = other_rsa_key.to_public_key();
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
+        // It should work again if we set it to None.
+        rd.signing_key = rd_original.signing_key.clone();
+        rd.fingerprint = None;
+        verify(&rd).unwrap().if_valid_at(&now).unwrap();
+        rd = rd_original.clone();
+
+        // If we change the ntor-onion-key, the crosscert verification will fail.
+        // We must generate a valid random key here because otherwise, decompress
+        // will fail.
+        let other_curve25519 = curve25519::StaticSecret::random_from_rng(&mut *rng);
+        let other_curve25519 = (&other_curve25519).into();
+        rd.ntor_onion_key = Curve25519Public(other_curve25519);
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
+        rd = rd_original.clone();
+
+        // Testing for a signature key of the wrong size is hard because
+        // tor-llcrypto makes it purposely hard to generate key sizes other
+        // than 1024 bit.
+
+        // TODO: Test family certificates.
+
+        // Violate the outer ed25519 signatures, which can be done by swapping
+        // the signing key to something else.
+        let different_ed25519_sign = ed25519::Keypair::generate(rng);
+        rd.identity_ed25519 = Ed25519IdentityCert::new_signed(
+            &ed25519_id,
+            Ed25519Identity::from(different_ed25519_sign.public_key()),
+            expiration,
+        )
+        .unwrap();
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
+        rd = rd_original.clone();
+
+        // Violate the outer RSA signature by swapping the signing key and
+        // "disabling" the fingerprint.
+        let different_rsa_id = rsa::KeyPair::generate(rng).unwrap();
+        rd.signing_key = different_rsa_id.to_public_key();
+        rd.fingerprint = None;
+        assert_eq!(verify(&rd).unwrap_err(), VerifyFailed::VerifyFailed);
     }
 }