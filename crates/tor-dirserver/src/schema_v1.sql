eb043f2947c0e099a305a4696dcc5f205c4f83d6
Clara Engler <cve@cve.cx>
2026-08-25 12:07:21 +0200

    tor-netdoc: Use docid instead of rowid in consensus_diff
    This commit changes the consensus_diff table to use docid's instead of
    rowid's for referencing the base and target consensus.  This is indeed a
    bigger waste of space, but for now it is better to have consistency and
    optimize later on, as consensus_diff is currently the outliner table
    with this design.  The next commit will add a TODO comment reflecting on
    this.

--- crates/tor-dirserver/src/schema_v1.sql
+++ crates/tor-dirserver/src/schema_v1.sql
@@ -35,11 +35,11 @@ CREATE TABLE consensus(
 CREATE TABLE consensus_diff(
     rowid                   INTEGER PRIMARY KEY AUTOINCREMENT,
     docid                   TEXT NOT NULL UNIQUE,
-    base_consensus_rowid    INTEGER NOT NULL,
-    target_consensus_rowid  INTEGER NOT NULL,
+    base_consensus_docid    TEXT NOT NULL,
+    target_consensus_docid  TEXT NOT NULL,
     FOREIGN KEY(docid) REFERENCES store(docid),
-    FOREIGN KEY(base_consensus_rowid) REFERENCES consensus(rowid),
-    FOREIGN KEY(target_consensus_rowid) REFERENCES consensus(rowid)
+    FOREIGN KEY(base_consensus_docid) REFERENCES consensus(docid),
+    FOREIGN KEY(target_consensus_docid) REFERENCES consensus(docid)
 ) STRICT;
 
 -- Stores the router descriptors.
