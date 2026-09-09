d7f44c0e9274ab6c1cbca8cf05646710be55313b
Clara Engler <cve@cve.cx>
2026-09-09 12:17:16 +0200

    Box AuthorityRequestError::Request
    Puts the AuthorityRequestError::Request value into a Box in order to not
    have a clippy size warning like we do right now.
    
    It also adds a From implementation manually in order to nicely Box the
    errors.

--- crates/tor-dirserver/src/err.rs
+++ crates/tor-dirserver/src/err.rs
@@ -31,7 +31,7 @@ pub(crate) enum AuthorityRequestError {
     /// here and there with regard to this, hence why we have dirclient in the
     /// first place.
     #[error("dirclient error: {0}")]
-    Request(#[from] tor_dirclient::RequestFailedError),
+    Request(Box<tor_dirclient::RequestFailedError>),
 
     /// A response does not make semantic sense.
     ///
@@ -43,6 +43,12 @@ pub(crate) enum AuthorityRequestError {
     /// An internal error.
     #[error("internal error")]
     Bug(#[from] tor_error::Bug),
+}
+
+impl From<tor_dirclient::RequestFailedError> for AuthorityRequestError {
+    fn from(value: tor_dirclient::RequestFailedError) -> Self {
+        Self::Request(Box::new(value))
+    }
 }
 
 impl IsFatal for AuthorityRequestError {
