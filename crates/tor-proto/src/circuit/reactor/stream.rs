bb5adeca3f83584ccaafd87a73974383b6fbc868
Steven Engler <opara@torproject.org>
2026-09-21 14:12:25 -0400

    tor-proto: fix variable name in `send_msg_to_bwd()`

--- crates/tor-proto/src/circuit/reactor/stream.rs
+++ crates/tor-proto/src/circuit/reactor/stream.rs
@@ -558,26 +558,23 @@ impl StreamReactor {
         // TODO(DEDUP): this contains parts of Circuit::send_relay_cell_inner()
 
         {
-            // XXXX: remove me
-            let bwd_msg = &msg;
-
             // We might be out of capacity entirely; see if we are about to hit a limit.
             //
             // TODO: If we ever add a notion of _recoverable_ errors below, we'll
             // need a way to restore this limit, and similarly for about_to_send().
             self.hop.decrement_cell_limit()?;
 
-            let c_t_w = sendme::cmd_counts_towards_windows(bwd_msg.cmd());
+            let c_t_w = sendme::cmd_counts_towards_windows(msg.cmd());
 
             // We need to apply stream-level flow control *before* encoding the message
             // (the BWD handles the encoding)
             if c_t_w {
-                if let Some(stream_id) = bwd_msg.stream_id() {
+                if let Some(stream_id) = msg.stream_id() {
                     self.hop.about_to_send(
                         self.unique_id,
                         self.circ_id,
                         stream_id,
-                        bwd_msg.msg(),
+                        msg.msg(),
                     )?;
                 }
             }
